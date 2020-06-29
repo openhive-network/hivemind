09e4e5c0da3ccfa49378fe398d754e160f2c498d
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-06-29 17:47:23 +0200

    [WIP] Works on hivemind API test framework
    - added initial implementation for tests using pyresttest framework
      * note: test have no arguments added so they will not work,
        arguments will be added in next commit
    - fixes in imports in "manual" tests
    - program for calling enum_virtual_ops added to test timeout issues
      for given block range. Timeout issues were reported by Gandalf.

---
+++ tests/pyrest_tests/run_api_tests.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+NODE='http://127.0.0.1'
+RPC_PORT=8080
+EXIT_CODE=0
+COMPARATOR=''
+
+if [ $1 == 'equal' ]
+then
+   COMPARATOR='comparator_equal'
+elif [ $1 == 'contain' ]
+then
+   COMPARATOR='comparator_contain'
+else
+   echo FATAL: $1 is not a valid comparator! && exit -1
+fi
+
+echo COMPARATOR: $COMPARATOR
+
+pyresttest $NODE:$RPC_PORT ./basic_smoketest.yaml
+[ $? -ne 0 ] && echo FATAL: hivemind not running? && exit -1
+
+pyresttest $NODE:$RPC_PORT ./bridge_api/bridge_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+[ $? -ne 0 ] && EXIT_CODE=-1
+
+pyresttest $NODE:$RPC_PORT ./condenser_api/condenser_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+[ $? -ne 0 ] && EXIT_CODE=-1
+
+pyresttest $NODE:$RPC_PORT ./database_api/database_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+[ $? -ne 0 ] && EXIT_CODE=-1
+
+pyresttest $NODE:$RPC_PORT ./follow_api/follow_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+[ $? -ne 0 ] && EXIT_CODE=-1
+
+pyresttest $NODE:$RPC_PORT ./hive_api/hive_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+[ $? -ne 0 ] && EXIT_CODE=-1
+
+pyresttest $NODE:$RPC_PORT ./tags_api/tags_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+[ $? -ne 0 ] && EXIT_CODE=-1
+
+exit $EXIT_CODE
