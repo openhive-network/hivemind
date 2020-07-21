137975351987d25ce64da209a94dd658b22c9cfc
Bartek Wrona <wrona@syncad.com>
2020-07-21 14:19:21 -0400

    Fixed typo (missing colon) in setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         #'aiopg==0.16.0',
         'aiopg @ https://github.com/aio-libs/aiopg/tarball/862fff97e4ae465333451a4af2a838bfaa3dd0bc',
-        'jsonrpcserver @ https://github.com/bcb/jsonrpcserver/tarball/8f3437a19b6d1a8f600ee2c9b112116c85f17827#egg=jsonrpcserver'
+        'jsonrpcserver @ https://github.com/bcb/jsonrpcserver/tarball/8f3437a19b6d1a8f600ee2c9b112116c85f17827#egg=jsonrpcserver',
         'simplejson',
         'aiohttp',
         'certifi',
