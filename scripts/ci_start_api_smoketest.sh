1d4ab849e02a4574f951bee4c89cd008495213b6
Bartek Wrona <wrona@syncad.com>
2020-08-03 16:57:14 -0400

    - Prerequisites to run tavern tests on CI - Improvements on error checking in CI scripts (pipe failure shall be detected) - Improvements on killing previous processes running for given branch.

--- scripts/ci_start_api_smoketest.sh
+++ scripts/ci_start_api_smoketest.sh
@@ -2,9 +2,19 @@
 
 set -e
 
-cd tests/
-rm -rf ./build
-mkdir ./build
-cd build/
-cmake -DTEST_NODE="$1" ../
-ctest -R api/pyresttests/5000000 --output-on-failure
+cd tests/tests_api/hivemind/tavern
+
+pip3 install --user jsondiff
+pip3 install --user tavern
+pip3 install --user pytest
+
+export HIVEMIND_ADDRESS=$1
+export HIVEMIND_PORT=$2
+echo Attempting to start tests on hivemind instance listeing on: $HIVEMIND_ADDRESS port: $HIVEMIND_PORT
+
+echo "Selected test group (if empty all will be executed): $3"
+
+python3 -m pytest -W ignore::pytest.PytestDeprecationWarning --junitxml=../../../../$4 $3
+
+cd ../../../../
+
