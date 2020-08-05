d5e21b61f9b18afb3b50f90523befdc7e71fcecd
ABW <andrzejl@syncad.com>
2020-08-05 20:21:39 +0200

    [ABW]: it turns out old code actually did track ineffective (post-payout) votes - changed upsert query to reflect that

--- hive/indexer/votes.py
+++ hive/indexer/votes.py
@@ -90,34 +90,28 @@ class Votes:
         cls.inside_flush = True
         if cls._votes_data:
             sql = """
-                    INSERT INTO hive_votes
-                    (post_id, voter_id, author_id, permlink_id, weight, rshares, vote_percent, last_update) 
-                    select data_source.post_id, data_source.voter_id, data_source.author_id, data_source.permlink_id, data_source.weight, data_source.rshares, data_source.vote_percent, data_source.last_update
-                    from 
-                    (
-                    SELECT hp.id as post_id, ha_v.id as voter_id, ha_a.id as author_id, hpd_p.id as permlink_id, t.weight, t.rshares, t.vote_percent, t.last_update
-                    from
-                    (
-                    VALUES
-                    --   voter, author, permlink, weight, rshares, vote_percent, last_update
-                      {}
-                    ) AS T(voter, author, permlink, weight, rshares, vote_percent, last_update)
-                    INNER JOIN hive_accounts ha_v ON ha_v.name = t.voter
-                    INNER JOIN hive_accounts ha_a ON ha_a.name = t.author
-                    INNER JOIN hive_permlink_data hpd_p ON hpd_p.permlink = t.permlink
-                    INNER JOIN hive_posts hp ON hp.author_id = ha_a.id AND hp.permlink_id = hpd_p.id
-                    WHERE NOT hp.is_paidout
-                    ) as data_source(post_id, voter_id, author_id, permlink_id, weight, rshares, vote_percent, last_update)
-                    ON CONFLICT ON CONSTRAINT hive_votes_ux1 DO
-                      UPDATE
-                        SET
-                          weight = EXCLUDED.weight,
-                          rshares = EXCLUDED.rshares,
-                          vote_percent = EXCLUDED.vote_percent,
-                          last_update = EXCLUDED.last_update,
-                          num_changes = hive_votes.num_changes + 1
-                      WHERE hive_votes.voter_id = EXCLUDED.voter_id and hive_votes.author_id = EXCLUDED.author_id and hive_votes.permlink_id = EXCLUDED.permlink_id;
-                      """
+                INSERT INTO hive_votes
+                (post_id, voter_id, author_id, permlink_id, weight, rshares, vote_percent, last_update) 
+                SELECT hp.id as post_id, ha_v.id as voter_id, ha_a.id as author_id, hpd_p.id as permlink_id, t.weight, t.rshares, t.vote_percent, t.last_update
+                FROM
+                (
+                VALUES
+                  -- voter, author, permlink, weight, rshares, vote_percent, last_update
+                  {}
+                ) AS T(voter, author, permlink, weight, rshares, vote_percent, last_update)
+                INNER JOIN hive_accounts ha_v ON ha_v.name = t.voter
+                INNER JOIN hive_accounts ha_a ON ha_a.name = t.author
+                INNER JOIN hive_permlink_data hpd_p ON hpd_p.permlink = t.permlink
+                INNER JOIN hive_posts hp ON hp.author_id = ha_a.id AND hp.permlink_id = hpd_p.id
+                ON CONFLICT ON CONSTRAINT hive_votes_ux1 DO
+                UPDATE
+                  SET
+                    weight = (CASE (SELECT hp.is_paidout FROM hive_posts hp WHERE hp.id = EXCLUDED.id) WHEN true THEN hive_votes.weight ELSE EXCLUDED.weight END),
+                    rshares = (CASE (SELECT hp.is_paidout FROM hive_posts hp WHERE hp.id = EXCLUDED.id) WHEN true THEN hive_votes.rshares ELSE EXCLUDED.rshares END),
+                    vote_percent = EXCLUDED.vote_percent,
+                    last_update = EXCLUDED.last_update,
+                    num_changes = hive_votes.num_changes + 1
+                """
 
             values = []
             values_limit = 1000
