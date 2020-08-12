d59f7f751c4beab066393306a8f93fbc6d255328
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-08-12 10:47:06 +0200

    Build and install tweaks
    - removed tests_require from setup.py it is depreciated in favor of tox
    - setup test env in tox
    - removed gitpython dependency

--- scripts/ci_start_api_smoketest.sh
+++ scripts/ci_start_api_smoketest.sh
@@ -2,19 +2,12 @@
 
 set -e
 
-cd tests/tests_api/hivemind/tavern
-
-pip3 install --user jsondiff==1.2.0
-pip3 install --user tavern==1.2.2
-pip3 install --user pytest==6.0.1
-
 export HIVEMIND_ADDRESS=$1
 export HIVEMIND_PORT=$2
 echo Attempting to start tests on hivemind instance listeing on: $HIVEMIND_ADDRESS port: $HIVEMIND_PORT
 
 echo "Selected test group (if empty all will be executed): $3"
 
-python3 -m pytest -W ignore::pytest.PytestDeprecationWarning --junitxml=../../../../$4 $3
+tox -- -W ignore::pytest.PytestDeprecationWarning --junitxml=$4 $3
 
-cd ../../../../
 
