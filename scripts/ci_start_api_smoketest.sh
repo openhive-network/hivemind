af6542135ba6fe0088391a166141330c0c9b09be
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-08-14 09:53:16 +0200

    Fixed path for junitxml

--- scripts/ci_start_api_smoketest.sh
+++ scripts/ci_start_api_smoketest.sh
@@ -9,6 +9,6 @@ echo Attempting to start tests on hivemind instance listeing on: $HIVEMIND_ADDRE
 
 echo "Selected test group (if empty all will be executed): $3"
 
-tox -- -W ignore::pytest.PytestDeprecationWarning --junitxml=$4 $3
+tox -- -W ignore::pytest.PytestDeprecationWarning --junitxml=../../../../$4 $3
 
 
