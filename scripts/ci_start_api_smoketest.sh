0949a7ec16b7126887e672310657fc618cd33c44
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-08-17 13:55:15 +0200

    pytest-xdist plugin and durations option
    - Added pytest-xdist plugin to enable multithreaded support for tests
    - Added durations option to measure test execution time

--- scripts/ci_start_api_smoketest.sh
+++ scripts/ci_start_api_smoketest.sh
@@ -7,6 +7,8 @@ cd tests/tests_api/hivemind/tavern
 pip3 install --user jsondiff==1.2.0
 pip3 install --user tavern==1.2.2
 pip3 install --user pytest==6.0.1
+# package for multithraded test execution
+pip3 install --user pytest-xdist
 pip3 install --user deepdiff[murmur]
 
 export HIVEMIND_ADDRESS=$1
@@ -15,7 +17,9 @@ echo Attempting to start tests on hivemind instance listeing on: $HIVEMIND_ADDRE
 
 echo "Selected test group (if empty all will be executed): $3"
 
-python3 -m pytest -W ignore::pytest.PytestDeprecationWarning --junitxml=../../../../$4 $3
+# -n -- number of thread to run tests with
+# --durations=x show x slowest tests with execution time, 0 show execution time of all tests
+python3 -m pytest -W ignore::pytest.PytestDeprecationWarning -n auto --durations=0 --junitxml=../../../../$4 $3
 
 cd ../../../../
 
