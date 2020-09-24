cc8b861cd4a52fa9e0d27ff05e159032f4da89df
Bartek Wrona <wrona@syncad.com>
2020-09-24 22:11:28 +0200

    Changes to incorporate new table and procedure defined in MR https://gitlab.syncad.com/hive/hivemind/-/merge_requests/181

--- hive/db/sql_scripts/upgrade.sql
+++ hive/db/sql_scripts/upgrade.sql
@@ -515,3 +515,60 @@ WHERE uhp.id = data_source.queried_parent
 END
 $BODY$;
 
+CREATE TABLE IF NOT EXISTS hive_mentions
+(
+  post_id INTEGER NOT NULL,
+  account_id INTEGER NOT NULL,
+
+  CONSTRAINT hive_mentions_PK PRIMARY KEY (post_id, account_id),
+  CONSTRAINT hive_mentions_post_id_FK FOREIGN KEY (post_id)
+    REFERENCES public.hive_posts (id),
+  CONSTRAINT hive_mentions_account_id_FK FOREIGN KEY (post_id)
+    REFERENCES public.hive_accounts (id)
+);
+
+DROP FUNCTION IF EXISTS update_hive_posts_mentions(INTEGER, INTEGER);
+
+CREATE OR REPLACE FUNCTION update_hive_posts_mentions(in _first_block INTEGER, in _last_block INTEGER)
+RETURNS VOID
+LANGUAGE 'plpgsql'
+AS
+$function$
+DECLARE
+  FIRST_BLOCK_TIME TIMESTAMP;
+  LAST_BLOCK_TIME TIMESTAMP;
+BEGIN
+
+  FIRST_BLOCK_TIME = ( SELECT created_at FROM hive_blocks WHERE num = _first_block );
+  LAST_BLOCK_TIME = ( SELECT created_at FROM hive_blocks WHERE num = _last_block );
+
+  IF (LAST_BLOCK_TIME - '90 days'::interval) > FIRST_BLOCK_TIME THEN
+    FIRST_BLOCK_TIME = LAST_BLOCK_TIME - '90 days'::interval;
+  END IF;
+
+  INSERT INTO hive_mentions( post_id, account_id )
+    SELECT DISTINCT T.id_post, ha.id
+    FROM
+      hive_accounts ha
+    INNER JOIN
+    (
+      SELECT T.id_post, LOWER( ( SELECT trim( T.mention::text, '{""}') ) ) mention, T.author_id
+      FROM
+      (
+        SELECT
+          hp.id, REGEXP_MATCHES( hpd.body, '(?:^|[^a-zA-Z0-9_!#$%&*@\\/])(?:@)([a-zA-Z0-9\\.-]{1,16}[a-zA-Z0-9])(?![a-z])', 'g') mention, hp.author_id
+        FROM hive_posts hp
+          INNER JOIN hive_post_data hpd ON hp.id = hpd.id
+        WHERE
+        (
+          hp.created_at >= FIRST_BLOCK_TIME
+        )
+      )T( id_post, mention, author_id )
+    )T( id_post, mention, author_id ) ON ha.name = T.mention
+    WHERE ha.id != T.author_id
+  ON CONFLICT DO NOTHING;
+
+END
+$function$
+;
+
