e51eb1b32a0f6e12566150282167ca0fb0c1ed5e
Bartek Wrona <wrona@syncad.com>
2020-09-28 14:32:49 +0200

    Cherry picked Changes moved to the upgrade.sql

--- hive/db/sql_scripts/upgrade.sql
+++ hive/db/sql_scripts/upgrade.sql
@@ -2920,3 +2920,238 @@ BEGIN
 END
 $BODY$
 ;
+
+--- cherry-pick-98eaf112
+
+DROP FUNCTION IF EXISTS public.calculate_account_reputations;
+
+CREATE OR REPLACE FUNCTION public.calculate_account_reputations(
+  _first_block_num integer,
+  _last_block_num integer,
+  _tracked_account character varying DEFAULT NULL::character varying)
+    RETURNS SETOF accountreputation 
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    STABLE 
+    ROWS 1000
+AS $BODY$
+DECLARE
+  __vote_data RECORD;
+  __account_reputations AccountReputation[];
+  __author_rep bigint;
+  __new_author_rep bigint;
+  __voter_rep bigint;
+  __implicit_voter_rep boolean;
+  __implicit_author_rep boolean;
+  __rshares bigint;
+  __prev_rshares bigint;
+  __rep_delta bigint;
+  __prev_rep_delta bigint;
+  __traced_author int;
+  __account_name varchar;
+BEGIN
+  SELECT INTO __account_reputations ARRAY(SELECT ROW(a.account_id, a.reputation, a.is_implicit)::AccountReputation
+  FROM hive_account_reputation_status a
+  WHERE a.account_id != 0
+  ORDER BY a.account_id);
+
+  SELECT COALESCE((SELECT ha.id FROM hive_accounts ha WHERE ha.name = _tracked_account), 0) INTO __traced_author;
+
+  FOR __vote_data IN
+    SELECT rd.id, rd.author_id, rd.voter_id, rd.rshares,
+      COALESCE((SELECT prd.rshares
+                FROM hive_reputation_data prd
+                WHERE prd.author_id = rd.author_id and prd.voter_id = rd.voter_id
+                      and prd.permlink = rd.permlink and prd.id < rd.id
+                        ORDER BY prd.id DESC LIMIT 1), 0) as prev_rshares
+      FROM hive_reputation_data rd 
+      WHERE (_first_block_num IS NULL AND _last_block_num IS NULL) OR (rd.block_num BETWEEN _first_block_num AND _last_block_num)
+      ORDER BY rd.id
+    LOOP
+      __voter_rep := __account_reputations[__vote_data.voter_id - 1].reputation;
+      __implicit_author_rep := __account_reputations[__vote_data.author_id - 1].is_implicit;
+    
+      IF __vote_data.author_id = __traced_author THEN
+           raise notice 'Processing vote <%> rshares: %, prev_rshares: %', __vote_data.id, __vote_data.rshares, __vote_data.prev_rshares;
+       select ha.name into __account_name from hive_accounts ha where ha.id = __vote_data.voter_id;
+       raise notice 'Voter `%` (%) reputation: %', __account_name, __vote_data.voter_id,  __voter_rep;
+      END IF;
+
+      CONTINUE WHEN __voter_rep < 0;
+
+      __implicit_voter_rep := __account_reputations[__vote_data.voter_id - 1].is_implicit;
+    
+      __author_rep := __account_reputations[__vote_data.author_id - 1].reputation;
+      __rshares := __vote_data.rshares;
+      __prev_rshares := __vote_data.prev_rshares;
+      __prev_rep_delta := (__prev_rshares >> 6)::bigint;
+
+      IF NOT __implicit_author_rep AND --- Author must have set explicit reputation to allow its correction
+         (__prev_rshares > 0 OR
+          --- Voter must have explicitly set reputation to match hived old conditions
+         (__prev_rshares < 0 AND NOT __implicit_voter_rep AND __voter_rep > __author_rep - __prev_rep_delta)) THEN
+            __author_rep := __author_rep - __prev_rep_delta;
+            __implicit_author_rep := __author_rep = 0;
+            __account_reputations[__vote_data.author_id - 1] := ROW(__vote_data.author_id, __author_rep, __implicit_author_rep)::AccountReputation;
+            IF __vote_data.author_id = __traced_author THEN
+             raise notice 'Corrected author_rep by prev_rep_delta: % to have reputation: %', __prev_rep_delta, __author_rep;
+            END IF;
+      END IF;
+
+      __implicit_voter_rep := __account_reputations[__vote_data.voter_id - 1].is_implicit;
+      --- reread voter's rep. since it can change above if author == voter
+    __voter_rep := __account_reputations[__vote_data.voter_id - 1].reputation;
+    
+      IF __rshares > 0 OR
+         (__rshares < 0 AND NOT __implicit_voter_rep AND __voter_rep > __author_rep) THEN
+
+        __rep_delta := (__rshares >> 6)::bigint;
+        __new_author_rep = __author_rep + __rep_delta;
+        __account_reputations[__vote_data.author_id - 1] := ROW(__vote_data.author_id, __new_author_rep, False)::AccountReputation;
+        IF __vote_data.author_id = __traced_author THEN
+          raise notice 'Changing account: <%> reputation from % to %', __vote_data.author_id, __author_rep, __new_author_rep;
+        END IF;
+      ELSE
+        IF __vote_data.author_id = __traced_author THEN
+            raise notice 'Ignoring reputation change due to unmet conditions... Author_rep: %, Voter_rep: %', __author_rep, __voter_rep;
+        END IF;
+      END IF;
+    END LOOP;
+
+    RETURN QUERY
+      SELECT id, Reputation, is_implicit
+      FROM unnest(__account_reputations)
+    WHERE Reputation IS NOT NULL
+    ;
+END
+$BODY$
+;
+
+DROP INDEX IF EXISTS hive_reputation_data_block_num_idx;
+
+CREATE INDEX hive_reputation_data_block_num_idx
+    ON public.hive_reputation_data (block_num)
+    ;
+
+-- Changes from https://gitlab.syncad.com/hive/hivemind/-/merge_requests/208/diffs
+
+DROP VIEW IF EXISTS public.hive_posts_view;
+CREATE OR REPLACE VIEW public.hive_posts_view
+AS
+SELECT hp.id,
+  hp.community_id,
+  hp.root_id,
+  hp.parent_id,
+  ha_a.name AS author,
+  hp.active,
+  hp.author_rewards,
+  hp.author_id,
+  hpd_p.permlink,
+  hpd.title,
+  hpd.body,
+  hpd.img_url,
+  hpd.preview,
+  hcd.category,
+  hp.depth,
+  hp.promoted,
+  hp.payout,
+  hp.pending_payout,
+  hp.payout_at,
+  hp.last_payout_at,
+  hp.cashout_time,
+  hp.is_paidout,
+  hp.children,
+  0 AS votes,
+  0 AS active_votes,
+  hp.created_at,
+  hp.updated_at,
+    COALESCE(
+      (
+        SELECT SUM( v.rshares )
+        FROM hive_votes v
+        WHERE v.post_id = hp.id
+        GROUP BY v.post_id
+      ), 0
+    ) AS rshares,
+    COALESCE(
+      (
+        SELECT SUM( CASE v.rshares >= 0 WHEN True THEN v.rshares ELSE -v.rshares END )
+        FROM hive_votes v
+        WHERE v.post_id = hp.id AND NOT v.rshares = 0
+        GROUP BY v.post_id
+      ), 0
+    ) AS abs_rshares,
+    COALESCE(
+      (
+        SELECT COUNT( 1 )
+        FROM hive_votes v
+        WHERE v.post_id = hp.id AND v.is_effective
+        GROUP BY v.post_id
+      ), 0
+    ) AS total_votes,
+    COALESCE(
+      (
+        SELECT SUM( CASE v.rshares > 0 WHEN True THEN 1 ELSE -1 END )
+        FROM hive_votes v
+        WHERE v.post_id = hp.id AND NOT v.rshares = 0
+        GROUP BY v.post_id
+      ), 0
+    ) AS net_votes,
+  hpd.json,
+  ha_a.reputation AS author_rep,
+  hp.is_hidden,
+  hp.is_grayed,
+  hp.total_vote_weight,
+  ha_pp.name AS parent_author,
+  ha_pp.id AS parent_author_id,
+    ( CASE hp.depth > 0
+      WHEN True THEN hpd_pp.permlink
+      ELSE hcd.category
+    END ) AS parent_permlink_or_category,
+  hp.curator_payout_value,
+  ha_rp.name AS root_author,
+  hpd_rp.permlink AS root_permlink,
+  rcd.category as root_category,
+  hp.max_accepted_payout,
+  hp.percent_hbd,
+    True AS allow_replies,
+  hp.allow_votes,
+  hp.allow_curation_rewards,
+  hp.beneficiaries,
+    CONCAT('/', rcd.category, '/@', ha_rp.name, '/', hpd_rp.permlink,
+      CASE (rp.id)
+        WHEN hp.id THEN ''
+        ELSE CONCAT('#@', ha_a.name, '/', hpd_p.permlink)
+      END
+    ) AS url,
+  rpd.title AS root_title,
+  hp.sc_trend,
+  hp.sc_hot,
+  hp.is_pinned,
+  hp.is_muted,
+  hp.is_nsfw,
+  hp.is_valid,
+  hr.title AS role_title,
+  hr.role_id AS role_id,
+  hc.title AS community_title,
+  hc.name AS community_name,
+  hp.block_num
+  FROM hive_posts hp
+    JOIN hive_posts pp ON pp.id = hp.parent_id
+    JOIN hive_posts rp ON rp.id = hp.root_id
+    JOIN hive_accounts ha_a ON ha_a.id = hp.author_id
+    JOIN hive_permlink_data hpd_p ON hpd_p.id = hp.permlink_id
+    JOIN hive_post_data hpd ON hpd.id = hp.id
+    JOIN hive_accounts ha_pp ON ha_pp.id = pp.author_id
+    JOIN hive_permlink_data hpd_pp ON hpd_pp.id = pp.permlink_id
+    JOIN hive_accounts ha_rp ON ha_rp.id = rp.author_id
+    JOIN hive_permlink_data hpd_rp ON hpd_rp.id = rp.permlink_id
+    JOIN hive_post_data rpd ON rpd.id = rp.id
+    JOIN hive_category_data hcd ON hcd.id = hp.category_id
+    JOIN hive_category_data rcd ON rcd.id = rp.category_id
+    LEFT JOIN hive_communities hc ON hp.community_id = hc.id
+    LEFT JOIN hive_roles hr ON hp.author_id = hr.account_id AND hp.community_id = hr.community_id
+  WHERE hp.counter_deleted = 0;
+
+
