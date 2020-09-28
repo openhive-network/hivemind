4267ac2a2042ff6bc8f2fd4c5fb1c5ddb90c92a0
Marcin Ickiewicz <mickiewicz@syncad.com>
2020-09-28 12:43:07 +0200

    speedup notifications
    Notifications range is narrowed to last 90 days before the head block

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -2191,8 +2191,10 @@ def setup(db):
       "bridge_get_ranked_post_for_communities.sql",
       "bridge_get_ranked_post_for_observer_communities.sql",
       "bridge_get_ranked_post_for_tag.sql",
-      "bridge_get_ranked_post_for_all.sql"
-     ,"calculate_account_reputations.sql"
+      "bridge_get_ranked_post_for_all.sql",
+      "calculate_account_reputations.sql",
+      "head_block_time.sql",
+      "notifications_view.sql"
     ]
     from os.path import dirname, realpath
     dir_path = dirname(realpath(__file__))
