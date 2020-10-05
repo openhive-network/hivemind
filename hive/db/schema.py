85a1ede66e9898e3c535a0f4f800c844c2ade16a
Bartek Wrona <wrona@syncad.com>
2020-10-05 17:05:12 +0200

    - Eliminated `hive_votes_last_update_idx` index which leads postgres engined into using bad execution plan during evaluating contents of hive_accounts_info_view. It directly affected `bridge.get_profile` API. - Definition of `hive_accounts_info_view` slightly changed to eliminate usage of existing and required indexes during evaluating MAX what leads to bad plan serious query slowdown.

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -202,8 +202,7 @@ def build_metadata():
         sa.Index('hive_votes_voter_id_idx', 'voter_id'),
         sa.Index('hive_votes_voter_id_post_id_idx', 'voter_id', 'post_id'),
         sa.Index('hive_votes_post_id_voter_id_idx', 'post_id', 'voter_id'),
-        sa.Index('hive_votes_block_num_idx', 'block_num'),
-        sa.Index('hive_votes_last_update_idx', 'last_update')
+        sa.Index('hive_votes_block_num_idx', 'block_num')
     )
 
     sa.Table(
@@ -703,7 +702,7 @@ def setup(db):
               created_at,
               COALESCE(
                 (
-                  select max(hp.created_at)
+                  select max(hp.created_at + '0 days'::interval)
                   FROM hive_posts hp
                   WHERE ha.id=hp.author_id
                 ),
@@ -711,7 +710,7 @@ def setup(db):
               ),
               COALESCE(
                 (
-                  select max(hv.last_update)
+                  select max(hv.last_update + '0 days'::interval)
                   from hive_votes hv
                   WHERE ha.id=hv.voter_id
                 ),
