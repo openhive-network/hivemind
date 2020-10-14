b67bc32c00d68dc44449d37ca5a41bd2683968e9
Pawel Maniora <pmaniora@syncad.com>
2020-10-14 16:02:25 +0200

    full sync tavern tests

--- scripts/ci_start_api_smoketest.sh
+++ scripts/ci_start_api_smoketest.sh
@@ -5,6 +5,8 @@ pip3 install tox --user
 
 export HIVEMIND_ADDRESS=$1
 export HIVEMIND_PORT=$2
+export TAVERN_DIR="tests/tests_api/hivemind/tavern"
+
 echo Attempting to start tests on hivemind instance listeing on: $HIVEMIND_ADDRESS port: $HIVEMIND_PORT
 
 echo "Selected test group (if empty all will be executed): $3"
