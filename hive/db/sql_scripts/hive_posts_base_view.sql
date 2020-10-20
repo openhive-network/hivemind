91a55b3a48ffa37291116cab730f0be3a1ccec5d
Marcin Ickiewicz <mickiewicz@syncad.com>
2020-10-20 08:43:48 +0200

    issue #81: vote value in notifications

--- hive/db/sql_scripts/hive_posts_base_view.sql
+++ hive/db/sql_scripts/hive_posts_base_view.sql
@@ -1,23 +1,26 @@
 DROP VIEW if exists public.hive_posts_base_view cascade;
-
 CREATE OR REPLACE VIEW public.hive_posts_base_view
- AS
- SELECT 
-    hp.block_num,
-    hp.id,
-    hp.author_id,
-    hp.permlink_id,
-    hp.payout,
-    hp.pending_payout,
-    COALESCE(( SELECT sum(
-                CASE v.rshares >= 0
-                    WHEN true THEN v.rshares
-                    ELSE - v.rshares
-                END) AS sum
-           FROM hive_votes v
-          WHERE v.post_id = hp.id AND NOT v.rshares = 0
-          GROUP BY v.post_id), 0::numeric) AS abs_rshares
+AS
+SELECT
+      hp.block_num
+    , hp.id
+    , hp.author_id
+    , hp.permlink_id
+    , hp.payout
+    , hp.pending_payout
+    , COALESCE( SUM( CASE hv.rshares >= 0 WHEN true THEN hv.rshares ELSE - hv.rshares END), 0::numeric ) AS abs_rshares
+    , COALESCE( SUM( hv.rshares ), 0::numeric ) AS rshares
    FROM hive_posts hp
+   JOIN hive_votes hv ON hp.id = hv.post_id
+   WHERE NOT hv.rshares = 0
+   GROUP BY
+      hv.post_id
+    , hp.block_num
+    , hp.id
+    , hp.author_id
+    , hp.permlink_id
+    , hp.payout
+    , hp.pending_payout
 ;
 
 DROP VIEW IF EXISTS public.hive_posts_pp_view CASCADE;
