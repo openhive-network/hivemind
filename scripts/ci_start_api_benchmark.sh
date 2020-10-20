632361f055516c75a010416e026837d784070c15
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-10-20 12:47:07 +0200

    Html reports are based on benchmark data not on xml junit data

--- scripts/ci_start_api_benchmark.sh
+++ scripts/ci_start_api_benchmark.sh
@@ -4,8 +4,6 @@
 # $2 - server port
 # $3 - path to test directory
 # $4 - name of the benchmark script file
-# $5 - path to generated junit xml file
-
 
 set -e
 
@@ -13,17 +11,16 @@ echo "=========================  BENCHMARKS  ================================="
 echo "Server address: $1"
 echo "Server port: $2"
 echo "Test directory to be processed: $3"
-echo "Benchmark test file path and name: $4"
-echo "junit xml file used to generate html with: $5"
+echo "Benchmark test file name: $4.py"
 
 BASE_DIR=$(pwd)
 echo "Script base dir is: $BASE_DIR"
 
 pip3 install tox --user
 
-echo "Creating benchmark test file as: $4"
-$BASE_DIR/tests/tests_api/hivemind/benchmarks/benchmark_generator.py $3 $4 "http://$1:$2"
+echo "Creating benchmark test file as: $4.py"
+$BASE_DIR/tests/tests_api/hivemind/benchmarks/benchmark_generator.py $3 "$4.py" "http://$1:$2"
 echo "Running benchmark tests on http://$1:$2"
-tox -e benchmark -- $4
-echo "Creating html report..."
-$BASE_DIR/scripts/xml_report_parser.py $3 $5
+tox -e benchmark -- --benchmark-json="$4.json" "$4.py"
+echo "Creating html report from $4.json"
+$BASE_DIR/scripts/json_report_parser.py $3 "$4.json"
--- scripts/json_report_parser.py
+++ scripts/json_report_parser.py
@@ -3,7 +3,7 @@
 import xml.dom.minidom
 import os
 from sys import exit
-from json import dumps
+from json import dumps, load
 
 def get_request_from_yaml(path_to_yaml):
     import yaml
@@ -29,7 +29,7 @@ def make_class_path_dict(root_dir):
         for name in files:
             if fnmatch(name, pattern):
                 test_path = os.path.join(path, name)
-                ret[test_path.replace("/", ".")] = test_path
+                ret[test_path.replace(".", "_").replace("-", "_").replace("/", "_")] = test_path
     return ret
 
 def class_to_path(class_name, class_to_path_dic):
@@ -43,13 +43,14 @@ if __name__ == '__main__':
     above_treshold = False
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_test_dir", type = str, help = "Path to test directory for given xml file")
-    parser.add_argument("xml_file", type = str, help = "Path to report file in xml format")
+    parser.add_argument("path_to_test_dir", type = str, help = "Path to test directory for given json benchmark file")
+    parser.add_argument("json_file", type = str, help = "Path to benchmark json file")
     parser.add_argument("--time-threshold", dest="time_threshold", type=float, default=1.0, help="Time threshold for test execution time, tests with execution time greater than threshold will be marked on red.")
     args = parser.parse_args()
-    html_file, _ = os.path.splitext(args.xml_file)
-    html_file = "tavern_time_report_" + html_file + ".html"
+    html_file, _ = os.path.splitext(args.json_file)
+    html_file = "tavern_benchmark_report_" + html_file + ".html"
     class_to_path_dic = make_class_path_dict(args.path_to_test_dir)
+    print(class_to_path_dic)
     with open(html_file, "w") as ofile:
         ofile.write("<html>\n")
         ofile.write("  <head>\n")
@@ -66,15 +67,15 @@ if __name__ == '__main__':
         ofile.write("  <body>\n")
         ofile.write("    <table>\n")
         ofile.write("      <tr><th>Test name</th><th>Time [s]</th></tr>\n")
-        document = xml.dom.minidom.parse(args.xml_file)
-        tests_collection = document.documentElement
-        for test in tests_collection.getElementsByTagName("testcase"):
-            if test.hasAttribute("name") and test.hasAttribute("time"):
-                if float(test.getAttribute("time")) > args.time_threshold:
-                    ofile.write("      <tr><td>{}<br/>Parameters: {}</td><td bgcolor=\"red\">{}</td></tr>\n".format(test.getAttribute("name"), get_request_from_yaml(class_to_path(test.getAttribute("classname"), class_to_path_dic)), test.getAttribute("time")))
-                    above_treshold = True
-                else:
-                    ofile.write("      <tr><td>{}</td><td>{}</td></tr>\n".format(test.getAttribute("name"), test.getAttribute("time")))
+        json_data = None
+        with open(args.json_file, "r") as json_file:
+            json_data = load(json_file)
+        for benchmark in json_data['benchmarks']:
+            if float(benchmark['stats']['mean']) > args.time_threshold:
+                ofile.write("      <tr><td>{}<br/>Parameters: {}</td><td bgcolor=\"red\">{}</td></tr>\n".format(benchmark['name'], get_request_from_yaml(class_to_path(benchmark['name'][5:], class_to_path_dic)), benchmark['stats']['mean']))
+                above_treshold = True
+            else:
+                ofile.write("      <tr><td>{}</td><td>{}</td></tr>\n".format(benchmark['name'], benchmark['stats']['mean']))
         ofile.write("    </table>\n")
         ofile.write("  </body>\n")
         ofile.write("</html>\n")
