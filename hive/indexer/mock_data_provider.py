43a9a49f8f6e4f539ff7a6d105d6cc6ff6fe84e4
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-10-28 16:51:11 +0100

    Initial commit
    * introduced two new classes: MockBlockProvider and MockVopsProvider
    * possibility to inject artificial data to block and vops using data
      from mock providers
    * data for mock providers is stored in json files, examples provided
    * added command line options to set paths for mock json files

---
+++ hive/indexer/mock_data_provider.py
@@ -0,0 +1,31 @@
+""" Data provider for test operations """
+import logging
+log = logging.getLogger(__name__)
+
+from json import load
+
+class MockDataProvider():
+    """ Data provider for test operations """
+    block_data = {}
+
+    @classmethod
+    def get_max_block_number(cls):
+        return max([int(block) for block in cls.block_data.keys()])
+
+    @classmethod
+    def load_block_data(cls, data_path):
+        with open(data_path, "r") as data_file:
+            cls.block_data = load(data_file)
+
+    @classmethod
+    def add_block_data(cls, block_num, transactions):
+        if block_num in cls.block_data:
+            cls.block_data[block_num].extend(transactions)
+        else:
+            cls.block_data[block_num] = transactions
+
+    @classmethod
+    def get_block_data(cls, block_num, pop=False):
+        if pop:
+            return cls.block_data.pop(block_num, None)
+        return cls.block_data.get(block_num, None)
