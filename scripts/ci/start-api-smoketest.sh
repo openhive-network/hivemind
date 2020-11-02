00b73b774de8eda07537de05808b587bf96c4193
Pawel Maniora <pmaniora@syncad.com>
2020-11-02 12:09:51 +0100

    CI Tavern_dir fix

--- scripts/ci/start-api-smoketest.sh
+++ scripts/ci/start-api-smoketest.sh
@@ -5,6 +5,8 @@ pip install tox
 
 export HIVEMIND_ADDRESS=$1
 export HIVEMIND_PORT=$2
+export TAVERN_DIR="tests/tests_api/hivemind/tavern"
+
 echo "Starting tests on hivemind server running on ${HIVEMIND_ADDRESS}:${HIVEMIND_PORT}"
 
 echo "Selected test group (if empty all will be executed): $3"
