b407b93cd8e9c00f6923e6aedcd4d64c760452f9
Bartek Wrona <wrona@syncad.com>
2020-11-02 23:42:07 +0100

    Removed bad import

--- hive/server/bridge_api/methods.py
+++ hive/server/bridge_api/methods.py
@@ -1,6 +1,5 @@
 """Bridge API public endpoints for posts"""
 
-import hive.server.bridge_api.cursor as cursor
 from hive.server.bridge_api.objects import load_profiles, _bridge_post_object, append_statistics_to_post
 from hive.server.database_api.methods import find_votes_impl, VotesPresentation
 from hive.server.common.helpers import (
