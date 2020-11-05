298efcd4ebe6776f4a9b60fe8ba0b608ac1dd5a5
ABW <andrzejl@syncad.com>
2020-11-05 20:15:15 +0100

    [ABW]: removed duplicate (and slightly wrong) follow related SQL functions added missing .sql files to upgrade script

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -592,7 +592,22 @@ def setup(db):
       "condenser_tags.sql",
       "condenser_follows.sql",
       "hot_and_trends.sql",
-      "update_hive_posts_children_count.sql"
+      "update_hive_posts_children_count.sql",
+      "update_hive_posts_api_helper.sql",
+      "database_api_list_comments.sql",
+      "database_api_list_votes.sql",
+      "update_posts_rshares.sql",
+      "update_hive_post_root_id.sql",
+      "condenser_get_by_replies_to_account.sql",
+      "condenser_get_by_account_comments.sql",
+      "condenser_get_by_blog_without_reblog.sql",
+      "condenser_get_by_feed_with_reblog.sql",
+      "condenser_get_by_blog.sql",
+      "bridge_get_account_posts_by_blog.sql",
+      "condenser_get_names_by_reblogged.sql",
+      "condenser_get_discussions_by_comments.sql",
+      "condenser_get_account_reputations.sql"
+
     ]
     from os.path import dirname, realpath
     dir_path = dirname(realpath(__file__))
