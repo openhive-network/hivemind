7c8f5e5e25e5bd170e1f947c2dc71df301a68b4a
ABW <andrzejl@syncad.com>
2020-11-05 17:24:51 +0100

    [ABW]: reimplemented get_followers/get_following as SQL functions, unified checks and paging

--- hive/db/sql_scripts/condenser_follows.sql
+++ hive/db/sql_scripts/condenser_follows.sql
@@ -13,3 +13,59 @@ BEGIN
 END
 $function$
 language plpgsql STABLE;
+
+DROP FUNCTION IF EXISTS condenser_get_followers;
+-- list of account names that follow/ignore given account
+CREATE FUNCTION condenser_get_followers( in _account VARCHAR, in _start VARCHAR, in _type INT, in _limit INT )
+RETURNS SETOF hive_accounts.name%TYPE
+AS
+$function$
+DECLARE
+  __account_id INT;
+  __start_id INT;
+BEGIN
+  __account_id = find_account_id( _account, True );
+  __start_id = find_account_id( _start, _start <> '' );
+  IF __start_id <> 0 THEN
+      SELECT INTO __start_id COALESCE( ( SELECT id FROM hive_follows WHERE following = __account_id AND follower = __start_id ), 0 );
+  END IF;
+  RETURN QUERY SELECT
+     ha.name
+  FROM
+     hive_follows hf
+     JOIN hive_accounts ha ON hf.follower = ha.id
+  WHERE
+     hf.following = __account_id AND hf.state = _type AND ( __start_id = 0 OR hf.id < __start_id )
+  ORDER BY hf.id DESC
+  LIMIT _limit;
+END
+$function$
+language plpgsql STABLE;
+
+DROP FUNCTION IF EXISTS condenser_get_following;
+-- list of account names followed/ignored by given account
+CREATE FUNCTION condenser_get_following( in _account VARCHAR, in _start VARCHAR, in _type INT, in _limit INT )
+RETURNS SETOF hive_accounts.name%TYPE
+AS
+$function$
+DECLARE
+  __account_id INT;
+  __start_id INT;
+BEGIN
+  __account_id = find_account_id( _account, True );
+  __start_id = find_account_id( _start, _start <> '' );
+  IF __start_id <> 0 THEN
+      SELECT INTO __start_id COALESCE( ( SELECT id FROM hive_follows WHERE follower = __account_id AND following = __start_id ), 0 );
+  END IF;
+  RETURN QUERY SELECT
+     ha.name
+  FROM
+     hive_follows hf
+     JOIN hive_accounts ha ON hf.following = ha.id
+  WHERE
+     hf.follower = __account_id AND hf.state = _type AND ( __start_id = 0 OR hf.id < __start_id )
+  ORDER BY hf.id DESC
+  LIMIT _limit;
+END
+$function$
+language plpgsql STABLE;
