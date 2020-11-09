04d6cc5df84732ba662e070528608046762e6a78
jsalyers <jsalyers@syncad.com>
2020-11-09 16:59:20 -0500

    [JES] Try to apply muting at the SQL level instead for more efficiency. This only covers the bridge methods at the moment

--- hive/db/sql_scripts/bridge_get_ranked_post_for_observer_communities.sql
+++ hive/db/sql_scripts/bridge_get_ranked_post_for_observer_communities.sql
@@ -9,7 +9,11 @@ DECLARE
 BEGIN
   __post_id = find_comment_id( _author, _permlink, True );
   __account_id = find_account_id( _observer, True );
-  RETURN QUERY SELECT
+  RETURN QUERY 
+    WITH muted_accounts AS (SELECT following as muted_account_id from hive_follows WHERE follower = __account_id AND state = 2 UNION
+                            SELECT hive_follows_indirect.following as muted_account_id FROM hive_follows hive_follows_direct JOIN hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower 
+                              WHERE hive_follows_direct.follower = __account_id AND hive_follows_direct.follow_muted AND hive_follows_indirect.state = 2)
+  SELECT
       hp.id,
       hp.author,
       hp.parent_author,
@@ -45,7 +49,8 @@ BEGIN
       hp.community_title,
       hp.role_id,
       hp.is_pinned,
-      hp.curator_payout_value
+      hp.curator_payout_value,
+      hp.is_muted
   FROM
       hive_posts_view hp
       JOIN hive_subscriptions hs ON hp.community_id = hs.community_id
@@ -56,7 +61,7 @@ BEGIN
   LIMIT _limit;
 END
 $function$
-language plpgsql STABLE;
+language plpgsql VOLATILE;
 
 DROP FUNCTION IF EXISTS bridge_get_ranked_post_by_hot_for_observer_communities;
 CREATE FUNCTION bridge_get_ranked_post_by_hot_for_observer_communities( in _observer VARCHAR, in _author VARCHAR, in _permlink VARCHAR, in _limit SMALLINT )
@@ -109,10 +114,12 @@ BEGIN
       hp.community_title,
       hp.role_id,
       hp.is_pinned,
-      hp.curator_payout_value
+      hp.curator_payout_value,
+      hp.is_muted
   FROM
       hive_posts_view hp
       JOIN hive_subscriptions hs ON hp.community_id = hs.community_id
+      LEFT JOIN muted_accounts ON hp.author_id = muted_accounts.muted_account_id 
   WHERE hs.account_id = __account_id AND NOT hp.is_paidout AND hp.depth = 0
       AND ( __post_id = 0 OR hp.sc_hot < __hot_limit OR ( hp.sc_hot = __hot_limit AND hp.id < __post_id ) )
       AND hp.author NOT IN (SELECT muted FROM muted_accounts_view WHERE observer = _observer)
@@ -120,7 +127,7 @@ BEGIN
   LIMIT _limit;
 END
 $function$
-language plpgsql STABLE;
+language plpgsql VOLATILE;
 
 DROP FUNCTION IF EXISTS bridge_get_ranked_post_by_payout_comments_for_observer_communities;
 CREATE FUNCTION bridge_get_ranked_post_by_payout_comments_for_observer_communities( in _observer VARCHAR,  in _author VARCHAR, in _permlink VARCHAR, in _limit SMALLINT )
@@ -137,7 +144,11 @@ BEGIN
       SELECT ( hp.payout + hp.pending_payout ) INTO __payout_limit FROM hive_posts hp WHERE hp.id = __post_id;
   END IF;
   __account_id = find_account_id( _observer, True );
-  RETURN QUERY SELECT
+  RETURN QUERY
+  WITH muted_accounts AS (SELECT following as muted_account_id from hive_follows WHERE follower = __account_id AND state = 2 UNION
+                            SELECT hive_follows_indirect.following as muted_account_id FROM hive_follows hive_follows_direct JOIN hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower 
+                              WHERE hive_follows_direct.follower = __account_id AND hive_follows_direct.follow_muted AND hive_follows_indirect.state = 2)
+  SELECT
       hp.id,
       hp.author,
       hp.parent_author,
@@ -173,7 +184,8 @@ BEGIN
       hp.community_title,
       hp.role_id,
       hp.is_pinned,
-      hp.curator_payout_value
+      hp.curator_payout_value,
+      hp.is_muted
   FROM
   (
       SELECT
@@ -193,7 +205,7 @@ BEGIN
   LIMIT _limit;
 END
 $function$
-language plpgsql STABLE;
+language plpgsql VOLATILE;
 
 DROP FUNCTION IF EXISTS bridge_get_ranked_post_by_payout_for_observer_communities;
 CREATE FUNCTION bridge_get_ranked_post_by_payout_for_observer_communities( in _observer VARCHAR, in _author VARCHAR, in _permlink VARCHAR, in _limit SMALLINT )
@@ -212,7 +224,11 @@ BEGIN
   END IF;
   __account_id = find_account_id( _observer, True );
   __head_block_time = head_block_time();
-  RETURN QUERY SELECT
+  RETURN QUERY
+  WITH muted_accounts AS (SELECT following as muted_account_id from hive_follows WHERE follower = __account_id AND state = 2 UNION
+                            SELECT hive_follows_indirect.following as muted_account_id FROM hive_follows hive_follows_direct JOIN hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower 
+                              WHERE hive_follows_direct.follower = __account_id AND hive_follows_direct.follow_muted AND hive_follows_indirect.state = 2)
+  SELECT
       hp.id,
       hp.author,
       hp.parent_author,
@@ -248,10 +264,12 @@ BEGIN
       hp.community_title,
       hp.role_id,
       hp.is_pinned,
-      hp.curator_payout_value
+      hp.curator_payout_value,
+      hp.is_muted
   FROM
       hive_posts_view hp
       JOIN hive_subscriptions hs ON hp.community_id = hs.community_id
+      LEFT JOIN muted_accounts ON hp.author_id = muted_accounts.muted_account_id
   WHERE hs.account_id = __account_id AND NOT hp.is_paidout AND hp.payout_at BETWEEN __head_block_time + interval '12 hours' AND __head_block_time + interval '36 hours'
       AND ( __post_id = 0 OR ( hp.payout + hp.pending_payout ) < __payout_limit OR ( ( hp.payout + hp.pending_payout ) = __payout_limit AND hp.id < __post_id ) )
       AND hp.author NOT IN (SELECT muted FROM muted_accounts_view WHERE observer = _observer)
@@ -259,7 +277,7 @@ BEGIN
   LIMIT _limit;
 END
 $function$
-language plpgsql STABLE;
+language plpgsql VOLATILE;
 
 DROP FUNCTION IF EXISTS bridge_get_ranked_post_by_promoted_for_observer_communities;
 CREATE FUNCTION bridge_get_ranked_post_by_promoted_for_observer_communities( in _observer VARCHAR, in _author VARCHAR, in _permlink VARCHAR, in _limit SMALLINT )
@@ -276,7 +294,11 @@ BEGIN
       SELECT hp.promoted INTO __promoted_limit FROM hive_posts hp WHERE hp.id = __post_id;
   END IF;
   __account_id = find_account_id( _observer, True );
-  RETURN QUERY SELECT
+  RETURN QUERY
+  WITH muted_accounts AS (SELECT following as muted_account_id from hive_follows WHERE follower = __account_id AND state = 2 UNION
+                            SELECT hive_follows_indirect.following as muted_account_id FROM hive_follows hive_follows_direct JOIN hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower 
+                              WHERE hive_follows_direct.follower = __account_id AND hive_follows_direct.follow_muted AND hive_follows_indirect.state = 2)
+  SELECT
       hp.id,
       hp.author,
       hp.parent_author,
@@ -312,10 +334,12 @@ BEGIN
       hp.community_title,
       hp.role_id,
       hp.is_pinned,
-      hp.curator_payout_value
+      hp.curator_payout_value,
+      hp.is_muted
   FROM
       hive_posts_view hp
       JOIN hive_subscriptions hs ON hp.community_id = hs.community_id
+      LEFT JOIN muted_accounts ON hp.author_id = muted_accounts.muted_account_id
   WHERE hs.account_id = __account_id AND NOT hp.is_paidout AND hp.promoted > 0
       AND ( __post_id = 0 OR hp.promoted < __promoted_limit OR ( hp.promoted = __promoted_limit AND hp.id < __post_id ) )
       AND hp.author NOT IN (SELECT muted FROM muted_accounts_view WHERE observer = _observer)
@@ -323,7 +347,7 @@ BEGIN
   LIMIT _limit;
 END
 $function$
-language plpgsql STABLE;
+language plpgsql VOLATILE;
 
 DROP FUNCTION IF EXISTS bridge_get_ranked_post_by_trends_for_observer_communities;
 CREATE OR REPLACE FUNCTION bridge_get_ranked_post_by_trends_for_observer_communities( in _observer VARCHAR, in _author VARCHAR, in _permlink VARCHAR, in _limit SMALLINT )
@@ -377,7 +401,8 @@ BEGIN
       hp.community_title,
       hp.role_id,
       hp.is_pinned,
-      hp.curator_payout_value
+      hp.curator_payout_value,
+      hp.is_muted
   FROM
   (
       SELECT
@@ -398,7 +423,7 @@ BEGIN
   LIMIT _limit;
 END
 $function$
-language plpgsql STABLE;
+language plpgsql VOLATILE;
 
 DROP FUNCTION IF EXISTS bridge_get_ranked_post_by_muted_for_observer_communities;
 CREATE FUNCTION bridge_get_ranked_post_by_muted_for_observer_communities( in _observer VARCHAR, in _author VARCHAR, in _permlink VARCHAR, in _limit SMALLINT )
@@ -415,7 +440,11 @@ BEGIN
       SELECT ( hp.payout + hp.pending_payout ) INTO __payout_limit FROM hive_posts hp WHERE hp.id = __post_id;
   END IF;
   __account_id = find_account_id( _observer, True );
-  RETURN QUERY SELECT
+  RETURN QUERY
+  WITH muted_accounts AS (SELECT following as muted_account_id from hive_follows WHERE follower = __account_id AND state = 2 UNION
+                            SELECT hive_follows_indirect.following as muted_account_id FROM hive_follows hive_follows_direct JOIN hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower 
+                              WHERE hive_follows_direct.follower = __account_id AND hive_follows_direct.follow_muted AND hive_follows_indirect.state = 2)
+  SELECT
       hp.id,
       hp.author,
       hp.parent_author,
@@ -451,11 +480,13 @@ BEGIN
       hp.community_title,
       hp.role_id,
       hp.is_pinned,
-      hp.curator_payout_value
+      hp.curator_payout_value,
+      hp.is_muted
   FROM
       hive_posts_view hp
       JOIN hive_subscriptions hs ON hp.community_id = hs.community_id
       JOIN hive_accounts_view ha ON ha.id = hp.author_id
+      LEFT JOIN muted_accounts ON hp.author_id = muted_accounts.muted_account_id
   WHERE hs.account_id = __account_id AND NOT hp.is_paidout AND ha.is_grayed AND ( hp.payout + hp.pending_payout ) > 0
       AND ( __post_id = 0 OR ( hp.payout + hp.pending_payout ) < __payout_limit OR ( ( hp.payout + hp.pending_payout ) = __payout_limit AND hp.id < __post_id ) )
       AND hp.author NOT IN (SELECT muted FROM muted_accounts_view WHERE observer = _observer)
@@ -463,4 +494,4 @@ BEGIN
   LIMIT _limit;
 END
 $function$
-language plpgsql STABLE;
+language plpgsql VOLATILE;
