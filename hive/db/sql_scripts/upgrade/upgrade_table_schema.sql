66ef5243f651aeb8e057ae2fc32a7d9ca97e3b98
jsalyers <jsalyers@syncad.com>
2020-11-10 19:03:48 -0500

    [JES] Create a view for getting the muted accounts. Not In/Not Exists/Left Join all have extremely similar processing times. Went with Not In for readibility

--- hive/db/sql_scripts/upgrade/upgrade_table_schema.sql
+++ hive/db/sql_scripts/upgrade/upgrade_table_schema.sql
@@ -357,3 +357,20 @@ CREATE INDEX IF NOT EXISTS hive_posts_promoted_id_idx ON hive_posts (promoted, i
   WHERE NOT is_paidout AND counter_deleted = 0
  ;
 
+ DROP VIEW IF EXISTS muted_accounts_view;
+ CREATE OR REPLACE VIEW muted_accounts_view AS
+ (
+   SELECT observer_accounts.name AS observer, following_accounts.name AS muted
+   FROM hive_follows JOIN hive_accounts following_accounts ON hive_follows.following = following_accounts.id
+                     JOIN hive_accounts observer_accounts ON hive_follows.follower = observer_accounts.id
+   WHERE hive_follows.state = 2
+
+   UNION
+
+   SELECT observer_accounts.name AS observer, following_accounts.name AS muted
+   FROM hive_follows hive_follows_direct JOIN hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower
+                                         JOIN hive_accounts following_accounts ON hive_follows_indirect.following = following_accounts.id
+                                         JOIN hive_accounts observer_accounts ON hive_follows_direct.follower = observer_accounts.id
+   WHERE hive_follows_direct.follow_muted AND hive_follows_indirect.state = 2
+ );
+
