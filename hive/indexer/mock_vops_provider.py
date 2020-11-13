b88b6bb286c00d67a5f2a7e810dc20e8e0f6cd47
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-11-13 11:38:20 +0100

    Fixes in mock_vops_provider. Added example for ops_by_block

--- hive/indexer/mock_vops_provider.py
+++ hive/indexer/mock_vops_provider.py
@@ -9,12 +9,18 @@ class MockVopsProvider(MockDataProvider):
     """ Data provider for test vops """
     @classmethod
     def load_block_data(cls, data_path):
+        #clear old data and prepare container
+        cls.block_data.clear()
+        cls.block_data['ops'] = {}
+        cls.block_data['ops_by_block'] = {}
+
         if os.path.isdir(data_path):
             log.warning("Loading mock virtual ops data from directory: {}".format(data_path))
             cls.add_block_data_from_directory(data_path)
         else:
             log.warning("Loading mock virtual ops data from file: {}".format(data_path))
             cls.add_block_data_from_file(data_path)
+        print(cls.block_data)
 
     @classmethod
     def add_block_data_from_directory(cls, dir_name):
@@ -55,33 +61,36 @@ class MockVopsProvider(MockDataProvider):
             if data:
                 ret['timestamp'] = data[0]['timestamp']
                 if 'ops' in ret:
-                    ret['ops'].extend(data)
+                    ret['ops'].extend([op['op'] for op in data])
                 else:
-                    ret['ops'] = data
+                    ret['ops'] = [op['op'] for op in data]
 
         if 'ops_by_block' in cls.block_data and block_num in cls.block_data['ops_by_block']:
             data = cls.block_data['ops_by_block'][block_num]
             if data:
                 ret['timestamp'] = data['timestamp']
                 if 'ops_by_block' in ret:
-                    ret['ops_by_block'].extend(data['ops'])
+                    ret['ops_by_block'].extend([ops['op'] for ops in data['ops']])
                 else:
-                    ret['ops_by_block'] = data
+                    ret['ops_by_block'] = [ops['op'] for ops in data['ops']]
         return ret
 
     @classmethod
     def add_mock_vops(cls, ret, from_block, end_block):
+        # dont do anyting when there is no block data
+        if not cls.block_data or (not cls.block_data['ops_by_block'] and not cls.block_data['ops']):
+            return
         for block_num in range(from_block, end_block):
             mock_vops = cls.get_block_data(block_num)
             if mock_vops:
                 if block_num in ret:
                     if 'ops_by_block' in mock_vops:
-                        ret[block_num]['ops'].extend(mock_vops['ops_by_block'][block_num]['ops'])
+                        ret[block_num]['ops'].extend(mock_vops['ops_by_block'])
                     if 'ops' in mock_vops:
-                        ret[block_num]['ops'].extend(mock_vops['ops'][block_num])
+                        ret[block_num]['ops'].extend(mock_vops['ops'])
                 else:
-                    if 'ops_by_block' in mock_vops:
-                        ret[block_num] = {'timestamp':mock_vops['timestamp'], "ops" : mock_vops['ops_by_block'][block_num]['ops']}
                     if 'ops' in mock_vops:
-                        ret[block_num] = {'timestamp':mock_vops['timestamp'], "ops" : mock_vops['ops'][block_num]}
+                        ret[block_num] = {'timestamp':mock_vops['timestamp'], "ops" : mock_vops['ops']}
+                    if 'ops_by_block' in mock_vops:
+                        ret[block_num] = {'timestamp':mock_vops['timestamp'], "ops" : mock_vops['ops_by_block']}
 
