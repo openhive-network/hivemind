99db14a9e65d3511e7ad5590b123473b0b269008
jsalyers <jsalyers@syncad.com>
2020-11-18 18:13:56 -0500

    [JES] Case when was actually slowing queries down. I'm still not sure I understand that but ok. Updated the functions to check for mutes on ids instead of strings.

--- hive/db/sql_scripts/bridge_get_ranked_post_for_communities.sql
+++ hive/db/sql_scripts/bridge_get_ranked_post_for_communities.sql
@@ -3,7 +3,11 @@ CREATE FUNCTION bridge_get_ranked_post_pinned_for_community( in _community VARCH
 RETURNS SETOF bridge_api_post
 AS
 $function$
-  SELECT
+DECLARE
+  __observer_id INT;
+BEGIN
+  __observer_id = find_account_id(_observer, False);
+  RETURN QUERY SELECT
       hp.id,
       hp.author,
       hp.parent_author,
@@ -45,11 +49,12 @@ $function$
       hive_posts_view hp
       JOIN hive_communities hc ON hc.id = hp.community_id
   WHERE hc.name = _community AND hp.is_pinned
-  AND (CASE WHEN _observer IS NOT NULL THEN NOT EXISTS (SELECT 1 FROM muted_accounts_view WHERE observer = _observer AND muted = hp.author) ELSE true END)
+  AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp.author_id))
   ORDER BY hp.id DESC
   LIMIT _limit;
+END
 $function$
-language sql STABLE;
+language plpgsql STABLE;
 
 DROP FUNCTION IF EXISTS bridge_get_ranked_post_by_trends_for_community;
 CREATE FUNCTION bridge_get_ranked_post_by_trends_for_community( in _community VARCHAR, in _author VARCHAR, in _permlink VARCHAR, in _limit SMALLINT, in _bridge_api BOOLEAN, in _observer VARCHAR )
@@ -115,7 +120,7 @@ BEGIN
       WHERE hc.name = _community AND hp1.counter_deleted = 0 AND NOT hp1.is_paidout AND hp1.depth = 0
          AND ( NOT _bridge_api OR NOT hp1.is_pinned ) -- concatenated with bridge_get_ranked_post_pinned_for_community when called for bridge_api
          AND ( __post_id = 0 OR hp1.sc_trend < __trending_limit OR ( hp1.sc_trend = __trending_limit AND hp1.id < __post_id ) )
-         AND (CASE WHEN __observer_id <> 0 THEN NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id) ELSE true END)
+         AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id))
       ORDER BY hp1.sc_trend DESC, hp1.id DESC
       LIMIT _limit
   ) as trends
@@ -189,7 +194,7 @@ BEGIN
           JOIN hive_communities hc ON hp1.community_id = hc.id
       WHERE hc.name = _community AND hp1.counter_deleted = 0 AND NOT hp1.is_paidout AND hp1.promoted > 0
           AND ( __post_id = 0 OR hp1.promoted < __promoted_limit OR ( hp1.promoted = __promoted_limit AND hp1.id < __post_id ) )
-          AND (CASE WHEN __observer_id <> 0 THEN NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id) ELSE true END)
+          AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id))
       ORDER BY hp1.promoted DESC, hp1.id DESC
       LIMIT _limit
   ) as promoted
@@ -265,7 +270,7 @@ BEGIN
           JOIN hive_communities hc ON hp1.community_id = hc.id
       WHERE hc.name = _community AND hp1.counter_deleted = 0 AND NOT hp1.is_paidout AND hp1.payout_at BETWEEN __head_block_time + interval '12 hours' AND __head_block_time + interval '36 hours'
           AND ( __post_id = 0 OR ( hp1.payout + hp1.pending_payout ) < __payout_limit OR ( ( hp1.payout + hp1.pending_payout ) = __payout_limit AND hp1.id < __post_id ) )
-          AND (CASE WHEN __observer_id <> 0 THEN NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id) ELSE true END)
+          AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id))
       ORDER BY ( hp1.payout + hp1.pending_payout ) DESC, hp1.id DESC
       LIMIT _limit
   ) as payout
@@ -339,7 +344,7 @@ BEGIN
           JOIN hive_communities hc ON hp1.community_id = hc.id
       WHERE hc.name = _community AND hp1.counter_deleted = 0 AND NOT hp1.is_paidout AND hp1.depth > 0
           AND ( __post_id = 0 OR ( hp1.payout + hp1.pending_payout ) < __payout_limit OR ( ( hp1.payout + hp1.pending_payout ) = __payout_limit AND hp1.id < __post_id ) )
-          AND (CASE WHEN __observer_id <> 0 THEN NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id) ELSE true END)
+          AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id))
       ORDER BY ( hp1.payout + hp1.pending_payout ) DESC, hp1.id DESC
       LIMIT _limit
   ) as payout
@@ -485,7 +490,7 @@ BEGIN
           JOIN hive_communities hc ON hp1.community_id = hc.id
       WHERE hc.name = _community AND hp1.counter_deleted = 0 AND NOT hp1.is_paidout AND hp1.depth = 0
           AND ( __post_id = 0 OR hp1.sc_hot < __hot_limit OR ( hp1.sc_hot = __hot_limit AND hp1.id < __post_id ) )
-          AND (CASE WHEN __observer_id <> 0 THEN NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id) ELSE true END)
+          AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id))
       ORDER BY hp1.sc_hot DESC, hp1.id DESC
       LIMIT _limit
   ) as hot
@@ -552,11 +557,10 @@ BEGIN
       FROM
           hive_posts hp1
           JOIN hive_communities hc ON hp1.community_id = hc.id
-          JOIN hive_accounts_view ha ON hp1.author_id = ha.id
       WHERE hc.name = _community AND hp1.counter_deleted = 0 AND hp1.depth = 0
           AND ( NOT _bridge_api OR NOT hp1.is_pinned ) -- concatenated with bridge_get_ranked_post_pinned_for_community when called for bridge_api
-          AND NOT ha.is_grayed AND ( __post_id = 0 OR hp1.id < __post_id )
-          AND (CASE WHEN __observer_id <> 0 THEN NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id) ELSE true END)
+          AND ( __post_id = 0 OR hp1.id < __post_id )
+          AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = hp1.author_id))
       ORDER BY hp1.id DESC
       LIMIT _limit
   ) as created
