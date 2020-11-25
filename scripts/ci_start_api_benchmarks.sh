2694f8eef1917bc68fcc82c4119b8dca29c2245d
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-11-25 16:53:21 +0100

    Change xdist lib to parallel

--- scripts/ci_start_api_benchmarks.sh
+++ scripts/ci_start_api_benchmarks.sh
@@ -14,7 +14,7 @@ ITERATIONS=$3
 for (( i=0; i<$ITERATIONS; i++ ))
 do
   echo About to run iteration $i
-  tox -e tavern-benchmark -- -W ignore::pytest.PytestDeprecationWarning -n auto --junitxml=../../../../benchmarks-$i.xml 
+  tox -e tavern-benchmark -- -W ignore::pytest.PytestDeprecationWarning --workers auto --junitxml=../../../../benchmarks-$i.xml 
   echo Done!
 done
 ./scripts/xml_report_parser.py . ./tests/tests_api/hivemind/tavern
