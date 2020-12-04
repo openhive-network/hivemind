91130af4cb56fc029eb0ab60b97f16f038222966
jsalyers <jsalyers@syncad.com>
2020-12-04 18:28:05 -0500

    [JES] Order by should go in the string_agg function

--- hive/db/sql_scripts/hive_blacklisted_accounts_by_observer_view.sql
+++ hive/db/sql_scripts/hive_blacklisted_accounts_by_observer_view.sql
@@ -12,11 +12,11 @@ UNION ALL
  SELECT observer_accounts.id AS observer_id,
     following_accounts.id AS blacklisted_id,
     following_accounts.name AS blacklisted_name,
-    string_agg(('blacklisted by '::text || (indirect_accounts.name)::text), ','::text) AS source
+    string_agg(('blacklisted by '::text || (indirect_accounts.name)::text), ','::text ORDER BY indirect_accounts.name) AS source
    FROM (((hive_follows hive_follows_direct
      JOIN hive_follows hive_follows_indirect ON ((hive_follows_direct.following = hive_follows_indirect.follower)))
      JOIN hive_accounts following_accounts ON ((hive_follows_indirect.following = following_accounts.id)))
      JOIN hive_accounts observer_accounts ON ((hive_follows_direct.follower = observer_accounts.id)))
      JOIN hive_accounts indirect_accounts ON ((hive_follows_indirect.follower = indirect_accounts.id))
   WHERE (hive_follows_direct.follow_blacklists AND hive_follows_indirect.blacklisted)
-  GROUP BY observer_accounts.id, following_accounts.id ORDER BY source;
+  GROUP BY observer_accounts.id, following_accounts.id;
