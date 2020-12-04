5edc256e503a6c5afad9042a7e75eff32087ce63
jsalyers <jsalyers@syncad.com>
2020-12-04 17:28:58 -0500

    [JES] Add ordering to blacklists for consistency

--- hive/db/sql_scripts/hive_blacklisted_accounts_by_observer_view.sql
+++ hive/db/sql_scripts/hive_blacklisted_accounts_by_observer_view.sql
@@ -8,7 +8,7 @@ SELECT observer_accounts.id AS observer_id,
      JOIN hive_accounts following_accounts ON ((hive_follows.following = following_accounts.id)))
      JOIN hive_accounts observer_accounts ON ((hive_follows.follower = observer_accounts.id)))
   WHERE hive_follows.blacklisted
-UNION
+UNION ALL
  SELECT observer_accounts.id AS observer_id,
     following_accounts.id AS blacklisted_id,
     following_accounts.name AS blacklisted_name,
@@ -19,4 +19,4 @@ UNION
      JOIN hive_accounts observer_accounts ON ((hive_follows_direct.follower = observer_accounts.id)))
      JOIN hive_accounts indirect_accounts ON ((hive_follows_indirect.follower = indirect_accounts.id))
   WHERE (hive_follows_direct.follow_blacklists AND hive_follows_indirect.blacklisted)
-  GROUP BY observer_accounts.id, following_accounts.id;
+  GROUP BY observer_accounts.id, following_accounts.id ORDER BY source;
