04a91aeb6b61a05c458181ffcf00d9aadaa2b535
Bartek Wrona <wrona@syncad.com>
2020-12-05 12:22:06 +0100

    Implemented additional checks to prevent SQL failures during processing of bad tags specified for post.

--- hive/indexer/posts.py
+++ hive/indexer/posts.py
@@ -100,8 +100,11 @@ class Posts(DbAdapterHolder):
             pass
 
         tags = []
+
         if md and 'tags' in md and isinstance(md['tags'], list):
-            tags = md['tags']
+            for tag in md['tags']:
+                if tag and isinstance(tag, str):
+                    tags.append(escape_characters(tag))
 
         sql = """
             SELECT is_new_post, id, author_id, permlink_id, post_category, parent_id, community_id, is_valid, is_muted, depth
