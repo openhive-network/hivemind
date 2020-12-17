82457563e7b199b3e05d0490e192db7c3b64dd21
Marcin Ickiewicz <mickiewicz@syncad.com>
2020-12-17 17:16:04 +0100

    notification cache use autoincrement id

--- hive/db/sql_scripts/notifications_api.sql
+++ hive/db/sql_scripts/notifications_api.sql
@@ -34,7 +34,7 @@ BEGIN
   WHERE ha.id = __account_id;
 
   --- Warning given account can have no last_read_at set, so lets fallback to the block limit to avoid comparison to NULL.
-  SELECT COALESCE((SELECT hb.num 
+  SELECT COALESCE((SELECT hb.num
                    FROM hive_blocks hb
                    WHERE hb.created_at <= __last_read_at
                    ORDER by hb.created_at desc
@@ -156,10 +156,10 @@ BEGIN
   END IF;
 
   INSERT INTO hive_notification_cache
-  (id, block_num, type_id, created_at, src, dst, dst_post_id, post_id, score, payload, community, community_title)
-  SELECT nv.id, nv.block_num, nv.type_id, nv.created_at, nv.src, nv.dst, nv.dst_post_id, nv.post_id, nv.score, nv.payload, nv.community, nv.community_title
+  (block_num, type_id, created_at, src, dst, dst_post_id, post_id, score, payload, community, community_title)
+  SELECT nv.block_num, nv.type_id, nv.created_at, nv.src, nv.dst, nv.dst_post_id, nv.post_id, nv.score, nv.payload, nv.community, nv.community_title
   FROM hive_raw_notifications_view nv
-  WHERE nv.block_num > __limit_block AND (_first_block_num IS NULL OR nv.block_num BETWEEN _first_block_num AND _last_block_num)
+  WHERE nv.block_num > __limit_block AND (_first_block_num IS NULL OR nv.block_num BETWEEN _first_block_num AND _last_block_num) ORDER BY nv.block_num, nv.id
   ;
 END
 $function$
