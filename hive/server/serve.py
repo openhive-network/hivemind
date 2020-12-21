e4bd5824602410437859114138c0c8aa31ff0916
Dariusz Kędzierski <dkedzierski@syncad.com>
2020-12-21 13:27:01 +0100

    Attach Sent-At to response headers only when Sent-At is present in request headers

--- hive/server/serve.py
+++ hive/server/serve.py
@@ -283,7 +283,7 @@ def run_server(conf):
 
     async def jsonrpc_handler(request):
         """Handles all hive jsonrpc API requests."""
-        sent_at = request.headers.get('Sent-At', str(perf_counter()))
+        sent_at = request.headers.get('Sent-At', None)
         request = await request.text()
         # debug=True refs https://github.com/bcb/jsonrpcserver/issues/71
         response = None
@@ -305,15 +305,17 @@ def run_server(conf):
                 "id" : -1
             }
             headers = {
-                'Access-Control-Allow-Origin': '*',
-                'Sent-At' : '{}'.format(sent_at)
+                'Access-Control-Allow-Origin': '*'
             }
+            if sent_at is not None:
+                headers['Sent-At'] = '{}'.format(sent_at)
             return web.json_response(error_response, status=200, headers=headers, dumps=decimal_serialize)
         if response is not None and response.wanted:
             headers = {
-                'Access-Control-Allow-Origin': '*',
-                'Sent-At' : '{}'.format(sent_at)
+                'Access-Control-Allow-Origin': '*'
             }
+            if sent_at is not None:
+                headers['Sent-At'] = '{}'.format(sent_at)
             return web.json_response(response.deserialized(), status=200, headers=headers, dumps=decimal_serialize)
         return web.Response()
 
