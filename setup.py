0b303684ba58d675c7bc9929acd28a1391e9c66b
Dariusz Kędzierski <dkedzierski@syncad.com>
2021-01-13 13:01:07 +0100

    Refactoring

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
 # coding=utf-8
 import sys
 import os
+import logging
 
 from setuptools import find_packages
 from setuptools import setup
 
 assert sys.version_info[0] == 3 and sys.version_info[1] >= 6, "hive requires Python 3.6 or newer"
 
-VERSION = '0.0.1'
-
+VERSION = 'notag'
+GIT_REVISION = 'nogitrev'
 class GitRevisionProvider(object):
     """ Static class to provide version and git revision information"""
+    logger = logging.getLogger('GitRevisionProvider')
 
-    @staticmethod
-    def provide_git_revision():
+    @classmethod
+    def is_git_sha(cls, s):
+        from regex import fullmatch
+        return fullmatch('^g[0-9a-f]{8}$', s) is not None
+
+    @classmethod
+    def get_git_revision(cls, s):
+        git_revision = str(GIT_REVISION)
+        if cls.is_git_sha(s):
+            git_revision = s.lstrip('g')
+        return git_revision
+
+    @classmethod
+    def get_commits_count(cls, s):
+        commits = None
+        try:
+            commits = int(s)
+        except:
+            pass
+        return commits
+
+    @classmethod
+    def provide_git_revision(cls):
         """ Evaluate version and git revision and save it to a version file
             Evaluation is based on VERSION variable and git describe if
             .git directory is present in tree.
@@ -21,48 +44,54 @@ class GitRevisionProvider(object):
             from get_distribution call
 
         """
+        version = str(VERSION)
+        git_revision = str(GIT_REVISION)
         if os.path.exists(".git"):
             from subprocess import check_output
             command = 'git describe --tags --long --dirty'
-            version = check_output(command.split()).decode('utf-8').strip()
-            # git describe -> tag-commits-sha-dirty
-            version = version.rstrip('-dirty')
-            version = version.lstrip('v')   
-            parts = version.split('-')
-            # git revision
-            git_revision = parts[-1]
-            git_revision = git_revision.lstrip('g')
-
-            # commits after given tag
-            commits = None
-            try:
-                commits = int(parts[-2])
-            except:
-                pass
-            
-            # version based on tag
-            version = ''.join(parts[:-1])
-            if commits is not None:
-                version = ''.join(parts[:-2])
-            # normalize rc to rcN for PEP 440 comp
-            version = version.lower()
-            if version.endswith('rc'):
-                version += '0'
-
-            GitRevisionProvider._save_version_file(version, git_revision)
-            return version, git_revision
+            version_string = check_output(command.split()).decode('utf-8').strip()
+            if version_string != 'fatal: No names found, cannot describe anything.':
+                # git describe -> tag-commits-sha-dirty
+                version_string = version_string.rstrip('-dirty')
+                version_string = version_string.lstrip('v')   
+                parts = version_string.split('-')
+                parts_len = len(parts)
+                # only tag or git sha
+                if parts_len == 1:
+                    if cls.is_git_sha(parts[0]):
+                        git_revision = parts[0]
+                        git_revision = git_revision.lstrip('g')
+                    else:
+                        version = parts[0]
+                if parts_len == 2:
+                    version = parts[0]
+                    git_revision = cls.get_git_revision(parts[1])
+                if parts_len > 2:
+                    # git sha
+                    git_revision = cls.get_git_revision(parts[-1])
+                    # commits after given tag
+                    commits = cls.get_commits_count(parts[-2])
+                    # version based on tag
+                    version = ''.join(parts[:-1])
+                    if commits is not None:
+                        version = ''.join(parts[:-2])
+                    # normalize rc to rcN for PEP 440 compatibility
+                    version = version.lower()
+                    if version.endswith('rc'):
+                        version += '0'
+            else:
+                cls.logger.warning("Git describe command failed for current git repository")
         else:
             from pkg_resources import get_distribution
             try:
                 version, git_revision = get_distribution("hivemind").version.split("+")
-                GitRevisionProvider._save_version_file(version, git_revision)
-                return version, git_revision
             except:
-                GitRevisionProvider._save_version_file(VERSION, "nogitrev")
-        return VERSION, "nogitrev"
+                cls.logger.warning("Unable to get version and git revision from package data")
+        cls._save_version_file(version, git_revision)
+        return version, git_revision
 
-    @staticmethod
-    def _save_version_file(hivemind_version, git_revision):
+    @classmethod
+    def _save_version_file(cls, hivemind_version, git_revision):
         """ Helper method to save version.py with current version and git_revision """
         with open("hive/version.py", 'w') as version_file:
             version_file.write("# generated by setup.py\n")
