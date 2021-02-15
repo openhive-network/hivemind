f1719c44bd2738837e6d0e768c47ddee02c65266
Pawel Maniora <pmaniora@syncad.com>
2021-02-15 10:29:48 +0100

    addind python server

---
+++ openApi/server/openapi_server/models/__init__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from openapi_server.models.community import Community
+from openapi_server.models.community_context import CommunityContext
+from openapi_server.models.community_context_request import CommunityContextRequest
+from openapi_server.models.community_context_request_params import CommunityContextRequestParams
+from openapi_server.models.community_request import CommunityRequest
+from openapi_server.models.community_request_params import CommunityRequestParams
+from openapi_server.models.error_message import ErrorMessage
+from openapi_server.models.error_message_error import ErrorMessageError
+from openapi_server.models.list_all_subscriptions_request import ListAllSubscriptionsRequest
+from openapi_server.models.list_all_subscriptions_request_params import ListAllSubscriptionsRequestParams
+from openapi_server.models.list_communites_request import ListCommunitesRequest
+from openapi_server.models.list_communites_request_params import ListCommunitesRequestParams
+from openapi_server.models.list_community import ListCommunity
+from openapi_server.models.list_community_roles_request import ListCommunityRolesRequest
+from openapi_server.models.list_community_roles_request_params import ListCommunityRolesRequestParams
+from openapi_server.models.list_pop_communites_request import ListPopCommunitesRequest
+from openapi_server.models.list_pop_communites_request_params import ListPopCommunitesRequestParams
+from openapi_server.models.list_subscribers_request import ListSubscribersRequest
+from openapi_server.models.list_subscribers_request_params import ListSubscribersRequestParams
