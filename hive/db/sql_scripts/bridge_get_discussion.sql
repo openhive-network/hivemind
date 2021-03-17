78282356b3a3ac4a97d1af6acd626da941b45d89
Paulina Czempiel <pczempiel@syncad.com>
2021-03-17 11:12:16 +0100

    children sql is_muted

--- hive/db/sql_scripts/bridge_get_discussion.sql
+++ hive/db/sql_scripts/bridge_get_discussion.sql
@@ -64,6 +64,7 @@ BEGIN
             JOIN child_posts ON children.parent_id = child_posts.id
             JOIN hive_accounts ON children.author_id = hive_accounts.id
             AND (NOT EXISTS (SELECT 1 FROM muted_accounts_by_id_view WHERE observer_id = __observer_id AND muted_id = children.author_id))
+            WHERE NOT children.is_muted
         )
         SELECT hp2.id, cp.source
         FROM hive_posts hp2
