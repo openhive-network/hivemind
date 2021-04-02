f6e2da25a02cf661383bb07b693f49e4e0166497
Paulina Czempiel <pczempiel@syncad.com>
2021-04-02 16:32:28 +0200

    muted in community set grayed flag as True

--- hive/server/bridge_api/objects.py
+++ hive/server/bridge_api/objects.py
@@ -35,12 +35,11 @@ def append_statistics_to_post(post, row, is_pinned):
         if row['role_id']:
             post['author_role'] = ROLES[row['role_id']]
             post['author_title'] = row['role_title']
-            post['stats']['gray'] = row['role_id'] < 0 
         else:
             post['author_role'] = 'guest'
             post['author_title'] = ''
 
-    post['stats']['gray'] = row['is_grayed'] or row['is_muted'] 
+    post['stats']['gray'] = row['is_grayed'] or row['is_muted'] or (row['role_id'] == -2)
     if is_pinned:
         post['stats']['is_pinned'] = True
     return post
