511e243f7364cb6e9dfa9e4e4c5cc4ae86bdb3b4
Paulina Czempiel <pczempiel@syncad.com>
2021-06-23 14:16:25 +0200

    bridge_api_list_subscribers created_at changed to TIMESTAMP WITHOUT TIME ZONE

--- hive/db/sql_scripts/bridge_list_subscribers.sql
+++ hive/db/sql_scripts/bridge_list_subscribers.sql
@@ -3,7 +3,7 @@ CREATE TYPE bridge_api_list_subscribers AS (
     name VARCHAR,
     role VARCHAR,
     title VARCHAR,
-    created_at VARCHAR(19)
+    created_at TIMESTAMP WITHOUT TIME ZONE
 );
 
 DROP FUNCTION IF EXISTS bridge_list_subscribers
@@ -22,17 +22,17 @@ DECLARE
     __last_id INT := find_subscription_id(_last, _community, True);
 BEGIN
     RETURN QUERY
-    SELECT ha.name, get_role_name(COALESCE(hr.role_id,0)), hr.title, hs.created_at::VARCHAR(19)
+    SELECT ha.name, get_role_name(COALESCE(hr.role_id,0)), hr.title, hs.created_at
     FROM hive_subscriptions hs
     LEFT JOIN hive_roles hr ON hs.account_id = hr.account_id
     AND hs.community_id = hr.community_id
     JOIN hive_accounts ha ON hs.account_id = ha.id
     WHERE hs.community_id = __community_id
     AND (__last_id = 0 OR (
-	hs.id < __last_id
+        hs.id < __last_id
         )
     )
-    ORDER BY hs.created_at DESC, hs.id ASC
+    ORDER BY hs.id DESC
     LIMIT _limit;
 END
 $function$
