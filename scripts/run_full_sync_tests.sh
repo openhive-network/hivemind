7df53cd832a9f0565a386365e558124c548071f8
pczempiel <pczempiel@syncad.com>
2021-07-19 11:44:29 +0200

    Change path in run_full_sync_tests script

--- scripts/run_full_sync_tests.sh
+++ scripts/run_full_sync_tests.sh
@@ -2,7 +2,7 @@
 
 # script to run tavern tests for full sync hivemind node
 
-export TAVERN_DIR="tests/tests_api/hivemind/tavern_full_sync"
+export TAVERN_DIR="./tests/api_tests/hivemind/tavern_full_sync"
 
 SCRIPT=$(readlink -f "$0")
 
