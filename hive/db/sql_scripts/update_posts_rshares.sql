15dd6c4bdb3b10299c9f46e394bb607ea3519fb3
Bartek Wrona <wrona@syncad.com>
2021-09-02 13:55:53 +0200

    Performance optimization of `update_posts_rshares` procedure.

--- hive/db/sql_scripts/update_posts_rshares.sql
+++ hive/db/sql_scripts/update_posts_rshares.sql
@@ -11,45 +11,81 @@ $BODY$
 BEGIN
 SET LOCAL work_mem='2GB';
 
-UPDATE hive_posts hp
-SET
-    abs_rshares = votes_rshares.abs_rshares
-  , vote_rshares = votes_rshares.rshares
-  , sc_hot = CASE hp.is_paidout WHEN True Then 0 ELSE calculate_hot( votes_rshares.rshares, hp.created_at) END
-  , sc_trend = CASE hp.is_paidout WHEN True Then 0 ELSE calculate_trending( votes_rshares.rshares, hp.created_at) END
-  , total_votes = votes_rshares.total_votes
-  , net_votes = votes_rshares.net_votes
-FROM
-  (
-    SELECT
-        hv.post_id
-      , SUM( hv.rshares ) as rshares
-      , SUM( ABS( hv.rshares ) ) as abs_rshares
-      , SUM( CASE hv.is_effective WHEN True THEN 1 ELSE 0 END ) as total_votes
-      , SUM( CASE
-              WHEN hv.rshares > 0 THEN 1
-              WHEN hv.rshares = 0 THEN 0
-              ELSE -1
-            END ) as net_votes
-    FROM hive_votes hv
-    WHERE ((_last_block - _first_block) > 10000) OR ((_last_block - _first_block) <= 10000 AND EXISTS
-      (
-        SELECT NULL
-        FROM hive_votes hv2
-        WHERE hv2.post_id = hv.post_id AND hv2.block_num BETWEEN _first_block AND _last_block
-      ))
-    GROUP BY hv.post_id
-  ) as votes_rshares
-WHERE hp.id = votes_rshares.post_id
-AND hp.counter_deleted = 0
-AND (
-  hp.abs_rshares != votes_rshares.abs_rshares
-  OR hp.vote_rshares != votes_rshares.rshares
-  OR hp.total_votes != votes_rshares.total_votes
-  OR hp.net_votes != votes_rshares.net_votes
-);
+IF (_last_block - _first_block) > 10000 THEN
+  UPDATE hive_posts hp
+  SET
+       abs_rshares = votes_rshares.abs_rshares
+      ,vote_rshares = votes_rshares.rshares
+      ,sc_hot = CASE hp.is_paidout WHEN True Then 0 ELSE calculate_hot( votes_rshares.rshares, hp.created_at) END
+      ,sc_trend = CASE hp.is_paidout WHEN True Then 0 ELSE calculate_trending( votes_rshares.rshares, hp.created_at) END
+      ,total_votes = votes_rshares.total_votes
+      ,net_votes = votes_rshares.net_votes
+  FROM
+    (
+      SELECT
+          hv.post_id
+        , SUM( hv.rshares ) as rshares
+        , SUM( ABS( hv.rshares ) ) as abs_rshares
+        , SUM( CASE hv.is_effective WHEN True THEN 1 ELSE 0 END ) as total_votes
+        , SUM( CASE
+                WHEN hv.rshares > 0 THEN 1
+                WHEN hv.rshares = 0 THEN 0
+                ELSE -1
+              END ) as net_votes
+      FROM hive_votes hv
+      GROUP BY hv.post_id
+    ) as votes_rshares
+  WHERE hp.id = votes_rshares.post_id
+  AND hp.counter_deleted = 0
+  AND (
+    hp.abs_rshares != votes_rshares.abs_rshares
+    OR hp.vote_rshares != votes_rshares.rshares
+    OR hp.total_votes != votes_rshares.total_votes
+    OR hp.net_votes != votes_rshares.net_votes
+  );
+ELSE
+  UPDATE hive_posts hp
+  SET
+      abs_rshares = votes_rshares.abs_rshares
+     ,vote_rshares = votes_rshares.rshares
+     ,sc_hot = CASE hp.is_paidout WHEN True Then 0 ELSE calculate_hot( votes_rshares.rshares, hp.created_at) END
+     ,sc_trend = CASE hp.is_paidout WHEN True Then 0 ELSE calculate_trending( votes_rshares.rshares, hp.created_at) END
+     ,total_votes = votes_rshares.total_votes
+     ,net_votes = votes_rshares.net_votes
+  FROM
+    (
+      SELECT
+          hv.post_id
+        , SUM( hv.rshares ) as rshares
+        , SUM( ABS( hv.rshares ) ) as abs_rshares
+        , SUM( CASE hv.is_effective WHEN True THEN 1 ELSE 0 END ) as total_votes
+        , SUM( CASE
+                WHEN hv.rshares > 0 THEN 1
+                WHEN hv.rshares = 0 THEN 0
+                ELSE -1
+              END ) as net_votes
+      FROM hive_votes hv
+      WHERE EXISTS
+        (
+          SELECT NULL
+          FROM hive_votes hv2
+          WHERE hv2.post_id = hv.post_id AND hv2.block_num BETWEEN _first_block AND _last_block
+        )
+      GROUP BY hv.post_id
+    ) as votes_rshares
+  WHERE hp.id = votes_rshares.post_id
+  AND hp.counter_deleted = 0
+  AND (
+    hp.abs_rshares != votes_rshares.abs_rshares
+    OR hp.vote_rshares != votes_rshares.rshares
+    OR hp.total_votes != votes_rshares.total_votes
+    OR hp.net_votes != votes_rshares.net_votes
+  );
+END IF;
+
 RESET work_mem;
 
 END;
+
 $BODY$
 ;
