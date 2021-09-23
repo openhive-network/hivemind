9b63196c72b81a1c745795c461c4432a9d8ab33a
Bartek Wrona <wrona@syncad.com>
2021-09-23 21:07:33 +0200

    - JIT optimization is disabled at database level, if Postgres 12 or newer is present - update_posts_rshares does not contain anymore JIT disabling code = db_upgrade procedure contains a code to disable JIT at database level.

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -525,9 +525,18 @@ def setup(db):
 
     sql = """
           DO $$
+          DECLARE
+          __version INT;
           BEGIN
+            SELECT CURRENT_SETTING('server_version_num')::INT INTO __version;
+
             EXECUTE 'ALTER DATABASE '||current_database()||' SET join_collapse_limit TO 16';
             EXECUTE 'ALTER DATABASE '||current_database()||' SET from_collapse_limit TO 16';
+
+            IF __version >= 120000 THEN
+              RAISE NOTICE 'Disabling a JIT optimization on the current database level...';
+              EXECUTE 'ALTER DATABASE '||current_database()||' SET jit TO False';
+            END IF;
           END
           $$;
           """
