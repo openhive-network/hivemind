4c0d0b0611e0e25ab29742de42459cba007f3fb4
Bartek Wrona <wrona@syncad.com>
2021-09-23 21:10:11 +0200

    Eliminated redundant JIT disable code.

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -454,9 +454,6 @@ def setup(db):
     sql = """SELECT * FROM pg_extension WHERE extname='intarray'"""
     assert db.query_row( sql ), "The database requires created 'intarray' extension"
 
-    # disable jit
-    db.query_no_return("ALTER DATABASE hive SET jit = FALSE")
-
     # initialize schema
     build_metadata().create_all(db.engine())
 
