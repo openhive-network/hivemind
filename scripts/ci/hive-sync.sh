4ad4b96f9327b0e4f81d7dbc57df0ed8dc7902e2
Mateusz Żebrak <mzebrak@syncad.com>
2022-03-31 11:17:16 +0200

    Make hive-sync.sh be able to run sync in both - haf and hived modes

--- scripts/ci/hive-sync.sh
+++ scripts/ci/hive-sync.sh
@@ -2,22 +2,25 @@
 
 set -euo pipefail
 
+SOURCE=$1
+
 # For debug only!
 RUNNER_HIVEMIND_SYNC_MAX_BLOCK=5000026
 # RUNNER_HIVED_URL='{"default":"http://hived-node:8091"}'
 # RUNNER_HIVED_URL='{"default":"http://172.17.0.1:8091"}'
 
-hive_sync() {
-    # Start hive sync process
+USER=${RUNNER_POSTGRES_APP_USER}:${RUNNER_POSTGRES_APP_USER_PASSWORD}
+OPTIONS="host=${RUNNER_POSTGRES_HOST}&port=${RUNNER_POSTGRES_PORT}"
+DATABASE_URL="postgresql://${USER}@/${HIVEMIND_DB_NAME}?${OPTIONS}"
+
+
+hived_sync() {
+    # Start hive sync process from hived
 
     cat << EOF
 Starting hive sync using hived url: ${RUNNER_HIVED_URL}.
 Max sync block is: ${RUNNER_HIVEMIND_SYNC_MAX_BLOCK}.
 EOF
-
-    USER=${RUNNER_POSTGRES_APP_USER}:${RUNNER_POSTGRES_APP_USER_PASSWORD}
-    OPTIONS="host=${RUNNER_POSTGRES_HOST}&port=${RUNNER_POSTGRES_PORT}"
-    DATABASE_URL="postgresql://${USER}@/${HIVEMIND_DB_NAME}?${OPTIONS}"
 
     hive sync \
         --log-mask-sensitive-data \
@@ -37,7 +40,47 @@ EOF
         --mock-vops-data-path mock_data/block_data/community_op/mock_vops_data_community.json \
         --community-start-block 4998000 \
         2>&1 | tee -i hivemind-sync.log
-
 }
 
-hive_sync
+haf_sync() {
+    # Start hive sync process from haf database
+
+    cat << EOF
+Starting hive sync using haf url: ${RUNNER_HIVED_URL}.
+Max sync block is: ${RUNNER_HIVEMIND_SYNC_MAX_BLOCK}.
+EOF
+
+    hive sync \
+        --log-mask-sensitive-data \
+        --pid-file hive_sync.pid \
+        --test-max-block=${RUNNER_HIVEMIND_SYNC_MAX_BLOCK} \
+        --test-profile=False \
+        --prometheus-port 11011 \
+        --database-url "${DATABASE_URL}" \
+        --hived-database-url "${HIVED_DB_URL}" \
+        --mock-block-data-path mock_data/block_data/follow_op/mock_block_data_follow.json \
+          mock_data/block_data/follow_op/mock_block_data_follow_tests.json \
+          mock_data/block_data/community_op/mock_block_data_community.json \
+          mock_data/block_data/reblog_op/mock_block_data_reblog.json \
+          mock_data/block_data/reblog_op/mock_block_data_reblog_delete.json \
+          mock_data/block_data/payments_op/mock_block_data_payments.json \
+          mock_data/block_data/notify_op/mock_block_data.json \
+        --mock-vops-data-path mock_data/block_data/community_op/mock_vops_data_community.json \
+        --community-start-block 4998000 \
+        2>&1 | tee -i hivemind-sync.log
+}
+
+main() {
+  echo SOURCE in hive-sync.sh is $SOURCE
+  if [ "$SOURCE" = "hived" ]; then
+    hived_sync
+  elif [ "$SOURCE" = "haf" ] || [ "$SOURCE" = "haf" ]; then
+    haf_sync
+  else
+    echo "Invalid SOURCE argument (Available modes: \"hived\", \"haf\")"
+    exit 1
+  fi
+}
+
+
+main
