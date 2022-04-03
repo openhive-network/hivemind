cc8fe338100bf11e98627959fd727b912ed267d2
Martin Lees <drov0@users.noreply.github.com>
2022-04-03 19:52:29 -0400

    Update SetRole/SetUserTitle so that you have to be subscribed to get it, also added a script to generate flow.txt and cleaned up communtiy_flow

--- hive/indexer/community.py
+++ hive/indexer/community.py
@@ -195,15 +195,15 @@ class Community:
         """Get user role within a specific community."""
 
         return (
-            DB.query_one(
-                """SELECT role_id FROM hive_roles
+                DB.query_one(
+                    """SELECT role_id FROM hive_roles
                                     WHERE community_id = :community_id
                                       AND account_id = :account_id
                                     LIMIT 1""",
-                community_id=community_id,
-                account_id=account_id,
-            )
-            or Role.guest.value
+                    community_id=community_id,
+                    account_id=account_id,
+                )
+                or Role.guest.value
         )
 
     @classmethod
@@ -369,24 +369,22 @@ class CommunityOp:
 
         # Account-level actions
         elif action == 'setRole':
-            DB.query(
-                """INSERT INTO hive_roles
-                               (account_id, community_id, role_id, created_at)
-                        VALUES (:account_id, :community_id, :role_id, :date)
-                            ON CONFLICT (account_id, community_id)
-                            DO UPDATE SET role_id = :role_id """,
+            subscribed = DB.query_one(
+                """SELECT * FROM set_community_role_or_title(:community_id, :account_id, :role_id, NULL::varchar, CAST(:date AS timestamp ))""",
                 **params,
             )
+            if not subscribed:
+                log.info("set role failed account '%s' must be subscribed to the community", params['account'])
+                return
             self._notify('set_role', payload=Role(self.role_id).name)
         elif action == 'setUserTitle':
-            DB.query(
-                """INSERT INTO hive_roles
-                               (account_id, community_id, title, created_at)
-                        VALUES (:account_id, :community_id, :title, :date)
-                            ON CONFLICT (account_id, community_id)
-                            DO UPDATE SET title = :title""",
+            subscribed = DB.query_one(
+                """SELECT * FROM set_community_role_or_title(:community_id, :account_id, NULL::integer , :title, CAST(:date AS timestamp ))""",
                 **params,
             )
+            if not subscribed:
+                log.info("set role failed account '%s' must be subscribed to the community", params['account'])
+                return
             self._notify('set_label', payload=self.title)
 
         # Post-level actions
