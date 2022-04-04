12a1883a49d25234711ff56cdba92bc3fd9b1af6
Martin Lees <drov0@users.noreply.github.com>
2022-04-03 23:26:00 -0400

    small fix

--- hive/db/sql_scripts/community_helpers.sql
+++ hive/db/sql_scripts/community_helpers.sql
@@ -9,12 +9,14 @@ declare
         __role_id SMALLINT;
 BEGIN
         SELECT id INTO __subscription_id FROM hive_subscriptions WHERE account_id = _account_id AND community_id = _community_id;
-        -- We allow setting the MUTED role even if you're not subscribed
-        if _role_id IS NOT NULL AND _role_id < 0 THEN
-            SELECT role_id INTO __role_id FROM hive_roles WHERE account_id = _account_id AND community_id = _community_id;
-            -- We don't allow setting a higher role than the current one if you aren't subscribed
-            IF __subscription_id IS NULL AND ((__role_id IS NOT NULL AND __role_id < _role_id ) OR __role_id IS NULL)  THEN
-                return false;
+        IF _role_id IS NOT NULL THEN
+            -- We allow setting the MUTED role even if you're not subscribed
+            IF _role_id > 0 THEN
+                SELECT role_id INTO __role_id FROM hive_roles WHERE account_id = _account_id AND community_id = _community_id;
+                -- We don't allow setting a higher role than the current one if you aren't subscribed
+                IF __subscription_id IS NULL AND ((__role_id IS NOT NULL AND __role_id < _role_id ) OR __role_id IS NULL)  THEN
+                    return false;
+                END IF;
             END IF;
         ELSE
             IF __subscription_id IS NULL THEN
