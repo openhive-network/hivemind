2dbeb7ef1bcbe3135362c2182881c0811d8924f7
Mateusz Żebrak <mzebrak@syncad.com>
2022-04-23 09:11:44 +0200

    Prepare new sync mode - from HAF database

--- hive/steem/signal.py
+++ hive/steem/signal.py
@@ -1,7 +1,7 @@
 """Hive signal handling."""
 
 import logging
-from signal import SIGINT
+from signal import SIGINT, signal, SIGTERM
 
 from atomic import AtomicLong
 
@@ -11,7 +11,23 @@ EXCEPTION_THROWN = AtomicLong(0)
 FINISH_SIGNAL_DURING_SYNC = AtomicLong(0)
 
 
-def finish_signals_handler(signal, frame):
+default_sigint_handler = None
+default_sigterm_handler = None
+
+
+def set_custom_signal_handlers():
+    global default_sigint_handler
+    global default_sigterm_handler
+    default_sigint_handler = signal(SIGINT, custom_signals_handler)
+    default_sigterm_handler = signal(SIGTERM, custom_signals_handler)
+
+
+def restore_default_signal_handlers():
+    signal(SIGINT, default_sigint_handler)
+    signal(SIGTERM, default_sigterm_handler)
+
+
+def custom_signals_handler(signal, frame):
     global FINISH_SIGNAL_DURING_SYNC
     FINISH_SIGNAL_DURING_SYNC += 1
     log.info(
