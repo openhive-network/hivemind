f8619e7742d7fcd660d733f2c3b60398c359c739
Bartek Wrona <wrona@syncad.com>
2022-05-05 22:13:02 +0200

    Preliminary try to run CI on HAF-based hivemind sync

--- scripts/ci/hive-sync.sh
+++ scripts/ci/hive-sync.sh
@@ -7,24 +7,28 @@ RUNNER_HIVEMIND_SYNC_MAX_BLOCK=5000024
 # RUNNER_HIVED_URL='{"default":"http://hived-node:8091"}'
 # RUNNER_HIVED_URL='{"default":"http://172.17.0.1:8091"}'
 
-hive_sync() {
-    # Start hive sync process
+USER=${RUNNER_POSTGRES_APP_USER}:${RUNNER_POSTGRES_APP_USER_PASSWORD}
+OPTIONS="host=${RUNNER_POSTGRES_HOST}&port=${RUNNER_POSTGRES_PORT}"
+DATABASE_URL="postgresql://${USER}@/${HIVEMIND_DB_NAME}?${OPTIONS}"
+
+haf_sync() {
+    # Start hive sync process from haf database
+
+    echo Removing hivemind context and table from HAF database
+    psql $RUNNER_HIVED_DB_URL -c "SELECT hive.app_remove_context('hivemind_app');" || true
 
     cat << EOF
-Starting hive sync using hived url: ${RUNNER_HIVED_URL}.
+Starting hive sync using haf url: ${RUNNER_HIVED_DB_URL}.
 Max sync block is: ${RUNNER_HIVEMIND_SYNC_MAX_BLOCK}.
 EOF
-
-    USER=${RUNNER_POSTGRES_APP_USER}:${RUNNER_POSTGRES_APP_USER_PASSWORD}
-    OPTIONS="host=${RUNNER_POSTGRES_HOST}&port=${RUNNER_POSTGRES_PORT}"
-    DATABASE_URL="postgresql://${USER}@/${HIVEMIND_DB_NAME}?${OPTIONS}"
 
     hive sync \
         --log-mask-sensitive-data \
         --pid-file hive_sync.pid \
         --test-max-block=${RUNNER_HIVEMIND_SYNC_MAX_BLOCK} \
+        --test-last-block-for-massive=${RUNNER_HIVEMIND_LAST_BLOCK_FOR_MASSIVE} \
         --test-profile=False \
-        --steemd-url "${RUNNER_HIVED_URL}" \
+        --hived-database-url "${RUNNER_HIVED_DB_URL}" \
         --prometheus-port 11011 \
         --database-url "${DATABASE_URL}" \
         --mock-block-data-path mock_data/block_data/follow_op/mock_block_data_follow.json \
@@ -37,7 +41,6 @@ EOF
         --mock-vops-data-path mock_data/block_data/community_op/mock_vops_data_community.json \
         --community-start-block 4998000 \
         2>&1 | tee -i hivemind-sync.log
-
 }
 
-hive_sync
+haf_sync
