c398a9f2f0c0b66818a17984fb155792d480e1f4
Mateusz Żebrak <mzebrak@syncad.com>
2022-05-14 14:14:17 +0200

    Move context-related functions to separate module

---
+++ hive/indexer/hive_db/haf_functions.py
@@ -0,0 +1,31 @@
+import logging
+
+from hive.conf import SCHEMA_NAME
+from hive.db.adapter import Db
+
+log = logging.getLogger(__name__)
+
+
+def prepare_app_context(db: Db) -> None:
+    log.info(f"Looking for '{SCHEMA_NAME}' context.")
+    ctx_present = db.query_one(f"SELECT hive.app_context_exists('{SCHEMA_NAME}') as ctx_present;")
+    if not ctx_present:
+        log.info(f"No application context present. Attempting to create a '{SCHEMA_NAME}' context...")
+        db.query_no_return(f"SELECT hive.app_create_context('{SCHEMA_NAME}');")
+        log.info("Application context creation done.")
+
+
+def context_detach(db: Db) -> None:
+    is_attached = db.query_one(f"SELECT hive.app_context_is_attached('{SCHEMA_NAME}')")
+    if is_attached:
+        log.info("Trying to detach app context...")
+        db.query_no_return(f"SELECT hive.app_context_detach('{SCHEMA_NAME}')")
+        log.info("App context detaching done.")
+    else:
+        log.info("No attached context - detach skipped.")
+
+
+def context_attach(db: Db, block_number: int) -> None:
+    log.info(f"Trying to attach app context with block number: {block_number}")
+    db.query_no_return(f"SELECT hive.app_context_attach('{SCHEMA_NAME}', {block_number})")
+    log.info("App context attaching done.")
