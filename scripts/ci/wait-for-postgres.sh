7160995fb89777fa2fd0a31524b50e006a8f8160
Bartek Wrona <wrona@syncad.com>
2022-05-22 22:54:38 +0200

    CI jobs shall wait for ready haf-instance (at least postgres database hosted there)

--- scripts/ci/wait-for-postgres.sh
+++ scripts/ci/wait-for-postgres.sh
@@ -1,63 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # wait-for-postgres.sh
-# Use in docker-compose:
-# command: ["./wait-for-postgres.sh", "name-of-postgres-service", "python", "app.py"]
 
-set -e
+LIMIT=120 #seconds
 
-LIMIT=10 #seconds
-
-HOST=$1
-PORT=$2
-
-if [ -z "$HOST" ]
-then
-    HOST="$RUNNER_POSTGRES_HOST"
-fi
-if [ -z "$PORT" ]
-then
-    PORT="$RUNNER_POSTGRES_PORT"
-fi
+CONTAINER_NAME=$1
 
 wait_for_postgres() {
     counter=0
-    echo "Waiting for postgres on ${HOST}:${PORT}."
-    while ! pg_isready \
-            --host $HOST \
-            --port $PORT \
-            --timeout=1 --quiet; do
-        counter=$((counter+1))
-        sleep 1
-        if [ $counter -eq $LIMIT ]; then
-            echo "Timeout reached, postgres is unavailable, exiting."
-            exit 1
-        fi
-    done
-}
+    echo "Waiting for postgres hosted by contaienr: ${CONTAINER_NAME}."
 
-output_configuration() {
-
-    mkdir -p pg-stats
-    DIR=$PWD/pg-stats
-
-    echo "Postgres is up (discovered after ${counter}s)."
-    echo "-------------------------------------------------"
-    echo "Postgres version and configuration"
-    echo "-------------------------------------------------"
-    psql --username "$RUNNER_POSTGRES_ADMIN_USER" \
-            --host "$HOST" \
-            --port $PORT \
-            --dbname postgres <<EOF
-SELECT version();
--- select name, setting, unit from pg_settings;
--- show all;
-\copy (select name, setting, unit from pg_settings) to '$DIR/pg_settings_on_start.csv' WITH CSV HEADER
-\q
-EOF
-    echo "-------------------------------------------------"
-
+    timeout $LIMIT bash -c "until docker exec $CONTAINER_NAME pg_isready ; do sleep 1 ; done"
 }
 
 wait_for_postgres
-output_configuration
+
