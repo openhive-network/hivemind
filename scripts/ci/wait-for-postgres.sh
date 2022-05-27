3647edb5fd8681eee23839d13d8bd74c925c12d2
Bartek Wrona <wrona@syncad.com>
2022-05-27 14:54:57 +0200

    Old Dockerfile renamed to Dockerfile.obsolete

--- scripts/ci/wait-for-postgres.sh
+++ scripts/ci/wait-for-postgres.sh
@@ -2,7 +2,7 @@
 
 # wait-for-postgres.sh
 
-LIMIT=120 #seconds
+LIMIT=300 #seconds
 
 CONTAINER_NAME=$1
 
@@ -10,7 +10,9 @@ wait_for_postgres() {
     counter=0
     echo "Waiting for postgres hosted by contaienr: ${CONTAINER_NAME}."
 
-    timeout $LIMIT bash -c "until docker exec $CONTAINER_NAME pg_isready ; do sleep 1 ; done"
+    #timeout $LIMIT bash -c "until docker exec $CONTAINER_NAME pg_isready ; do sleep 1 ; done"
+
+    timeout $LIMIT bash -c "until psql --host ${CONTAINER_NAME} -d haf_block_log -U haf_app_admin -c 'SELECT * FROM hive.contexts;'; do sleep 3 ; done"
 }
 
 wait_for_postgres
