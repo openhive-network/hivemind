0b1c99fa186b357d90fb4d8c1fbc0a1726cdf3b6
Mateusz Żebrak <mzebrak@syncad.com>
2022-06-14 11:47:50 +0200

    Fix failing tests - materialized views refresh was not commited

--- hive/server/common/payout_stats.py
+++ hive/server/common/payout_stats.py
@@ -10,9 +10,15 @@ log = logging.getLogger(__name__)
 
 class PayoutStats(DbAdapterHolder):
     @classmethod
-    def generate(cls):
+    def generate(cls, separate_transaction: bool = False):
         """Re-generate payout_stats_view."""
 
-        log.warning("Rebuilding payout_stats_view")
+        log.warning(f"Rebuilding payout_stats_view{' in separate transaction' if separate_transaction else ''}")
+
+        if separate_transaction:
+            cls.db.query_no_return("START TRANSACTION")
 
         cls.db.query_no_return(f"REFRESH MATERIALIZED VIEW CONCURRENTLY {SCHEMA_NAME}.payout_stats_view;")
+
+        if separate_transaction:
+            cls.db.query_no_return("COMMIT")
