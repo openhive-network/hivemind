39182502b1f454c4dd0c8e7c089669a86383d118
Mateusz Żebrak <mzebrak@syncad.com>
2022-06-24 14:36:29 +0200

    Close db connections used by massive blocks data provider

--- hive/indexer/hive_db/massive_blocks_data_provider.py
+++ hive/indexer/hive_db/massive_blocks_data_provider.py
@@ -81,7 +81,7 @@ class MassiveBlocksDataProviderHiveDb(BlocksProviderBase):
     _op_types_dictionary = {}
 
     class Databases:
-        def __init__(self, db_root: Db, conf: Conf, shared: bool = False):
+        def __init__(self, db_root: Db, shared: bool = False):
             self._shared = shared
             self._db_root = db_root
             self._db_operations = db_root.clone('hivemind_MassiveBlocksProvider_OperationsData')
@@ -91,8 +91,7 @@ class MassiveBlocksDataProviderHiveDb(BlocksProviderBase):
             assert self._db_operations
             assert self._db_blocks_data
 
-        def close(self):
-            self._db_root.close()
+        def close_cloned_databases(self):
             self._db_operations.close()
             self._db_blocks_data.close()
 
@@ -114,6 +113,7 @@ class MassiveBlocksDataProviderHiveDb(BlocksProviderBase):
         BlocksProviderBase.__init__(self)
 
         self._conf = conf
+        self._databases = databases
         self._db = databases.get_root()
         self._lbound = None
         self._ubound = None
@@ -171,6 +171,9 @@ class MassiveBlocksDataProviderHiveDb(BlocksProviderBase):
 
         if self._conf.get('test_max_block'):
             self._last_block_num_in_db = self._db.query_one(sql=NUMBER_OF_BLOCKS_QUERY)
+
+    def close_databases(self):
+        self._databases.close_cloned_databases()
 
     @staticmethod
     def _id_to_virtual_type(id_: int):
