9e58292dd1052a5aa8484f6c9a983ba6624712b8
Mateusz Żebrak <mzebrak@syncad.com>
2022-06-25 16:16:05 +0200

    Auto generate mock blocks after db blocks
    When we need to mock more blocks than HAf databse already have,
    we need to push these blocks to the hive.blocks table.

--- hive/indexer/mocking/populate_haf_with_mocked_data.py
+++ hive/indexer/mocking/populate_haf_with_mocked_data.py
@@ -8,6 +8,7 @@ import configargparse
 
 from hive.db.adapter import Db
 from hive.indexer.mocking.mock_block import BlockMock
+from hive.indexer.mocking.mock_block import BlockMockAfterDb
 from hive.indexer.mocking.mock_block_provider import MockBlockProvider
 from hive.indexer.mocking.mock_vops_provider import MockVopsProvider
 
@@ -56,6 +57,19 @@ def load_mock_data(mock_block_data_paths: Optional[List[str]] = None, mock_vops_
         MockVopsProvider.load_block_data(path)
 
 
+def create_mocked_blocks_after_haf_db_blocks(lbound: int, ubound: int):
+    log.info(f'Creating mocked blocks in db from {lbound} to {ubound}')
+    for block_number in range(lbound, ubound + 1):
+        block_data = MockBlockProvider.make_empty_block(block_num=block_number)
+        block = BlockMockAfterDb(
+            block_number=block_number,
+            hash=block_data['block_id'],
+            previous_hash=block_data['previous'],
+            created_at=block_data['timestamp'],
+        )
+        block.push()
+
+
 def main():
     args = init_argparse(sys.argv[1:])
 
@@ -76,7 +90,26 @@ def main():
         block = BlockMock(block_number=block_number, block_data=data, virtual_ops=vops)
         blocks.append(block)
 
-    for block in sorted(blocks, key=lambda b: b.block_number):
+    blocks.sort(key=lambda b: b.block_number)
+
+    last_block_in_db = db.query_row("SELECT * FROM hive.blocks ORDER BY num DESC LIMIT 1")
+    log.info(f'Last block in db: {last_block_in_db["num"]}')
+
+    last_block_num_to_mock = blocks[-1].block_number
+    log.info(f'Last block to mock: {last_block_num_to_mock}')
+
+    if last_block_num_to_mock > last_block_in_db['num']:
+        MockBlockProvider.set_last_real_block_num_date(
+            block_num=last_block_in_db['num'],
+            block_date=last_block_in_db['created_at'],
+            block_id=last_block_in_db['hash'],
+        )
+
+        create_mocked_blocks_after_haf_db_blocks(lbound=last_block_in_db['num'] + 1, ubound=last_block_num_to_mock)
+        Db.instance().query_no_return(f'SELECT hive.set_irreversible({last_block_num_to_mock})')
+        log.info(f'Irreversible block set to {last_block_num_to_mock}')
+
+    for block in blocks:
         log.info(f'####################################################### Block number: {block.block_number} STARTING')
 
         for transaction in block.get_next_transaction():
