09c487bdfb21a5b4f5f49c6db5590ae426b5b05f
Mateusz Żebrak <mzebrak@syncad.com>
2022-07-08 10:19:38 +0200

    Store last imported block date in DB

--- hive/db/sql_scripts/update_hive_blocks_consistency_flag.sql
+++ hive/db/sql_scripts/update_hive_blocks_consistency_flag.sql
@@ -1,6 +1,7 @@
 DROP FUNCTION IF EXISTS hivemind_app.update_last_imported_block;
 CREATE OR REPLACE FUNCTION hivemind_app.update_last_imported_block(
-    in _block_number INTEGER)
+    in _block_number INTEGER,
+    in _block_date TIMESTAMP)
     RETURNS VOID
     LANGUAGE 'plpgsql'
     VOLATILE
@@ -8,7 +9,7 @@ AS
 $BODY$
 BEGIN
     UPDATE hivemind_app.hive_state
-    SET last_imported_block_num = _block_number;
+    SET last_imported_block_num = _block_number, last_imported_block_date = _block_date;
 END
 $BODY$
 ;
