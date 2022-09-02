bb931d6ffb3acfa900a89e1ef33b8933a762b7a0
Mateusz Żebrak <mzebrak@syncad.com>
2022-09-02 13:26:37 +0200

    Determine version with versioningit tool
    This tool is similar to the setuptools-scm
    but offers more configuration options which
    were required in the hivemind use case.
    
    Needed:
    - File-based versioning (write to file)
    - Substitutions (commit sha)
    - Substitutions (commit timestamp)
    
    Good comparison is available here:
    https://setuptools-git-versioning.readthedocs.io/en/latest/differences.html

--- hive/version.py
+++ hive/version.py
@@ -1,31 +1,8 @@
 from datetime import datetime
-from datetime import timezone
 from typing import Final
 
-import git
+import hive._version as generated
 
-
-class VersionProvider:
-    """Static class to provide version and git revision information"""
-
-    repo = git.Repo(search_parent_directories=True)
-
-    @classmethod
-    def latest_tag_on_current_branch(cls) -> str:
-        version = cls.repo.git.describe('--tags', '--abbrev=0')
-        return version
-
-    @classmethod
-    def git_revision(cls, short: bool = False) -> str:
-        sha = cls.repo.head.object.hexsha
-        return sha[:8] if short else sha
-
-    @classmethod
-    def git_revision_datetime(cls) -> datetime:
-        git_datetime = cls.repo.head.object.committed_datetime
-        return git_datetime.astimezone(timezone.utc)
-
-
-VERSION: Final[str] = VersionProvider.latest_tag_on_current_branch()
-GIT_REVISION: Final[str] = VersionProvider.git_revision()
-GIT_DATE: Final[datetime] = VersionProvider.git_revision_datetime()
+VERSION: Final[str] = generated.__version__
+GIT_REVISION: Final[str] = generated.__git_revision__
+GIT_DATE: Final[datetime] = datetime.fromisoformat(generated.__git_revision_date__)
