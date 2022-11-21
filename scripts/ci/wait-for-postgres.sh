0d838f9a15426e771f7288d2685121247637e312
Bartek Wrona <wrona@syncad.com>
2022-11-21 17:24:49 +0100

    wait-for-postgres.sh uses dedicated SQL function exposed by HAF, to verify instance ready state.

--- scripts/ci/wait-for-postgres.sh
+++ scripts/ci/wait-for-postgres.sh
@@ -9,8 +9,8 @@ DATABASE_URL=$1
 wait_for_postgres() {
   echo "Waiting for postgres hosted by container at the URL: ${DATABASE_URL}."
 
-  timeout $LIMIT bash -c "until psql "${DATABASE_URL}" -c 'SELECT * FROM hive.contexts;'; do sleep 3 ; done"
-  timeout $LIMIT bash -c "until psql "${DATABASE_URL}" -c 'SELECT NOT EXISTS(SELECT 1 FROM hive.indexes_constraints);'; do sleep 3 ; done"
+  timeout $LIMIT bash -c "until psql \"${DATABASE_URL}\" -c \"SELECT hive.wait_for_ready_instance('4 MIN'::interval);\" ; do sleep 3 ; done"
+
 }
 
 wait_for_postgres
