fe6ded267aeb82b47d23bfcb1d893141adb96c5d
Bartek Wrona <wrona@syncad.com>
2022-11-21 23:38:43 +0100

    Old upgrade code (specific to previous releases) removed and new upgrade procedure adjusted to HAF requirements.

--- hive/db/sql_scripts/db_upgrade.sh
+++ hive/db/sql_scripts/db_upgrade.sh
@@ -3,11 +3,12 @@
 set -e 
 set -o pipefail 
 
-echo "Usage ./db_upgrade.sh <user-name> <db-name>"
+echo "Usage ./db_upgrade.sh <postgresql_url>"
 rm -f ./upgrade.log
 
-for sql in upgrade/assert_public_schema.sql \
-          postgres_handle_view_changes.sql \
+#upgrade/assert_public_schema.sql \
+
+for sql in postgres_handle_view_changes.sql \
           upgrade/upgrade_table_schema.sql \
           utility_functions.sql \
           hive_accounts_view.sql \
@@ -78,12 +79,12 @@ for sql in upgrade/assert_public_schema.sql \
           update_table_statistics.sql # Must be last
 
 do
-    echo Executing psql -U $1 -d $2 -f $sql
-    time psql -a -1 -v "ON_ERROR_STOP=1" -U $1 -d $2  -c '\timing' -f $sql 2>&1 | tee -a -i upgrade.log
+    echo Executing psql "$1" -f $sql
+    time psql -a -1 -v "ON_ERROR_STOP=1" "$1"  -c '\timing' -f $sql 2>&1 | tee -a -i upgrade.log
   echo $?
 done
 
-time psql -a -v "ON_ERROR_STOP=1" -U $1 -d $2  -c '\timing' -f upgrade/upgrade_runtime_migration.sql 2>&1 | tee -a -i upgrade.log
+time psql -a -v "ON_ERROR_STOP=1" "$1"  -c '\timing' -f upgrade/upgrade_runtime_migration.sql 2>&1 | tee -a -i upgrade.log
 
-time psql -a -v "ON_ERROR_STOP=1" -U $1 -d $2  -c '\timing' -f upgrade/do_conditional_vacuum.sql 2>&1 | tee -a -i upgrade.log
+time psql -a -v "ON_ERROR_STOP=1" "$1"  -c '\timing' -f upgrade/do_conditional_vacuum.sql 2>&1 | tee -a -i upgrade.log
 
