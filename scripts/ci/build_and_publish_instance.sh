c7d85d3f1dcc01354d2548570c13d5e9ae50f533
Konrad Botor <kbotor@syncad.com>
2023-01-20 14:41:53 +0100

    Added support for all PEP 440-compliant version numbers - ref hive#434

--- scripts/ci/build_and_publish_instance.sh
+++ scripts/ci/build_and_publish_instance.sh
@@ -10,13 +10,17 @@ docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
 
 pushd "$CI_PROJECT_DIR"
 
+# Turn PEP 440-compliant version number into a Docker-compliant tag
+#shellcheck disable=SC2001
+TAG=$(echo "$CI_COMMIT_TAG" | sed 's/[!+]/-/g')
+
 docker buildx build --platform=amd64 --progress=plain --target=instance \
   --build-arg CI_REGISTRY_IMAGE="$CI_REGISTRY_IMAGE/" \
-  --tag "${CI_REGISTRY_IMAGE}/instance:instance-${CI_COMMIT_TAG}" \
-  --tag "hiveio/hive:${CI_COMMIT_TAG}" \
+  --tag "${CI_REGISTRY_IMAGE}/instance:instance-${TAG}" \
+  --tag "hiveio/hive:${TAG}" \
   --file Dockerfile .
 
-docker push "${CI_REGISTRY_IMAGE}/instance:instance-${CI_COMMIT_TAG}"
-#docker push "hiveio/hive:${CI_COMMIT_TAG}"
+docker push "${CI_REGISTRY_IMAGE}/instance:instance-${TAG}"
+#docker push "hiveio/hive:${TAG}"
 
 popd
