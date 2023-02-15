41010b25b6caecdfe17365ee455f4e5e097e638e
Konrad Botor <kbotor@syncad.com>
2023-02-15 12:53:11 +0100

    Enabled Docker Hub integration in Docker image publishing job - ref. hive#434

--- scripts/ci/publish_instance.sh
+++ scripts/ci/publish_instance.sh
@@ -6,7 +6,7 @@
 set -e
 
 docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
-#docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
+docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
 
 INSTANCE_TAG="${CI_REGISTRY_IMAGE}/instance:instance-${CI_COMMIT_SHA}"
 docker pull "$INSTANCE_TAG"
@@ -20,4 +20,4 @@ docker tag "$INSTANCE_TAG" "hiveio/hivemind:${TAG}"
 docker images
 
 docker push "${CI_REGISTRY_IMAGE}/instance:instance-${TAG}"
-#docker push "hiveio/hivemind:${TAG}"
+docker push "hiveio/hivemind:${TAG}"
