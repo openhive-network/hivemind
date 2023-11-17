4bcea295311a287011a815d1ab2f27bae1627925
Martin Lees <drov0@users.noreply.github.com>
2023-11-17 18:08:10 -0500

    Fix payout issue

--- hive/indexer/posts.py
+++ hive/indexer/posts.py
@@ -271,7 +271,7 @@ class Posts(DbAdapterHolder):
                 if author is None:
                     author = value['author']
                     permlink = value['permlink']
-                payout = sbd_amount(value['payout'])
+                payout = value['payout']
                 author_rewards = value['author_rewards']
                 total_payout_value = value['total_payout_value']
                 curator_payout_value = value['curator_payout_value']
