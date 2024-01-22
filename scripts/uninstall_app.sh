ffa7d3425c2b624079133b0644882094d6a17553
Bartek Wrona <wrona@syncad.com>
2024-01-22 12:39:32 +0100

    Objects being owned by hivemind role can't be reassigned to postgres, since it prevents execution of successfull setup next time (since already existing objects are owned by sysadm)

--- scripts/uninstall_app.sh
+++ scripts/uninstall_app.sh
@@ -61,7 +61,6 @@ POSTGRES_ACCESS_ADMIN=${POSTGRES_URL:-"postgresql://$POSTGRES_USER@$POSTGRES_HOS
 uninstall_app() {
     psql "$POSTGRES_ACCESS_ADMIN" -v "ON_ERROR_STOP=ON" -c "do \$\$ BEGIN if hive.app_context_exists('hivemind_app') THEN perform hive.app_remove_context('hivemind_app'); end if; END \$\$"
     psql "$POSTGRES_ACCESS_ADMIN" -v "ON_ERROR_STOP=ON" -c "DROP SCHEMA IF EXISTS hivemind_app CASCADE;"
-    psql "$POSTGRES_ACCESS_ADMIN" -v "ON_ERROR_STOP=ON" -c "REASSIGN OWNED BY hivemind TO postgres; "
     psql "$POSTGRES_ACCESS_ADMIN" -v "ON_ERROR_STOP=ON" -c "DROP OWNED BY hivemind"
     psql "$POSTGRES_ACCESS_ADMIN" -v "ON_ERROR_STOP=ON" -c "DROP ROLE hivemind"
 }
