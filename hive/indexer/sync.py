fcf6adcd9bb41622c0eb87f7f70bb679e65c2b0a
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-02-06 13:01:32 +0100

    build_schema does not attch context

--- hive/indexer/sync.py
+++ hive/indexer/sync.py
@@ -64,7 +64,8 @@ class SyncHiveDb:
 
         self._check_log_explain_queries()
 
-        context_attach(db=self._db, block_number=Blocks.last_imported())
+        if self._enter_sync:
+            context_attach(db=self._db, block_number=Blocks.last_imported())
 
         Accounts.load_ids()  # prefetch id->name and id->rank memory maps
 
@@ -81,7 +82,8 @@ class SyncHiveDb:
         log.info(f'LAST IMPORTED BLOCK IS: {last_imported_block}')
         log.info(f'LAST COMPLETED BLOCK IS: {Blocks.last_completed()}')
 
-        context_attach(db=self._db, block_number=last_imported_block)
+        if self._enter_sync:
+            context_attach(db=self._db, block_number=last_imported_block)
 
         Blocks.close_own_db_access()
         if self._databases:
