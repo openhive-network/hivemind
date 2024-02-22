559be7c679bf4c4aed41a424eb2d8cacadb1fb9d
Konrad Botor <kbotor@syncad.com>
2024-02-22 11:19:42 +0100

    Added Docker labels

--- scripts/ci-helpers/build_instance.sh
+++ scripts/ci-helpers/build_instance.sh
@@ -83,8 +83,44 @@ else
   BUILD_OPTIONS+=("--load")
 fi
 
+BUILD_TIME="$(date -uIseconds)"
+
+GIT_COMMIT_SHA="$(git rev-parse HEAD || true)"
+if [ -z "$GIT_COMMIT_SHA" ]; then
+  GIT_COMMIT_SHA="[unknown]"
+fi
+
+GIT_CURRENT_BRANCH="$(git branch --show-current || true)"
+if [ -z "$GIT_CURRENT_BRANCH" ]; then
+  GIT_CURRENT_BRANCH="$(git describe --abbrev=0 --all | sed 's/^.*\///' || true)"
+  if [ -z "$GIT_CURRENT_BRANCH" ]; then
+    GIT_CURRENT_BRANCH="[unknown]"
+  fi
+fi
+
+GIT_LAST_LOG_MESSAGE="$(git log -1 --pretty=%B || true)"
+if [ -z "$GIT_LAST_LOG_MESSAGE" ]; then
+  GIT_LAST_LOG_MESSAGE="[unknown]"
+fi
+
+GIT_LAST_COMMITTER="$(git log -1 --pretty="%an <%ae>" || true)"
+if [ -z "$GIT_LAST_COMMITTER" ]; then
+  GIT_LAST_COMMITTER="[unknown]"
+fi
+
+GIT_LAST_COMMIT_DATE="$(git log -1 --pretty="%aI" || true)"
+if [ -z "$GIT_LAST_COMMIT_DATE" ]; then
+  GIT_LAST_COMMIT_DATE="[unknown]"
+fi
+
 docker buildx build "${BUILD_OPTIONS[@]}" \
   --build-context "runtime=docker-image://${REGISTRY}runtime:${CI_IMAGE_TAG}" \
+  --build-arg BUILD_TIME="$BUILD_TIME" \
+  --build-arg GIT_COMMIT_SHA="$GIT_COMMIT_SHA" \
+  --build-arg GIT_CURRENT_BRANCH="$GIT_CURRENT_BRANCH" \
+  --build-arg GIT_LAST_LOG_MESSAGE="$GIT_LAST_LOG_MESSAGE" \
+  --build-arg GIT_LAST_COMMITTER="$GIT_LAST_COMMITTER" \
+  --build-arg GIT_LAST_COMMIT_DATE="$GIT_LAST_COMMIT_DATE" \
   --tag "$TAG" \
   --file Dockerfile .
 
