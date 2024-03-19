e7e9c55a7556399258f22275ffcbe6b423a7a6fc
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-03-19 11:01:13 +0100

    [#235] use separate table to associate tags with posts
    make faster api bridge_get_ranked_post for tag

--- hive/db/sql_scripts/upgrade/upgrade_runtime_migration.sql
+++ hive/db/sql_scripts/upgrade/upgrade_runtime_migration.sql
@@ -2,3 +2,19 @@
 
 --- Must be at the end
 TRUNCATE TABLE hivemind_app.hive_db_data_migration;
+
+
+INSERT INTO hivemind_app.hive_post_tags(post_id, tag_id)
+SELECT id, unnest(tags_ids)
+FROM hivemind_app.hive_posts;
+
+CREATE INDEX IF NOT EXISTS hive_post_tags_idx
+ON hivemind_app.hive_post_tags USING btree(post_id, tag_id)
+TABLESPACE haf_tablespace
+;
+
+CREATE INDEX IF NOT EXISTS hive_post_tags_tag_id_post_id_idx
+ON hivemind_app.hive_post_tags USING btree(tag_id, post_id DESC)
+TABLESPACE haf_tablespace
+;
+
