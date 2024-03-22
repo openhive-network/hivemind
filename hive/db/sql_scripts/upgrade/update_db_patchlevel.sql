ddf0472607008c4e410a08c82bb83bb70efa40b7
Bartek Wrona <wrona@syncad.com>
2024-03-22 16:51:24 +0100

    Updated database version patching script to eliminate risk of UK violation related to level column

--- hive/db/sql_scripts/upgrade/update_db_patchlevel.sql
+++ hive/db/sql_scripts/upgrade/update_db_patchlevel.sql
@@ -1,15 +1,13 @@
 START TRANSACTION;
 
 insert into hivemind_app.hive_db_patch_level
-(level, patch_date, patched_to_revision)
-select ds.level, ds.patch_date, ds.patch_revision
+(patch_date, patched_to_revision)
+select ds.patch_date, ds.patch_revision
 from
 (
 values
- (33, now(), 'f5ee382637a1164192bf82b68a3412ee1d5b0e60') -- https://gitlab.syncad.com/hive/hivemind/-/merge_requests/574
-,(34, now(), '9d2cc15bea71a39139abdf49569e0eac6dd0b970') -- https://gitlab.syncad.com/hive/hivemind/-/merge_requests/575
-
-) ds (level, patch_date, patch_revision)
+ (now(), '2a6576357f9a0a55fafd7e6169f32ed65663cb50') -- https://gitlab.syncad.com/hive/hivemind/-/merge_requests/677
+) ds (patch_date, patch_revision)
 where not exists (select null from hivemind_app.hive_db_patch_level hpl where hpl.patched_to_revision = ds.patch_revision);
 
 COMMIT;
