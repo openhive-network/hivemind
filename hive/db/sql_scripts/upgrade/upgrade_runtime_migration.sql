8abdd6e6e231c1a83f470fb2004611e70f058704
Bartek Wrona <wrona@syncad.com>
2024-03-24 16:26:27 +0100

    Fixed upgrade_table_schema/upgrade_runtime_migration bugs related to hive_post_tags table data migration.

--- hive/db/sql_scripts/upgrade/upgrade_runtime_migration.sql
+++ hive/db/sql_scripts/upgrade/upgrade_runtime_migration.sql
@@ -2,14 +2,28 @@ SET ROLE hivemind;
 
 --- Put runtime data migration code here
 
-INSERT INTO hivemind_app.hive_post_tags(post_id, tag_id)
-SELECT id, unnest(tags_ids)
-FROM hivemind_app.hive_posts;
+WITH data_source AS
+(
+  SELECT p.id AS post_id, unnest(p.tags_ids) AS tag_id
+  FROM hivemind_app.hive_posts p
+  EXCEPT
+  SELECT pt.post_id, pt.tag_id
+  FROM hivemind_app.hive_post_tags pt
+)
+INSERT INTO hivemind_app.hive_post_tags
+(post_id, tag_id)
+SELECT ds.post_id, ds.tag_id
+FROM data_source ds
+;
 
 CREATE INDEX IF NOT EXISTS hive_post_tags_idx
 ON hivemind_app.hive_post_tags USING btree(post_id, tag_id)
 TABLESPACE haf_tablespace
 ;
+
+ALTER TABLE hivemind_app.hive_post_tags INHERIT hive.hivemind_app;
+
+ANALYZE VERBOSE hivemind_app.hive_post_tags;
 
 --- Must be at the end
 TRUNCATE TABLE hivemind_app.hive_db_data_migration;
--- hive/db/sql_scripts/upgrade/upgrade_table_schema.sql
+++ hive/db/sql_scripts/upgrade/upgrade_table_schema.sql
@@ -30,12 +30,16 @@ DROP INDEX IF EXISTS hive_posts_tags_ids_live_post_cond_idx;
 DROP INDEX IF EXISTS hive_posts_tags_ids_live_cond_idx;
 
 CREATE TABLE IF NOT EXISTS hivemind_app.hive_post_tags (
+    --- Column must be explicitly declared to satisfy further ALTER TABLE needed to INHERIT hive.hivemind_app table.
+    hive_rowid BIGINT NOT NULL DEFAULT nextval('hive.hivemind_app_hive_rowid_seq'::regclass),
+
     post_id INT NOT NULL
   , tag_id INT
   , CONSTRAINT hive_post_tags_fk1 FOREIGN KEY( post_id ) REFERENCES hivemind_app.hive_posts(id) DEFERRABLE
   , CONSTRAINT hive_post_tags_fk2 FOREIGN KEY( tag_id ) REFERENCES hivemind_app.hive_tag_data(id) DEFERRABLE
-) INHERITS(hive.hivemind_app);
+);
 
+--- This index is critical due to migration process which must be done incrementally
 CREATE UNIQUE INDEX IF NOT EXISTS hive_post_tags_tag_id_post_id_idx
     ON hivemind_app.hive_post_tags USING btree (tag_id, post_id DESC);
 
