d29a80d18035f35a6ad64d36b3805302d07e557c
Bartek Wrona <wrona@syncad.com>
2024-03-29 18:48:56 +0100

    Upgrade code should be executed conditionally to avoid errors when upgrade is not applicable to given version and should be skipped.

--- hive/db/sql_scripts/upgrade/upgrade_runtime_migration.sql
+++ hive/db/sql_scripts/upgrade/upgrade_runtime_migration.sql
@@ -2,6 +2,13 @@ SET ROLE hivemind;
 
 --- Put runtime data migration code here
 
+DO
+$$
+BEGIN
+
+IF EXISTS (SELECT column_name FROM information_schema.columns WHERE table_name='hivemind_app.hive_posts' and column_name='tags_ids') THEN
+  RAISE NOTICE 'Performing TAG-IDs migration';
+
 WITH data_source AS
 (
   SELECT p.id AS post_id, unnest(p.tags_ids) AS tag_id
@@ -24,6 +31,12 @@ TABLESPACE haf_tablespace
 ALTER TABLE hivemind_app.hive_post_tags INHERIT hive.hivemind_app;
 
 ANALYZE VERBOSE hivemind_app.hive_post_tags;
+ELSE
+  RAISE NOTICE 'TAG-IDs migration skipped';
+END IF;
+END
+$$
+;
 
 --- Must be at the end
 TRUNCATE TABLE hivemind_app.hive_db_data_migration;
