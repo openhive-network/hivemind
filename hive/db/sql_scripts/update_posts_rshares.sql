0ea6f3bff5065dc075055cf5c0a93abd6b2e0d8a
Michal Zander <mzander@syncad.com>
2024-07-25 08:54:08 +0000

    Add CTE in update_posts_rshares

--- hive/db/sql_scripts/update_posts_rshares.sql
+++ hive/db/sql_scripts/update_posts_rshares.sql
@@ -12,6 +12,21 @@ BEGIN
 SET LOCAL work_mem='2GB';
 
 IF (_last_block - _first_block) > 10000 THEN
+  WITH votes_rshares_view AS MATERIALIZED
+  (
+    SELECT
+      hv.post_id
+    , SUM( hv.rshares ) as rshares
+    , SUM( ABS( hv.rshares ) ) as abs_rshares
+    , SUM( CASE hv.is_effective WHEN True THEN 1 ELSE 0 END ) as total_votes
+    , SUM( CASE
+            WHEN hv.rshares > 0 THEN 1
+            WHEN hv.rshares = 0 THEN 0
+            ELSE -1
+          END ) as net_votes
+    FROM hivemind_app.hive_votes hv
+    GROUP BY hv.post_id
+  )
   UPDATE hivemind_app.hive_posts hp
   SET
        abs_rshares = votes_rshares.abs_rshares
@@ -20,21 +35,7 @@ IF (_last_block - _first_block) > 10000 THEN
       ,sc_trend = CASE hp.is_paidout WHEN True Then 0 ELSE hivemind_app.calculate_trending( votes_rshares.rshares, hp.created_at) END
       ,total_votes = votes_rshares.total_votes
       ,net_votes = votes_rshares.net_votes
-  FROM
-    (
-      SELECT
-          hv.post_id
-        , SUM( hv.rshares ) as rshares
-        , SUM( ABS( hv.rshares ) ) as abs_rshares
-        , SUM( CASE hv.is_effective WHEN True THEN 1 ELSE 0 END ) as total_votes
-        , SUM( CASE
-                WHEN hv.rshares > 0 THEN 1
-                WHEN hv.rshares = 0 THEN 0
-                ELSE -1
-              END ) as net_votes
-      FROM hivemind_app.hive_votes hv
-      GROUP BY hv.post_id
-    ) as votes_rshares
+  FROM votes_rshares_view votes_rshares
   WHERE hp.id = votes_rshares.post_id
   AND hp.counter_deleted = 0
   AND (
