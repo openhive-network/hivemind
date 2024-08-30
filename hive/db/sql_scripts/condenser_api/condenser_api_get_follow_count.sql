cf10ade943b9369eb460ced8d6d140db4ba769bd
asuch <asuch@syncad.com>
2024-08-30 15:19:23 +0200

    Add hivemind_endpoints.condenser_api_get_follow_count

---
+++ hive/db/sql_scripts/condenser_api/condenser_api_get_follow_count.sql
@@ -0,0 +1,17 @@
+DROP FUNCTION IF EXISTS hivemind_endpoints.condenser_api_get_follow_count;
+CREATE OR REPLACE FUNCTION hivemind_endpoints.condenser_api_get_follow_count(IN _account TEXT)
+RETURNS JSON
+LANGUAGE 'plpgsql'
+AS
+$$
+DECLARE
+  _account_id INT;
+BEGIN
+  _account_id = hivemind_utilities.find_account_id(hivemind_utilities.valid_account(_account, False), True);
+  RETURN (
+    SELECT to_json(row) FROM (
+      SELECT name AS account, ha.following AS following_count, ha.followers AS follower_count FROM hivemind_app.hive_accounts ha WHERE ha.id = _account_id
+    ) row );
+END;
+$$
+;
