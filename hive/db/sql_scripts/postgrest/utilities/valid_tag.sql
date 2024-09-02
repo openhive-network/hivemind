3e046fbb0da33d4a5c6bfc91c8d4126b0fe1885b
asuch <asuch@syncad.com>
2024-09-02 22:24:36 +0200

    hivemind_utilities.valid_tag

---
+++ hive/db/sql_scripts/postgrest/utilities/valid_tag.sql
@@ -0,0 +1,21 @@
+DROP FUNCTION IF EXISTS hivemind_utilities.valid_tag;
+CREATE OR REPLACE FUNCTION hivemind_utilities.valid_tag(in _tag TEXT, _allow_empty BOOLEAN DEFAULT FALSE)
+RETURNS TEXT
+LANGUAGE plpgsql
+STABLE
+AS
+$BODY$
+BEGIN
+  IF _tag IS NULL OR _tag = '' THEN
+    IF NOT _allow_empty THEN
+      RAISE EXCEPTION '%', hivemind_utilities.raise_parameter_validation_exception('tag was blank');
+    END IF;
+    RETURN _tag;
+  END IF;
+  IF NOT _tag ~ '^[a-z0-9_]' THEN
+    RAISE EXCEPTION '%', hivemind_utilities.raise_parameter_validation_exception('invalid tag `' || _tag || '`');
+  END IF;
+  RETURN _tag;
+END;
+$BODY$
+;
