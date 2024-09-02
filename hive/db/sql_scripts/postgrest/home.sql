d617b35d7dbf876532ffe971c88fe192d907afa3
asuch <asuch@syncad.com>
2024-09-02 14:40:10 +0200

    Instead of json_type text variable, use flag if parameters are in object or array type.

--- hive/db/sql_scripts/postgrest/home.sql
+++ hive/db/sql_scripts/postgrest/home.sql
@@ -15,7 +15,7 @@ DECLARE
   __api_type TEXT;
   __method_type TEXT;
   __is_legacy_style BOOLEAN;
-  __json_type TEXT;
+  __json_type_is_object BOOLEAN;
   __exception_message TEXT;
   __exception JSONB;
 BEGIN
@@ -37,24 +37,30 @@ BEGIN
     __api_type = __params->>0;
     __method_type = __params->>1;
     __params = __params->>2;
-    __json_type = json_typeof(__params);
   ELSE
     SELECT substring(__method FROM '^[^.]+') INTO __api_type;
     SELECT substring(__method FROM '[^.]+$') INTO __method_type;
-    __json_type = json_typeof(__params);
+  END IF;
+
+  IF json_typeof(__params) = 'object' THEN
+    __json_type_is_object = True;
+  ELSEIF json_typeof(__params) = 'array' THEN
+    __json_type_is_object = False;
+  ELSE
+    RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_json_format_exception('Invalid JSON format:' || json_typeof(__params)::text);
   END IF;
 
   __is_legacy_style := __api_type = 'condenser_api';
 
   IF __api_type = 'condenser_api' THEN
     IF __method_type = 'get_follow_count' THEN
-      PERFORM hivemind_utilities.validate_json_parameters(__json_type, __params, '{"account":"string"}', 1, 1, '["string"]');
-      SELECT hivemind_endpoints.condenser_api_get_follow_count(_account => hivemind_utilities.parse_argument_from_json(__params, __json_type, 'account', 0, True)) INTO __result;
+      PERFORM hivemind_utilities.validate_json_parameters(__json_type_is_object, __params, '{"account":"string"}', 1, 1, '["string"]');
+      SELECT hivemind_endpoints.condenser_api_get_follow_count(_account => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'account', 0, True)) INTO __result;
 
     ELSEIF __method_type = 'get_reblogged_by' THEN
-      PERFORM hivemind_utilities.validate_json_parameters(__json_type, __params, '{"author":"string","permlink":"string"}', 2, 2, '["string","string"]');
-      SELECT hivemind_endpoints.condenser_api_get_reblogged_by(_author => hivemind_utilities.parse_argument_from_json(__params, __json_type, 'author', 0, True),
-                                                               _permlink => hivemind_utilities.parse_argument_from_json(__params, __json_type, 'permlink', 1, True)) INTO __result;
+      PERFORM hivemind_utilities.validate_json_parameters(__json_type_is_object, __params, '{"author":"string","permlink":"string"}', 2, 2, '["string","string"]');
+      SELECT hivemind_endpoints.condenser_api_get_reblogged_by(_author => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'author', 0, True),
+                                                               _permlink => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'permlink', 1, True)) INTO __result;
     END IF;
   END IF;
 
--- hive/db/sql_scripts/postgrest/utilities/parse_argument_from_json.sql
+++ hive/db/sql_scripts/postgrest/utilities/parse_argument_from_json.sql
@@ -1,27 +1,20 @@
-DROP FUNCTION IF EXISTS hivemind_utilities.parse_argument_from_json;
-CREATE FUNCTION hivemind_utilities.parse_argument_from_json(_params JSON, _json_type TEXT, _arg_name TEXT, _arg_number INT, _exception_on_unset_field BOOLEAN, _is_bool BOOLEAN = FALSE)
+-- at the moment all type checks are performed by hivemind_utilities.validate_json_parameters. So it shouldn't be necessary to check again type of passed parameter.
+
+DROP FUNCTION IF EXISTS hivemind_utilities.parse_string_argument_from_json;
+CREATE FUNCTION hivemind_utilities.parse_string_argument_from_json(_params JSON, _json_is_object BOOLEAN, _arg_name TEXT, _arg_number INT, _exception_on_unset_field BOOLEAN, _is_bool BOOLEAN = FALSE)
 RETURNS TEXT
 LANGUAGE 'plpgsql'
 AS
 $$
-DECLARE
-  __param TEXT;
 BEGIN
-  IF _exception_on_unset_field AND (_json_type = 'object' AND _params->>_arg_name IS NULL OR _json_type = 'array' AND _params->>_arg_number IS NULL) THEN
+  IF _exception_on_unset_field AND (_json_is_object AND _params->>_arg_name IS NULL OR NOT _json_is_object AND _params->>_arg_number IS NULL) THEN
       RAISE EXCEPTION '%', hivemind_utilities.raise_missing_required_argument_exception(_arg_name);
   END IF;
 
-  SELECT CASE WHEN _json_type = 'object' THEN
-    _params->>_arg_name
+  IF _json_is_object THEN
+    RETURN _params->>_arg_name;
   ELSE
-    _params->>_arg_number
-  END INTO __param;
-
-  -- TODO: this is done to replicate behaviour of HAfAH python, might remove
-  IF _is_bool IS TRUE AND __param ~ '([A-Z].+)' THEN
-    RAISE invalid_text_representation;
-  ELSE
-    RETURN __param;
+    RETURN _params->>_arg_number;
   END IF;
 END
 $$
