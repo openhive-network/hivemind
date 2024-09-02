33211b982a295a88525608ee51b4a6c6daa3e754
asuch <asuch@syncad.com>
2024-09-02 22:22:39 +0200

    Added hivemind_utilities.parse_integer_argument_from_json

--- hive/db/sql_scripts/postgrest/utilities/parse_argument_from_json.sql
+++ hive/db/sql_scripts/postgrest/utilities/parse_argument_from_json.sql
@@ -39,3 +39,43 @@ BEGIN
 END
 $$
 ;
+
+DROP FUNCTION IF EXISTS hivemind_utilities.parse_integer_argument_from_json;
+CREATE FUNCTION hivemind_utilities.parse_integer_argument_from_json(_params JSON, _json_is_object BOOLEAN, _arg_name TEXT, _arg_number INT, _exception_on_unset_field BOOLEAN)
+RETURNS INTEGER
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  IF _exception_on_unset_field THEN
+    IF _json_is_object THEN
+      IF _params->>_arg_name IS NULL THEN
+        RAISE EXCEPTION '%', hivemind_utilities.raise_missing_required_argument_exception(_arg_name);
+      ELSE
+        RETURN _params->>_arg_name;
+      END IF;
+    ELSE
+      IF _params->>_arg_number IS NULL THEN
+        RAISE EXCEPTION '%', hivemind_utilities.raise_missing_required_argument_exception(_arg_name);
+      ELSE
+        RETURN _params->>_arg_number;
+      END IF;
+    END IF;
+  ELSE
+    IF _json_is_object THEN
+      IF _params->>_arg_name IS NULL THEN
+        RETURN NULL;
+      ELSE
+        RETURN _params->>_arg_name;
+      END IF;
+    ELSE
+      IF _params->>_arg_number IS NULL THEN
+        RETURN NULL;
+      ELSE
+        RETURN _params->>_arg_number;
+      END IF;
+    END IF;
+  END IF;
+END
+$$
+;
