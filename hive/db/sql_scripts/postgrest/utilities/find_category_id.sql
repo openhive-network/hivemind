1d43bd4e78ba5499b1596fa0763f38b0178d082e
asuch <asuch@syncad.com>
2024-09-02 22:24:58 +0200

    Add hivemind_utilities.find_category_id

---
+++ hive/db/sql_scripts/postgrest/utilities/find_category_id.sql
@@ -0,0 +1,22 @@
+DROP FUNCTION IF EXISTS hivemind_utilities.find_category_id CASCADE;
+CREATE OR REPLACE FUNCTION hivemind_utilities.find_category_id(
+    in _category_name hivemind_app.hive_category_data.category%TYPE,
+    in _allow_empty BOOLEAN
+)
+RETURNS INTEGER
+LANGUAGE 'plpgsql' STABLE
+AS
+$function$
+DECLARE
+  _category_id INT = 0;
+BEGIN
+  IF (_category_name <> '') THEN
+    SELECT INTO _category_id COALESCE( ( SELECT id FROM hivemind_app.hive_category_data WHERE category=_category_name ), 0 );
+    IF _allow_empty AND _category_id = 0 THEN
+      RAISE EXCEPTION '%', hivemind_utilities.raise_category_not_exists_exception(_category_name);
+    END IF;
+  END IF;
+  RETURN _category_id;
+END
+$function$
+;
