5940dd13b46fe8fbdd8a71b43ab7d0b5ff46eddf
asuch <asuch@syncad.com>
2024-09-05 13:38:23 +0200

    Update hivemind_utilities.validate_json_parameters in order to match responses to patterns

--- hive/db/sql_scripts/postgrest/home.sql
+++ hive/db/sql_scripts/postgrest/home.sql
@@ -14,7 +14,7 @@ DECLARE
   __result JSON;
   __api_type TEXT;
   __method_type TEXT;
-  __json_type_is_object BOOLEAN;
+  __json_with_params_is_object BOOLEAN;
   __exception_message TEXT;
   __exception JSONB;
 BEGIN
@@ -29,42 +29,46 @@ BEGIN
     RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_json_format_exception('Invalid JSON-RPC');
   END IF;
 
-  if lower(__method) = 'call' and json_typeof(__params) = 'array' THEN
+  if lower(__method) = 'call' THEN
     if json_array_length(__params) < 2 THEN
       RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_json_format_exception('Invalid JSON-RPC');
     END IF;
     __api_type = __params->>0;
     __method_type = __params->>1;
     __params = __params->>2;
+    __json_with_params_is_object = False;
   ELSE
     SELECT substring(__method FROM '^[^.]+') INTO __api_type;
     SELECT substring(__method FROM '[^.]+$') INTO __method_type;
-  END IF;
-
-  IF json_typeof(__params) = 'object' THEN
-    __json_type_is_object = True;
-  ELSEIF json_typeof(__params) = 'array' THEN
-    __json_type_is_object = False;
-  ELSE
-    RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_json_format_exception('Invalid JSON format:' || json_typeof(__params)::text);
+    IF json_typeof(__params) = 'object' THEN
+      __json_with_params_is_object = True;
+    ELSEIF json_typeof(__params) = 'array' THEN
+      IF json_array_length(__params) <> 0 THEN
+        __json_with_params_is_object = False;
+      ELSE
+        __json_with_params_is_object = True;
+      END IF;
+    ELSE
+      RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_json_format_exception('Invalid JSON format:' || json_typeof(__params)::text);
+    END IF;
   END IF;
 
   IF __api_type = 'condenser_api' THEN
     IF __method_type = 'get_follow_count' THEN
-      PERFORM hivemind_utilities.validate_json_parameters(__json_type_is_object, __params, '{"account":"string"}', 1, 1, '["string"]');
-      SELECT hivemind_endpoints.condenser_api_get_follow_count(_account => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'account', 0, True)) INTO __result;
+      PERFORM hivemind_utilities.validate_json_parameters(__json_with_params_is_object, __params, '{"account"}', '{"string"}');
+      SELECT hivemind_endpoints.condenser_api_get_follow_count(_account => hivemind_utilities.parse_string_argument_from_json(__params, __json_with_params_is_object, 'account', 0, True)) INTO __result;
 
     ELSEIF __method_type = 'get_reblogged_by' THEN
-      PERFORM hivemind_utilities.validate_json_parameters(__json_type_is_object, __params, '{"author":"string","permlink":"string"}', 2, 2, '["string","string"]');
-      SELECT hivemind_endpoints.condenser_api_get_reblogged_by(_author => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'author', 0, True),
-                                                               _permlink => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'permlink', 1, True)) INTO __result;
+      PERFORM hivemind_utilities.validate_json_parameters(__json_with_params_is_object, __params, '{"author","permlink"}','{"string","string"}');
+      SELECT hivemind_endpoints.condenser_api_get_reblogged_by(_author => hivemind_utilities.parse_string_argument_from_json(__params, __json_with_params_is_object, 'author', 0, True),
+                                                               _permlink => hivemind_utilities.parse_string_argument_from_json(__params, __json_with_params_is_object, 'permlink', 1, True)) INTO __result;
     ELSEIF __method_type = 'get_trending_tags' THEN
-      PERFORM hivemind_utilities.validate_json_parameters(__json_type_is_object, __params, '{"start_tag":"string","limit":"number"}', 0, 2, '["string","number"]');
-      SELECT hivemind_endpoints.condenser_api_get_trending_tags(_start_tag => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'start_tag', 0, False),
-                                                                _limit => hivemind_utilities.parse_integer_argument_from_json(__params, __json_type_is_object, 'limit', 1, False)) INTO __result;
+      PERFORM hivemind_utilities.validate_json_parameters(__json_with_params_is_object, __params, '{"start_tag","limit"}', '{"string","number"}');
+      SELECT hivemind_endpoints.condenser_api_get_trending_tags(_start_tag => hivemind_utilities.parse_string_argument_from_json(__params, __json_with_params_is_object, 'start_tag', 0, False),
+                                                                _limit => hivemind_utilities.parse_integer_argument_from_json(__params, __json_with_params_is_object, 'limit', 1, False)) INTO __result;
     ELSEIF __method_type = 'get_state' THEN
-      PERFORM hivemind_utilities.validate_json_parameters(__json_type_is_object, __params, '{"path":"string"}', 0, 1, '["string"]');
-      SELECT hivemind_endpoints.condenser_api_get_state(_path => hivemind_utilities.parse_string_argument_from_json(__params, __json_type_is_object, 'path', 0, False)) INTO __result;
+      PERFORM hivemind_utilities.validate_json_parameters(__json_with_params_is_object, __params, '{"path":"string"}', 0, 1, '["string"]');
+      SELECT hivemind_endpoints.condenser_api_get_state(_path => hivemind_utilities.parse_string_argument_from_json(__params, __json_with_params_is_object, 'path', 0, False)) INTO __result;
     END IF;
   END IF;
 
--- hive/db/sql_scripts/postgrest/utilities/validate_json_parameters.sql
+++ hive/db/sql_scripts/postgrest/utilities/validate_json_parameters.sql
@@ -1,5 +1,5 @@
 DROP FUNCTION IF EXISTS hivemind_utilities.validate_json_parameters;
-CREATE OR REPLACE FUNCTION hivemind_utilities.validate_json_parameters(IN _json_is_object BOOLEAN, IN _params JSON, IN _expected_params_for_object JSON, IN _expected_json_array_min_len INT, IN _expected_json_array_max_len INT, IN _expected_json_types_for_array JSON)
+CREATE OR REPLACE FUNCTION hivemind_utilities.validate_json_parameters(IN _json_is_object BOOLEAN, IN _params JSON, IN _expected_params_names TEXT[], IN _expected_params_types TEXT[])
 RETURNS JSON
 LANGUAGE 'plpgsql'
 AS
@@ -8,32 +8,42 @@ DECLARE
 passed_arg_key TEXT;
 passed_type_to_check TEXT;
 expected_type_to_check TEXT;
-array_idx INT := 0;
+array_idx INT;
+expected_array_len INT;
 BEGIN
   IF _json_is_object THEN
+    -- There is a case, when we expect params as an object, but no parameters are actually passed and we have an empty json array.
+    -- In that case, we should don't check anything (because no params are passed). Otherwise, we should investigate and find solution.
+    IF json_typeof(_params) = 'array' THEN
+      IF json_array_length(_params) <> 0 THEN
+        RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_json_format_exception('Invalid JSON-RPC - validate_json_parameters');
+      ELSE
+        RETURN NULL;
+      END IF;
+    END IF;
     FOR passed_arg_key IN SELECT key FROM json_each(_params) LOOP
-      IF _expected_params_for_object->passed_arg_key IS NOT NULL THEN
+      array_idx = array_position(_expected_params_names, passed_arg_key);
+      IF array_idx IS NOT NULL THEN
         passed_type_to_check = json_typeof(_params->passed_arg_key);
-        expected_type_to_check = _expected_params_for_object->>passed_arg_key;
+        expected_type_to_check = _expected_params_types[array_idx];
         IF passed_type_to_check <> expected_type_to_check THEN
-          RAISE EXCEPTION '%', hivemind_utilities.raise_parameter_validation_exception('Invalid type for parameter: ' || passed_arg_key || ' which is type of: ' || passed_type_to_check || ', expected type: ' || expected_type_to_check);
+          RAISE EXCEPTION '%', hivemind_utilities.raise_parameter_validation_exception('invalid ' || passed_arg_key || ' type');
         END IF;
       ELSE
         RAISE EXCEPTION '%', hivemind_utilities.raise_unexpected_keyword_exception(passed_arg_key);
       END IF;
     END LOOP;
   ELSE
-    IF json_array_length(_params) > _expected_json_array_max_len THEN
-      RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_array_exception(True);
-    ELSEIF json_array_length(_params) < _expected_json_array_min_len THEN
-      RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_array_exception(True);
+    expected_array_len = array_length(_expected_params_types, 1);
+    IF json_array_length(_params) <> expected_array_len THEN
+      RAISE EXCEPTION '%', hivemind_utilities.raise_invalid_parameters_array_length_exception(expected_array_len, json_array_length(_params));
     ELSE
-      FOR array_idx IN 0.._expected_json_array_max_len-1 LOOP
+      FOR array_idx IN 1..expected_array_len-1 LOOP
         IF _params->array_idx IS NOT NULL THEN
-          expected_type_to_check = _expected_json_types_for_array->>array_idx;
-          passed_type_to_check = json_typeof(_params->array_idx);
+          expected_type_to_check = _expected_params_types[array_idx];
+          passed_type_to_check = json_typeof(_params->(array_idx-1));
           IF passed_type_to_check <> expected_type_to_check THEN
-            RAISE EXCEPTION '%', hivemind_utilities.raise_parameter_validation_exception('Invalid type at position ' || array_idx || ': ' || passed_type_to_check || ', expected type: ' || expected_type_to_check);
+            RAISE EXCEPTION '%', hivemind_utilities.raise_parameter_validation_exception('invalid ' || _expected_params_names[array_idx] || ' type');
           END IF;
         -- not all parameters are required, so return without an error
         ELSE
