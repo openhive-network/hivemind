81d25e32b6f52afff770dfa813076fd31c332416
Michał Kudela <kudmich@syncad.com>
2024-09-10 15:15:52 +0200

    Move `valid_community` function to hivemind_postgrest_utilities schema

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -730,6 +730,7 @@ def setup_runtime_code(db):
         "postgrest/condenser_api/condenser_api_get_state.sql",
         "postgrest/condenser_api/condenser_api_get_account_reputations.sql",
         "postgrest/utilities/check_community.sql",
+        "postgrest/utilities/valid_community.sql",
     ]
 
     sql_scripts_dir_path = Path(__file__).parent / 'sql_scripts'
