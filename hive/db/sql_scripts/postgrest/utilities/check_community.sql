fdb897679f8e2ea56ac0233a12cb582e684f8ac7
Michał Kudela <kudmich@syncad.com>
2024-09-10 14:58:08 +0200

    Move `check_community` function to hivemind_postgrest_utilities schema

---
+++ hive/db/sql_scripts/postgrest/utilities/check_community.sql
@@ -0,0 +1,22 @@
+DROP FUNCTION IF EXISTS hivemind_postgrest_utilities.check_community;
+CREATE OR REPLACE FUNCTION hivemind_postgrest_utilities.check_community(_name TEXT)
+  RETURNS BOOLEAN
+  LANGUAGE plpgsql
+  STABLE
+AS
+$BODY$
+BEGIN
+  IF _name IS NOT NULL AND
+    LENGTH(_name) > 5 AND
+    SUBSTRING(_name FROM 1 FOR 5) = 'hive-' AND
+    SUBSTRING('hive-2123456' FROM 6 FOR 1) IN ('1', '2', '3') AND
+    _name ~ '^hive-[123]\d{4,6}$' THEN
+
+    RETURN TRUE;
+  ELSE
+
+    RETURN FALSE;
+  END IF;
+END;
+$BODY$
+;
