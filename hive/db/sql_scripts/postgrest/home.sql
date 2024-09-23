b7bc32801daa24d33238fd13885833ca193c61fc
Michał Kudela <kudmich@syncad.com>
2024-09-23 14:10:27 +0200

    Refactor `home` function: move logic josn input validation into new funciton -> `check_general_json_format`

--- hive/db/sql_scripts/postgrest/home.sql
+++ hive/db/sql_scripts/postgrest/home.sql
@@ -12,11 +12,7 @@ DECLARE
   __params JSON;
   __id JSON;
 
-  __result JSON;
-  __api_type TEXT;
-  __method_type TEXT;
-  __json_with_params_is_object BOOLEAN;
-  __method_is_call BOOLEAN;
+  __result JSONB;
   __exception_message TEXT;
   __exception JSONB;
 BEGIN
@@ -25,45 +21,19 @@ BEGIN
   __params = (__request_data->'params');
   __id = (__request_data->'id');
 
-  SELECT NULL::JSON INTO __result;
-
-  IF __jsonrpc != '2.0' OR __jsonrpc IS NULL OR __params IS NULL OR __id IS NULL OR __method IS NULL THEN
-    RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_invalid_json_format_exception('Invalid JSON-RPC');
-  END IF;
-
-  if lower(__method) = 'call' THEN
-    if json_array_length(__params) < 2 THEN
-      RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_invalid_json_format_exception('Invalid JSON-RPC');
-    END IF;
-    __api_type = __params->>0;
-    __method_type = __params->>1;
-    __params = __params->>2;
-    __json_with_params_is_object = False;
-    __method_is_call = True;
-  ELSE
-    SELECT substring(__method FROM '^[^.]+') INTO __api_type;
-    SELECT substring(__method FROM '[^.]+$') INTO __method_type;
-    __method_is_call = False;
-    IF json_typeof(__params) = 'object' THEN
-      __json_with_params_is_object = True;
-    ELSEIF json_typeof(__params) = 'array' THEN
-      IF json_array_length(__params) <> 0 THEN
-        __json_with_params_is_object = False;
-      ELSE
-        __json_with_params_is_object = True;
-      END IF;
-    ELSE
-      RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_invalid_json_format_exception('Invalid JSON format:' || json_typeof(__params)::text);
-    END IF;
-  END IF;
-
-  __result := hivemind_postgrest_utilities.dispatch(__api_type, __method_type, __json_with_params_is_object, __method_is_call, __params);
+  SELECT hivemind_postgrest_utilities.check_general_json_format(__jsonrpc, __method, __params, __id) INTO __result;
 
   RETURN jsonb_build_object(
-      'jsonrpc', '2.0',
-      'result', __result,
-      'id', __id
-    );
+    'jsonrpc', '2.0',
+    'id', __id,
+    'result', hivemind_postgrest_utilities.dispatch(
+                                                    __result->>'api_type'::TEXT,
+                                                    __result->>'method_type'::TEXT,
+                                                    (__result->>'json_with_params_is_object')::BOOLEAN,
+                                                    (__result->>'method_is_call')::BOOLEAN,
+                                                    (__result->>'params')::JSON
+    )
+  );
 
   EXCEPTION
     WHEN raise_exception THEN
