70f0c32a17bafb65e01887490b28aa47fae1a7ee
Michał Kudela <kudmich@syncad.com>
2024-09-30 16:15:31 +0000

    Create type `follow_arguments` and function `get_validated_follow_arguments`

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -731,6 +731,7 @@ def setup_runtime_code(db):
         "postgrest/condenser_api/condenser_api_get_blog.sql",
         "postgrest/condenser_api/condenser_api_get_content.sql",
         "postgrest/utilities/valid_follow_type.sql",
+        "postgrest/utilities/follow_arguments.sql",
     ]
 
     sql_scripts_dir_path = Path(__file__).parent / 'sql_scripts'
