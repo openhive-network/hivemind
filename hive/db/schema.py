41669fee9cb2202882443eb54f09ad97a8f61c92
asuch <asuch@syncad.com>
2024-09-30 10:01:27 +0200

    Added function hivemind_postgrest_utilities.create_condenser_post_object for condenser get_blog

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -723,6 +723,8 @@ def setup_runtime_code(db):
         "postgrest/utilities/check_general_json_format.sql",
         "postgrest/utilities/valid_offset.sql",
         "postgrest/utilities/list_votes.sql",
+        "postgrest/utilities/assets_operations.sql",
+        "postgrest/utilities/create_condenser_post_object.sql",
     ]
 
     sql_scripts_dir_path = Path(__file__).parent / 'sql_scripts'
