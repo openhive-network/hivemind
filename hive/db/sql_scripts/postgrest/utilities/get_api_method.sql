71fe388bf60cc51ceb31f9b36cfafc47971238bc
asuch <asuch@syncad.com>
2024-10-02 08:41:08 +0200

    Enable postgrest tests for: get_follow_count,get_blog,get_blog_entries,get_reblogged_by. These methods use condenser_api methods which are already implemented.

--- hive/db/sql_scripts/postgrest/utilities/get_api_method.sql
+++ hive/db/sql_scripts/postgrest/utilities/get_api_method.sql
@@ -48,6 +48,14 @@ BEGIN
   CASE
     WHEN __method_type = 'get_account_reputations' THEN
       result := hivemind_endpoints.condenser_api_get_account_reputations(__json_with_params_is_object, __params, /* _fat_node_style */ False)::JSONB;
+    WHEN __method_type = 'get_blog' THEN
+      result := hivemind_endpoints.condenser_api_get_blog(__json_with_params_is_object, __params, /* _get_entries */ False)::JSONB;
+    WHEN __method_type = 'get_blog_entries' THEN
+      result := hivemind_endpoints.condenser_api_get_blog(__json_with_params_is_object, __params, /* _get_entries */ True)::JSONB;
+    WHEN __method_type = 'get_follow_count' THEN
+      result :=  hivemind_endpoints.condenser_api_get_follow_count(__json_with_params_is_object, __params)::JSONB;
+    WHEN __method_type = 'get_reblogged_by' THEN
+      result := hivemind_endpoints.condenser_api_get_reblogged_by(__json_with_params_is_object, __params)::JSONB;
     ELSE
       RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_method_not_found_exception(__method);
   END CASE;
