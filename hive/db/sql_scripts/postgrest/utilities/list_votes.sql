8cde0a8511d9b73fd163f52287e74eb2449cd06c
asuch <asuch@syncad.com>
2024-10-03 12:51:33 +0200

    Update hivemind_postgrest_utilities.list_votes - it should be able to be called in other functions as well to avoid copying code

--- hive/db/sql_scripts/postgrest/utilities/list_votes.sql
+++ hive/db/sql_scripts/postgrest/utilities/list_votes.sql
@@ -21,7 +21,17 @@ FOR _vote IN SELECT * FROM jsonb_array_elements(_votes) LOOP
       'voter', _vote->>'voter'
     )::jsonb;
   ELSIF _presentation_mode = 'database_api' THEN
-    RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_parameter_validation_exception('create_votes_json_array for database_api not implemented');
+    _result = COALESCE(_result, '[]'::jsonb) || json_build_object(
+      'id', _vote->'id',
+      'voter', _vote->>'voter',
+      'author', _vote->>'author',
+      'permlink', _vote->>'permlink',
+      'weight', _vote->'weight',
+      'rshares', _vote->'rshares',
+      'vote_percent', _vote->'percent',
+      'last_update', hivemind_postgrest_utilities.json_date(to_timestamp(_vote->>'last_update', 'YYYY-MM-DD"T"HH24:MI:SS')),
+      'num_changes', _vote->'num_changes'
+    )::jsonb;
   ELSIF _presentation_mode = 'bridge_api' THEN
     RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_parameter_validation_exception('create_votes_json_array for bridge_api not implemented');
   ELSIF _presentation_mode = 'active_votes' THEN
@@ -42,8 +52,11 @@ END;
 $function$
 ;
 
+DROP TYPE IF EXISTS hivemind_postgrest_utilities.list_votes_case CASCADE;
+CREATE TYPE hivemind_postgrest_utilities.list_votes_case AS ENUM( 'create_condenser_post', 'database_list_by_comment_voter', 'database_list_by_voter_comment');
+
 DROP FUNCTION IF EXISTS hivemind_postgrest_utilities.list_votes;
-CREATE FUNCTION hivemind_postgrest_utilities.list_votes(IN _author TEXT, IN _permlink TEXT, IN _limit INT, IN _presentation_mode hivemind_postgrest_utilities.vote_presentation)
+CREATE FUNCTION hivemind_postgrest_utilities.list_votes(IN _author TEXT, IN _permlink TEXT, IN _limit INT, IN _case hivemind_postgrest_utilities.list_votes_case, IN _presentation_mode hivemind_postgrest_utilities.vote_presentation, IN _voter TEXT DEFAULT NULL)
 RETURNS JSONB
 LANGUAGE plpgsql
 STABLE
@@ -51,9 +64,15 @@ AS
 $function$
 DECLARE
 _post_id INT;
+_voter_id INT;
 _result JSONB;
 BEGIN
-  _post_id = hivemind_app.find_comment_id( _author, _permlink, True);
+  IF _case = ANY(ARRAY['database_list_by_comment_voter'::hivemind_postgrest_utilities.list_votes_case,'database_list_by_voter_comment'::hivemind_postgrest_utilities.list_votes_case]) THEN
+    assert _voter IS NOT NULL;
+    _voter_id = hivemind_postgrest_utilities.find_account_id(_voter, True);
+  END IF;
+
+  _post_id =  hivemind_postgrest_utilities.find_comment_id( _author, _permlink, True);
   _result = hivemind_postgrest_utilities.create_votes_json_array(
     ( 
       SELECT jsonb_agg(row_to_json( r )) FROM (
@@ -71,9 +90,21 @@ BEGIN
         FROM
           hivemind_app.hive_votes_view v
         WHERE
-          v.post_id = _post_id
+          (
+            CASE
+              WHEN _case = 'create_condenser_post' THEN v.post_id = _post_id
+              WHEN _case = 'database_list_by_comment_voter' THEN (v.post_id = _post_id AND v.voter_id >= _voter_id)
+              WHEN _case = 'database_list_by_voter_comment' THEN (v.voter_id = _voter_id AND v.post_id >= _post_id)
+              END
+          )
         ORDER BY
-          voter_id
+          (
+            CASE
+              WHEN _case = 'create_condenser_post' THEN v.voter_id
+              WHEN _case = 'database_list_by_comment_voter' THEN v.voter_id
+              WHEN _case = 'database_list_by_voter_comment' THEN v.post_id
+              END
+          )
       LIMIT _limit
     ) r ),
     _presentation_mode
@@ -87,8 +118,3 @@ BEGIN
 END;
 $function$
 ;
-
-
-
-
-    
