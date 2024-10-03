adffc12aa8c5518ab2306c334090a2b3407b4416
Marcin Ickiewicz <mickiewicz@syncad.com>
2024-10-03 14:35:37 +0200

    correctly check if rep_tracker is installed

--- scripts/install_app.sql
+++ scripts/install_app.sql
@@ -1,12 +1,22 @@
 -- Script contains all ADMINISTRATIVE steps required to setup a target database for Hivemind App
+CREATE OR REPLACE FUNCTION hivemind_check_reptracker( _reptracker_schema TEXT )
+RETURNS VOID
+LANGUAGE plpgsql
+STABLE
+AS $$
+BEGIN
+    ASSERT EXISTS( SELECT 1 FROM information_schema.schemata WHERE schema_name = _reptracker_schema )
+        , 'Reputation tracker is not installed';
+END;
+$$;
+
+SELECT hivemind_check_reptracker( :'REPTRACKER_SCHEMA' );
+DROP FUNCTION hivemind_check_reptracker;
+
 DO $$
 DECLARE
 __version INT;
 BEGIN
-    --ASSERT EXISTS( SELECT 1 FROM information_schema.schemata WHERE schema_name = ':REPTRACKER_SCHEMA' )
-    --    , 'Reputation tracker is not installed'
-    --;
-
   SELECT CURRENT_SETTING('server_version_num')::INT INTO __version;
 
   EXECUTE 'ALTER DATABASE '||current_database()||' SET join_collapse_limit TO 16';
