c1b76a8425a0c6ee90c7af63c449843e58bb034b
asuch <asuch@syncad.com>
2024-10-08 18:09:30 +0200

    Implemented hivemind_postgrest_utilities.rep_log10

---
+++ hive/db/sql_scripts/postgrest/utilities/rep_log10.sql
@@ -0,0 +1,34 @@
+DROP FUNCTION IF EXISTS hivemind_postgrest_utilities.rep_log10;
+CREATE FUNCTION hivemind_postgrest_utilities.rep_log10(IN _rep BIGINT)
+  RETURNS FLOAT
+  LANGUAGE plpgsql
+  IMMUTABLE
+AS
+$BODY$
+DECLARE
+_rep_is_negative BOOLEAN DEFAULT FALSE;
+_result NUMERIC;
+BEGIN
+  IF _rep = 0 THEN
+    RETURN 25;
+  END IF;
+
+  IF _rep < 0 THEN
+    _rep_is_negative = True;
+    _rep = @_rep;
+  END IF;
+                  --( first four digits \/              )
+  _result = LOG(10, (_rep / 10^(FLOOR(LOG(10, _rep) - 3)))) + 0.00000001;
+          --( HOW MANY DIGITS \/  )
+  _result = (FLOOR(LOG10(_rep) + 1) - 1) + (_result - FLOOR(_result));
+  _result = GREATEST(_result - 9, 0);
+  IF _rep_is_negative THEN
+    _result = -_result;
+  END IF;
+
+  _result = (_result * 9) + 25;
+  _result = ROUND(_result, 2);
+  RETURN _result;
+END;
+$BODY$
+;
