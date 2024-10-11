0406fd780882d9f4d1343b34d8a1baee76199c5a
Michał Kudela <kudmich@syncad.com>
2024-10-11 11:30:21 +0200

    Rewrite bridge API `get_realtionship_between_accounts` to sql

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -748,6 +748,7 @@ def setup_runtime_code(db):
         "postgrest/hive_api/hive_api_db_head_state.sql",
         "postgrest/utilities/get_account_posts.sql",
         "postgrest/bridge_api/bridge_api_get_account_posts.sql",
+        "postgrest/bridge_api/bridge_api_get_relationship_between_accounts.sql",
     ]
 
     sql_scripts_dir_path = Path(__file__).parent / 'sql_scripts'
