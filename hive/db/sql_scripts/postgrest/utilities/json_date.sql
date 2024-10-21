b19fba32a7cdb6509040cd8169aa1b1ca578d099
asuch <asuch@syncad.com>
2024-10-21 14:50:55 +0200

    Handle infinity in hivemind_postgrest_utilities.json_date

--- hive/db/sql_scripts/postgrest/utilities/json_date.sql
+++ hive/db/sql_scripts/postgrest/utilities/json_date.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION hivemind_postgrest_utilities.json_date(_date TIMESTAM
 AS
 $BODY$
 BEGIN
-  IF _date IS NULL OR _date = '9999-12-31 23:59:59+00'::TIMESTAMPTZ THEN
+  IF _date IS NULL OR _date = '9999-12-31 23:59:59+00'::TIMESTAMPTZ OR _date = 'infinity' THEN
       RETURN '1969-12-31T23:59:59';
   END IF;
 
