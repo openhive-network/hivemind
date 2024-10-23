b15fb322a46ee20962522c197f75689c9242b6bd
Michał Kudela <kudmich@syncad.com>
2024-10-23 13:57:13 +0200

    Repair parameter type in function `valid_limit`

--- hive/db/sql_scripts/postgrest/utilities/valid_limit.sql
+++ hive/db/sql_scripts/postgrest/utilities/valid_limit.sql
@@ -1,16 +1,12 @@
 DROP FUNCTION IF EXISTS hivemind_postgrest_utilities.valid_limit;
-CREATE OR REPLACE FUNCTION hivemind_postgrest_utilities.valid_limit(
-  _limit NUMERIC,
-  ubound INT,
-  default_num INT
-)
-  RETURNS INT
-  LANGUAGE plpgsql
-  STABLE
+CREATE OR REPLACE FUNCTION hivemind_postgrest_utilities.valid_limit(_limit INT, _ubound INT, _default_num INT)
+RETURNS INT
+LANGUAGE plpgsql
+STABLE
 AS
 $BODY$
 BEGIN
-  RETURN hivemind_postgrest_utilities.valid_number(_limit, default_num, 'limit', 1, ubound);
+  RETURN hivemind_postgrest_utilities.valid_number(_limit, _default_num, 1, _ubound);
 END;
 $BODY$
 ;
