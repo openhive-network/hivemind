7664cbdc45ba6bfdbe1c0cf72e3c343a8dc0a0e1
Michał Kudela <kudmich@syncad.com>
2024-10-24 12:42:25 +0200

    wip

--- hive/db/sql_scripts/postgrest/bridge_api/bridge_api_get_trending_topics.sql
+++ hive/db/sql_scripts/postgrest/bridge_api/bridge_api_get_trending_topics.sql
@@ -6,15 +6,42 @@ STABLE
 AS
 $$
 DECLARE
-    _limit INTEGER := 25;
+    _limit INTEGER := 10;
+    _observer TEXT;
+    result JSONB := '[]';
+    tag TEXT;
 BEGIN
-    PERFORM hivemind_postgrest_utilities.validate_json_parameters(_json_is_object, _params, '{"limit"}', '{"number"}');
+    PERFORM hivemind_postgrest_utilities.validate_json_parameters(_json_is_object, _params, '{"limit", "observer"}', '{"number", "string"}');
 
     _limit = hivemind_postgrest_utilities.parse_integer_argument_from_json(_params, _json_is_object, 'limit', 0, False);
     _limit = hivemind_postgrest_utilities.valid_number(_limit, 10, 1, 25, 'limit');
 
-    _top_communities = hivemind_postgrest_utilities.list_top_communities(_limit)
-    
+    _observer = hivemind_postgrest_utilities.parse_string_argument_from_json(_params, _json_is_object, 'observer', 1, False);
+    PERFORM hivemind_postgrest_utilities.valid_account(_observer, TRUE);
+
+    SELECT jsonb_agg(jsonb_build_array(
+                community_data->>0, 
+                COALESCE(NULLIF(community_data->>1, ''), community_data->>0)
+           )) 
+    INTO result
+    FROM (
+        SELECT list_top_communities AS community_data 
+        FROM hivemind_postgrest_utilities.list_top_communities(_limit)
+    ) AS subquery;
+
+    IF jsonb_array_length(result) < _limit THEN
+        FOR tag IN
+            SELECT unnest(ARRAY['photography', 'travel', 'gaming', 'crypto', 'newsteem', 'music', 'food'])
+        LOOP
+            IF jsonb_array_length(result) < _limit THEN
+                result := result || jsonb_agg(jsonb_build_array(tag, '#' || tag));
+            ELSE
+                EXIT;
+            END IF;
+        END LOOP;
+    END IF;
+
+    RETURN result;
 END
 $$
 ;
