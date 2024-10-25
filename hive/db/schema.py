fb8dc5eeb6648ba50ee97e56c10b5afef4005c81
Michał Kudela <kudmich@syncad.com>
2024-10-25 11:15:17 +0200

    Rewrite list_communities to sql

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -771,6 +771,7 @@ def setup_runtime_code(db):
         "postgrest/bridge_api/bridge_api_list_subscribers.sql",
         "postgrest/bridge_api/bridge_api_get_trending_topics.sql",
         "postgrest/utilities/validate_community_sort_type.sql",
+        "postgrest/bridge_api/bridge_api_list_communities.sql",
     ]
 
     sql_scripts_dir_path = Path(__file__).parent / 'sql_scripts'
--- hive/db/sql_scripts/postgrest/utilities/get_api_method.sql
+++ hive/db/sql_scripts/postgrest/utilities/get_api_method.sql
@@ -131,6 +131,8 @@ BEGIN
       result := hivemind_endpoints.bridge_api_list_subscribers(__json_with_params_is_object, __params);
     WHEN __method_type = 'get_trending_topics' THEN
       result := hivemind_endpoints.bridge_api_get_trending_topics(__json_with_params_is_object, __params);
+    WHEN __method_type = 'list_communities' THEN
+      result := hivemind_endpoints.bridge_api_list_communities(__json_with_params_is_object, __params);
     ELSE
       RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_method_not_found_exception(__method_type);
   END CASE;
