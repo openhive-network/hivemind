220b45034b0195d577d495c2b7895dd20a7b90be
Michał Kudela <kudmich@syncad.com>
2024-10-31 15:09:26 +0100

    Rewrite `list_pop_communities` to sql

---
+++ hive/db/sql_scripts/postgrest/bridge_api/bridge_api_list_pop_communities.sql
@@ -0,0 +1,23 @@
+DROP FUNCTION IF EXISTS hivemind_endpoints.bridge_api_list_pop_communities;
+CREATE FUNCTION hivemind_endpoints.bridge_api_list_pop_communities(IN _json_is_object BOOLEAN, IN _params JSONB)
+RETURNS JSONB
+LANGUAGE 'plpgsql'
+STABLE
+AS
+$$
+DECLARE
+    _limit INTEGER;
+    _response JSONB;
+BEGIN
+    PERFORM hivemind_postgrest_utilities.validate_json_parameters(_json_is_object, _params, '{"limit"}', '{"number"}');
+
+    _limit := hivemind_postgrest_utilities.parse_integer_argument_from_json(_params, _json_is_object, 'limit', 0, False);
+    _limit := hivemind_postgrest_utilities.valid_number(_limit, 25, 1, 25, 'limit');
+
+    SELECT jsonb_agg(jsonb_build_array(name, title) ORDER BY newsubs DESC, id DESC) INTO _response
+    FROM hivemind_app.bridge_list_pop_communities(_limit);
+
+    RETURN _response;
+END
+$$
+;
--- hive/db/sql_scripts/postgrest/utilities/get_api_method.sql
+++ hive/db/sql_scripts/postgrest/utilities/get_api_method.sql
@@ -152,6 +152,8 @@ BEGIN
       result := hivemind_endpoints.bridge_api_list_community_roles(__json_with_params_is_object, __params);
     WHEN __method_type = 'list_all_subscriptions' THEN
       result := hivemind_endpoints.bridge_api_list_all_subscriptions(__json_with_params_is_object, __params);
+    WHEN __method_type = 'list_pop_communities' THEN
+      result := hivemind_endpoints.bridge_api_list_pop_communities(__json_with_params_is_object, __params);
     ELSE
       RAISE EXCEPTION '%', hivemind_postgrest_utilities.raise_method_not_found_exception(__method_type);
   END CASE;
