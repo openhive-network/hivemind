1b1a01ece8b8abed07ed94da596724e5cda32a4d
Bartek Wrona <wrona@syncad.com>
2024-12-02 21:31:36 +0100

    Simplified `blacklisted_by_observer_view` definition

--- hive/db/sql_scripts/hive_blacklisted_accounts_by_observer_view.sql
+++ hive/db/sql_scripts/hive_blacklisted_accounts_by_observer_view.sql
@@ -1,20 +1,17 @@
 DROP VIEW IF EXISTS hivemind_app.blacklisted_by_observer_view;
 CREATE OR REPLACE VIEW hivemind_app.blacklisted_by_observer_view AS
- SELECT  hive_follows.follower AS observer_id,
-    following_accounts.id AS blacklisted_id,
-    following_accounts.name AS blacklisted_name,
+  SELECT hive_follows.follower AS observer_id,
+    hive_follows.following AS blacklisted_id,
     'my blacklist'::text AS source
    FROM hivemind_app.hive_follows
-     JOIN hivemind_app.hive_accounts following_accounts ON hive_follows.following = following_accounts.id
   WHERE hive_follows.blacklisted
 UNION ALL
  SELECT hive_follows_direct.follower AS observer_id,
-    following_accounts.id AS blacklisted_id,
-    following_accounts.name AS blacklisted_name,
+    hive_follows_indirect.following AS blacklisted_id,
     string_agg('blacklisted by '::text || indirect_accounts.name::text, ','::text ORDER BY indirect_accounts.name) AS source
    FROM hivemind_app.hive_follows hive_follows_direct
      JOIN hivemind_app.hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower
-     JOIN hivemind_app.hive_accounts following_accounts ON hive_follows_indirect.following = following_accounts.id
      JOIN hivemind_app.hive_accounts indirect_accounts ON hive_follows_indirect.follower = indirect_accounts.id
   WHERE hive_follows_direct.follow_blacklists AND hive_follows_indirect.blacklisted
-  GROUP BY hive_follows_direct.follower, following_accounts.id;
+  GROUP BY hive_follows_direct.follower, hive_follows_indirect.following
+  ;
