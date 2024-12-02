b1cde618165ae14d06913a34587d2996a3ee285a
Dan Notestein <dan@syncad.com>
2024-12-02 02:40:38 -0500

    fix get_trending_ranked_posts_for_communities

--- hive/db/sql_scripts/postgrest/utilities/get_ranked_posts.sql
+++ hive/db/sql_scripts/postgrest/utilities/get_ranked_posts.sql
@@ -134,17 +134,13 @@ BEGIN
     )
     ) FROM (
       WITH -- get_trending_ranked_posts_for_communities
-      community_posts as MATERIALIZED
+      ranked_community_posts as 
       (
-        SELECT id FROM hivemind_app.live_posts_view
-        WHERE community_id = (SELECT id FROM hivemind_app.hive_communities WHERE name = _tag) --LIMIT 1 is not necessary here
-      ),
-      ranked_community_posts as (
-        SELECT
-          hp.id
-        FROM community_posts hp
-        WHERE
-          NOT(_called_from_bridge_api AND hp.is_pinned)
+        SELECT hp.id 
+        FROM hivemind_app.live_posts_view hp
+        WHERE 
+          hp.community_id = (SELECT id FROM hivemind_app.hive_communities WHERE name = _tag) --LIMIT 1 is not necessary here
+          AND NOT(_called_from_bridge_api AND hp.is_pinned)
           AND NOT (_post_id <> 0 AND hp.sc_trend >= _trending_limit AND NOT ( hp.sc_trend = _trending_limit AND hp.id < _post_id ))
           AND NOT (_observer_id <> 0 AND EXISTS (SELECT 1 FROM hivemind_app.muted_accounts_by_id_view WHERE observer_id = _observer_id AND muted_id = hp.author_id))
         ORDER BY
