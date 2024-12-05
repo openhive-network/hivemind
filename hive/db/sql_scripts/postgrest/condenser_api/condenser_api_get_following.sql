5a367219c32073a395a93780ab7419504a90e3e4
Dan Notestein <dan@syncad.com>
2024-12-05 11:51:31 -0500

    get_following

--- hive/db/sql_scripts/postgrest/condenser_api/condenser_api_get_following.sql
+++ hive/db/sql_scripts/postgrest/condenser_api/condenser_api_get_following.sql
@@ -28,23 +28,33 @@ BEGIN
         )
         ORDER BY row.id DESC
       ) FROM (
-        WITH following_set AS -- condenser_api_get_following
+        WITH 
+        max_10k_following AS
         (
           SELECT
             hf.id,
             hf.following
           FROM hivemind_app.hive_follows hf
-          WHERE
+          WHERE -- INDEX ONLY SCAN of hive_follows_follower_following_state_idx
             hf.state = (_params->'hive_follows_state')::SMALLINT
             AND hf.follower = (_params->'account_id')::INT
-            AND NOT (_start_id <> 0 AND hf.id >= _start_id)
+          LIMIT 10000     -- if user follows more than 10K accounts, limit them
+        ),        
+        following_page AS -- condenser_api_get_following
+        (
+          SELECT
+            hf.id,
+            hf.following
+          FROM max_10k_following hf
+          WHERE
+            (_start_id = 0 OR hf.id < _start_id)
           ORDER BY hf.id DESC 
           LIMIT (_params->'limit')::INT
         )
         SELECT
           fs.id,
           ha.name
-        FROM following_set fs
+        FROM following_page fs
         JOIN hivemind_app.hive_accounts ha ON fs.following = ha.id
         ORDER BY fs.id DESC
         LIMIT (_params->'limit')::INT
