c8206d04d60ac22aa7c15a86d15e1c0ab7e17cc7
Dan Notestein <dan@syncad.com>
2024-12-07 16:04:22 -0500

    Speed hive_accounts_info_view to always use proper index and simplify query

--- hive/db/sql_scripts/hive_accounts_info_view.sql
+++ hive/db/sql_scripts/hive_accounts_info_view.sql
@@ -28,11 +28,9 @@ CREATE OR REPLACE VIEW hivemind_app.hive_accounts_info_view
     ha.name,
     ha.post_count,
     ha.created_at,
-    ( SELECT GREATEST(ha.created_at,
-                      COALESCE(latest_post.latest_post, '1970-01-01 00:00:00'::timestamp without time zone),
-                      COALESCE(whole_votes.latest_vote, '1970-01-01 00:00:00'::timestamp without time zone))
-                      AS "greatest"
-                     ) AS active_at,
+    GREATEST(ha.created_at,
+             COALESCE(latest_post.latest_post_time, '1970-01-01 00:00:00'::timestamp without time zone),
+             COALESCE(whole_votes.latest_vote_time, '1970-01-01 00:00:00'::timestamp without time zone)) AS active_at,
     ha.reputation,
     ha.rank,
     ha.following,
@@ -43,16 +41,15 @@ CREATE OR REPLACE VIEW hivemind_app.hive_accounts_info_view
    FROM hivemind_app.hive_accounts_info_view_lite ha
    LEFT JOIN LATERAL 
    (
-      SELECT hp1.created_at AS latest_post
-      FROM hivemind_app.hive_posts hp1
-      WHERE hp1.counter_deleted = 0 and hp1.author_id = ha.id
-      ORDER BY hp1.created_at DESC, hp1.author_id DESC LIMIT 1
+      SELECT MAX(hp1.created_at) AS latest_post_time
+      FROM hivemind_app.hive_posts hp1                     -- hive_posts_author_id_created_at_id_idx
+      WHERE hp1.author_id = ha.id
    ) latest_post on true
    LEFT JOIN LATERAL
    (
-     SELECT hvf.last_update AS latest_vote
-     FROM hivemind_app.hive_votes hvf
-     WHERE hvf.voter_id = ha.id
-     ORDER BY hvf.voter_id DESC, hvf.last_update DESC LIMIT 1
+     SELECT MAX(hvf.last_update) AS latest_vote_time
+     FROM hivemind_app.hive_votes hvf                      
+     WHERE hvf.voter_id = ha.id                         -- hive_votes_voter_id_last_update_idx
+     GROUP BY hvf.voter_id
    ) whole_votes ON true
    ;
