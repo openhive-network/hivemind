442bef9b4638f887cd935e303b7c93efe1710d1e
Dan Notestein <dan@syncad.com>
2024-12-19 13:38:22 -0500

    speedup list_pop_communities

--- hive/db/sql_scripts/bridge_list_pop_communities.sql
+++ hive/db/sql_scripts/bridge_list_pop_communities.sql
@@ -15,14 +15,17 @@ RETURNS SETOF hivemind_app.bridge_api_list_pop_communities
 LANGUAGE plpgsql
 AS
 $function$
+DECLARE
+  _head_block_time TIMESTAMP;
 BEGIN
+    _head_block_time := hivemind_app.head_block_time();
     RETURN QUERY
     SELECT name, title, newsubs, id
     FROM hivemind_app.hive_communities
     JOIN (
         SELECT community_id, COUNT(*) newsubs
         FROM hivemind_app.hive_subscriptions
-        WHERE created_at > hivemind_app.head_block_time() - INTERVAL '1 MONTH'
+        WHERE created_at >  _head_block_time - INTERVAL '1 MONTH'
         GROUP BY community_id
     ) stats
     ON stats.community_id = id
