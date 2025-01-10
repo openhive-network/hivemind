6a97c40e15dfcfc858d40f4c1bb3c48c7d2c1d78
Krzysztof Leśniak <klesniak@syncad.com>
2025-01-10 16:37:21 +0100

    Change hivemind_endpoints.bridge_api_get_relationship_between_accounts to new follows tables

--- hive/db/sql_scripts/bridge_get_relationship_between_accounts.sql
+++ hive/db/sql_scripts/bridge_get_relationship_between_accounts.sql
@@ -1,13 +1,11 @@
 DROP FUNCTION IF EXISTS hivemind_app.bridge_get_relationship_between_accounts;
 
 CREATE FUNCTION hivemind_app.bridge_get_relationship_between_accounts( in _account1 VARCHAR, in _account2 VARCHAR,
-  out state hivemind_app.hive_follows.state%TYPE,
-  out blacklisted hivemind_app.hive_follows.blacklisted%TYPE,
-  out follow_blacklists hivemind_app.hive_follows.follow_blacklists%TYPE,
-  out follow_muted hivemind_app.hive_follows.follow_muted%TYPE,
-  out id hivemind_app.hive_follows.id%TYPE,
-  out created_at hivemind_app.hive_follows.created_at%TYPE,
-  out block_num hivemind_app.hive_follows.block_num%TYPE)
+  out follows BOOLEAN,
+  out muted BOOLEAN,
+  out blacklisted BOOLEAN,
+  out follow_blacklists BOOLEAN,
+  out follow_muted BOOLEAN)
 AS
 $function$
 DECLARE
@@ -16,26 +14,12 @@ DECLARE
 BEGIN
   __account1_id = hivemind_app.find_account_id( _account1, True );
   __account2_id = hivemind_app.find_account_id( _account2, True );
-  SELECT
-      hf.state,
-      hf.blacklisted,
-      hf.follow_blacklists,
-      hf.follow_muted,
-      hf.id,
-      hf.created_at,
-      hf.block_num
-  INTO
-      state,
-      blacklisted,
-      follow_blacklists,
-      follow_muted,
-      id,
-      created_at,
-      block_num
-  FROM
-      hivemind_app.hive_follows hf
-  WHERE
-      hf.follower = __account1_id AND hf.following = __account2_id;
+
+  SELECT EXISTS (SELECT 1 FROM hivemind_app.follows WHERE follower=__account1_id  AND FOLLOWING=__account2_id) INTO follows;
+  SELECT EXISTS (SELECT 1 FROM hivemind_app.muted WHERE follower=__account1_id  AND FOLLOWING=__account2_id) INTO muted;
+  SELECT EXISTS (SELECT 1 FROM hivemind_app.blacklisted WHERE follower=__account1_id  AND FOLLOWING=__account2_id) INTO blacklisted;
+  SELECT EXISTS (SELECT 1 FROM hivemind_app.follow_blacklisted WHERE follower=__account1_id  AND FOLLOWING=__account2_id) INTO follow_blacklists;
+  SELECT EXISTS (SELECT 1 FROM hivemind_app.follow_muted WHERE follower=__account1_id  AND FOLLOWING=__account2_id) INTO follow_muted;
 END
 $function$
 language plpgsql STABLE;
