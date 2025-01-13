5f214213ba0cecb56e2a595cbf7cdae3d97bc4a4
asuch <asuch@syncad.com>
2025-01-13 14:20:33 +0100

    Rename option 'postgrest-server' to 'server'

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -191,10 +191,13 @@ case "$COMMAND" in
       date --utc --iso-8601=seconds > /tmp/block_processing_startup_time.txt
       run_hive
       ;;
-    postgrest-server)
+    server)
       HIVEMIND_ARGS=("${HIVEMIND_ARGS[@]:1}")
       log "global" "Running Hivemind with arguments '${HIVEMIND_ARGS[*]}'"
       run_hive
+      ;;
+    *)
+      log "global" "COMMAND - first argument is not valid. Available commands: setup, install_app, uninstall_app, sync, server"
       ;;
 esac
 
