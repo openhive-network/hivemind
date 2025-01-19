7148b52ca1a18ddef22b860d8e37e23335de0c69
Martin Lees <drov0@users.noreply.github.com>
2025-01-19 12:48:07 -0500

    Update list_subscribers to match the previous implementation order regarding joins and conditions

--- hive/db/sql_scripts/postgrest/bridge_api/bridge_api_list_subscribers.sql
+++ hive/db/sql_scripts/postgrest/bridge_api/bridge_api_list_subscribers.sql
@@ -8,7 +8,7 @@ $$
 DECLARE
     _community_id INT;
     _community TEXT;
-    _last_name TEXT;
+    _subscription_id INT;
     _limit INTEGER := 100;
 BEGIN
     _params = hivemind_postgrest_utilities.validate_json_arguments(_params, '{"community": "string", "last": "string", "limit": "number"}', 1, '{"community": "given community name is not valid"}');
@@ -17,14 +17,13 @@ BEGIN
     _community = hivemind_postgrest_utilities.valid_community(_community);
     _community_id = hivemind_postgrest_utilities.find_community_id(_community, True);
 
-    _last_name = hivemind_postgrest_utilities.valid_account(
-        hivemind_postgrest_utilities.parse_argument_from_json(_params, 'last', False),
-        True
-    );
-
-    IF _last_name IS NOT NULL THEN
-        PERFORM hivemind_postgrest_utilities.find_subscription_id(_last_name,  _community, True); -- Check that account exists and is subscribed to the community
-    END IF;
+    _subscription_id =
+      hivemind_postgrest_utilities.find_subscription_id(
+        hivemind_postgrest_utilities.valid_account(
+          hivemind_postgrest_utilities.parse_argument_from_json(_params, 'last', False),
+          True),
+        _community,
+        True);
 
     _limit = hivemind_postgrest_utilities.parse_integer_argument_from_json(_params, 'limit', False);
     _limit = hivemind_postgrest_utilities.valid_number(_limit, 100, 1, 100, 'limit');
@@ -41,8 +40,7 @@ BEGIN
           LEFT JOIN hivemind_app.hive_roles hr ON hs.account_id = hr.account_id
                AND hs.community_id = hr.community_id
           JOIN hivemind_app.hive_accounts ha ON hs.account_id = ha.id
-          WHERE hs.community_id = _community_id
-          AND (_last_name IS NULL OR ha.name COLLATE "C" > _last_name COLLATE "C")
+          WHERE hs.community_id = _community_id AND (_subscription_id = 0 OR (hs.id < _subscription_id))
           ORDER BY ha.name ASC
           LIMIT _limit
        ) row
@@ -50,4 +48,5 @@ BEGIN
       '[]'::JSONB
     );
 END
-$$;
+$$
+;
