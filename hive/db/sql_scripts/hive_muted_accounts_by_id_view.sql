51f9bff4b9258efa6f253a66f11378fe93feadce
Krzysztof Leśniak <klesniak@syncad.com>
2025-01-29 15:59:17 +0100

    Convert hivemind_app.muted_accounts_by_id_view to new tables

--- hive/db/sql_scripts/hive_muted_accounts_by_id_view.sql
+++ hive/db/sql_scripts/hive_muted_accounts_by_id_view.sql
@@ -1,12 +1,12 @@
 DROP VIEW IF EXISTS hivemind_app.muted_accounts_by_id_view CASCADE;
 CREATE OR REPLACE VIEW hivemind_app.muted_accounts_by_id_view AS
- SELECT hive_follows.follower AS observer_id,
-    hive_follows.following AS muted_id
-   FROM hivemind_app.hive_follows
-  WHERE hive_follows.state = 2
+SELECT
+  follower AS observer_id,
+  following AS muted_id
+FROM hivemind_app.muted
 UNION
- SELECT hive_follows_direct.follower AS observer_id,
-    hive_follows_indirect.following AS muted_id
-   FROM hivemind_app.hive_follows hive_follows_direct
-     JOIN hivemind_app.hive_follows hive_follows_indirect ON hive_follows_direct.following = hive_follows_indirect.follower
-  WHERE hive_follows_direct.follow_muted AND hive_follows_indirect.state = 2;
+SELECT
+  muted_direct.follower AS observer_id,
+  muted_indirect.following AS muted_id
+FROM hivemind_app.follow_muted AS muted_direct
+JOIN hivemind_app.muted AS muted_indirect ON muted_direct.following = muted_indirect.follower;
