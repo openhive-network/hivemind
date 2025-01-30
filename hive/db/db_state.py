e99646c448e3f0a155da2e2493570eb3b412a52d
Krzysztof Leśniak <klesniak@syncad.com>
2025-01-30 13:19:45 +0100

    Remove hive_follows table

--- hive/db/db_state.py
+++ hive/db/db_state.py
@@ -109,13 +109,6 @@ class DbState:
             'hive_feed_cache_created_at_idx',
             'hive_feed_cache_post_id_idx',
             'hive_feed_cache_account_id_created_at_post_id_idx',
-            'hive_follows_following_state_id_idx',  # (following, state, id)
-            'hive_follows_follower_state_idx',  # (follower, state, created_at, following)
-            'hive_follows_follower_following_state_idx',
-            'hive_follows_block_num_idx',
-            'hive_follows_follower_where_blacklisted_idx',
-            'hive_follows_follower_where_follow_muted_idx',
-            'hive_follows_follower_where_follow_blacklists_idx',
             'hive_posts_parent_id_id_idx',
             'hive_posts_depth_idx',
             'hive_posts_root_id_id_idx',
