0399b2af1d8a2719364128b12936e9f29a32308b
Konrad Botor <kbotor@syncad.com>
2025-02-06 14:44:54 +0100

    Removed word instance from image names

--- scripts/ci-helpers/build_instance.sh
+++ scripts/ci-helpers/build_instance.sh
@@ -16,12 +16,13 @@ print_help () {
 cat <<EOF
 Usage: $0 <image_tag> <src_dir> <registry_url> [OPTION[=VALUE]]...
 Allows to build docker image containing Hivemind installation
-The image will be tagged with name '<registry_url>/instance:instance-<image_tag>'
+The image will be tagged with name '<registry_url>:<image_tag>'
 
 OPTIONS:
-  -?,--help                        Display this help screen and exit
-  --dot-env-filename=<filename> File name of the dot env file to be generated
-  --dot-env-var-name=<var name> Vaiable name to be used in the generated file (default: 'IMAGE')
+  -?,--help                          Display this help screen and exit
+  --dot-env-filename=<filename>      File name of the dot env file to be generated
+  --dot-env-var-prefix=<var name>    Vaiable name prefix to be used in the generated file (default: '')
+  --additional-image-tag=<image_tag> Additional image tag (default: '')
 EOF
 }
 
@@ -34,9 +35,12 @@ while [ $# -gt 0 ]; do
     --dot-env-filename=*)
         DOT_ENV_FILENAME="${1#*=}"
         ;;
-    --dot-env-var-name=*)
-        DOTENV_VAR_NAME="${1#*=}"
+    --dot-env-var-prefix=*)
+        DOTENV_VAR_NAME_PREFIX="${1#*=}"
         ;;
+    --additional-image-tag=*)
+        CI_COMMIT_TAG="${1#*=}"
+        ;;    
     *)
         if [ -z "$BUILD_IMAGE_TAG" ];
         then
@@ -62,10 +66,6 @@ done
 [[ -z "$SRCROOTDIR" ]] && printf "Missing argument #2 - source directory path\n\n" && print_help && exit 1
 [[ -z "$REGISTRY" ]] && printf "Missing argument #3 - target container registry URL\n\n" && print_help && exit 1
 
-# Supplement a registry path by trailing slash (if needed)
-[[ "${REGISTRY}" != */ ]] && REGISTRY="${REGISTRY}/"
-REGISTRY_WITHOUT_SLASH="$(echo "$REGISTRY" | sed 's/\/$//')"
-
 printf "Moving into source root directory: %s\n" "$SRCROOTDIR"
 
 pushd "$SRCROOTDIR"
@@ -75,16 +75,18 @@ pwd
 
 CI_IMAGE_TAG=${CI_IMAGE_TAG:-"python-3.8-slim-6"} # see scripts/ci/build_ci_base_image.sh for the current tag
 BUILD_OPTIONS=("--platform=linux/amd64" "--target=instance" "--progress=plain")
-TAG="${REGISTRY}instance:$BUILD_IMAGE_TAG"
-MINIMAL_TAG="${REGISTRY}minimal-instance:$BUILD_IMAGE_TAG"
-ALTERNATE_TAG="${REGISTRY_WITHOUT_SLASH}:$BUILD_IMAGE_TAG"
-REWRITER_IMAGE_NAME=${REGISTRY}postgrest-rewriter:$BUILD_IMAGE_TAG
+TAG_BUILD_ARGS=("--platform=linux/amd64" "--progress=plain")
+TAG="${REGISTRY}:$BUILD_IMAGE_TAG"
+MINIMAL_TAG="${REGISTRY}/minimal:$BUILD_IMAGE_TAG"
+REWRITER_IMAGE_TAG=${REGISTRY}/postgrest-rewriter:$BUILD_IMAGE_TAG
 
 # On CI push the images to the registry
 if [[ -n "${CI:-}" ]]; then
   BUILD_OPTIONS+=("--push")
+  TAG_BUILD_ARGS+=("--push")
 else
   BUILD_OPTIONS+=("--load")
+  TAG_BUILD_ARGS+=("--load")
 fi
 
 BUILD_TIME="$(date -uIseconds)"
@@ -118,13 +120,14 @@ if [ -z "$GIT_LAST_COMMIT_DATE" ]; then
 fi
 
 REWRITER_TARGET=without_tag
-if [ ! -z "$BUILD_IMAGE_TAG" ]; then
+if [[ -n "$BUILD_IMAGE_TAG" ]]; then
   REWRITER_TARGET=with_tag
-  TAG_BUILD_ARGS="--build-arg GIT_COMMIT_TAG=$BUILD_IMAGE_TAG"
+  TAG_BUILD_ARGS+=("--build-arg" "GIT_COMMIT_TAG=$BUILD_IMAGE_TAG")
 fi
 
+echo "Building Hivemind full and minimal images..."
 docker buildx build "${BUILD_OPTIONS[@]}" \
-  --build-context "runtime=docker-image://${REGISTRY}runtime:${CI_IMAGE_TAG}" \
+  --build-context "runtime=docker-image://${REGISTRY}/runtime:${CI_IMAGE_TAG}" \
   --build-arg BUILD_TIME="$BUILD_TIME" \
   --build-arg GIT_COMMIT_SHA="$GIT_COMMIT_SHA" \
   --build-arg GIT_CURRENT_BRANCH="$GIT_CURRENT_BRANCH" \
@@ -132,29 +135,48 @@ docker buildx build "${BUILD_OPTIONS[@]}" \
   --build-arg GIT_LAST_COMMITTER="$GIT_LAST_COMMITTER" \
   --build-arg GIT_LAST_COMMIT_DATE="$GIT_LAST_COMMIT_DATE" \
   --tag "$TAG" \
+  --tag "$MINIMAL_TAG" \
   --file Dockerfile .
+echo "Done!"
 
-  docker buildx build \
-    --build-arg BUILD_TIME="$BUILD_TIME" \
-    --build-arg GIT_COMMIT_SHA="$GIT_COMMIT_SHA" \
-    --build-arg GIT_CURRENT_BRANCH="$GIT_CURRENT_BRANCH" \
-    --build-arg GIT_LAST_LOG_MESSAGE="$GIT_LAST_LOG_MESSAGE" \
-    --build-arg GIT_LAST_COMMITTER="$GIT_LAST_COMMITTER" \
-    --build-arg GIT_LAST_COMMIT_DATE="$GIT_LAST_COMMIT_DATE" \
-    --target=$REWRITER_TARGET \
-    $TAG_BUILD_ARGS \
-    --tag "$REWRITER_IMAGE_NAME" \
-    --load \
-    --file Dockerfile.rewriter .
+echo "Building HYivemind rewriter image..."
+docker buildx build "${TAG_BUILD_ARGS[@]}" \
+  --build-arg BUILD_TIME="$BUILD_TIME" \
+  --build-arg GIT_COMMIT_SHA="$GIT_COMMIT_SHA" \
+  --build-arg GIT_CURRENT_BRANCH="$GIT_CURRENT_BRANCH" \
+  --build-arg GIT_LAST_LOG_MESSAGE="$GIT_LAST_LOG_MESSAGE" \
+  --build-arg GIT_LAST_COMMITTER="$GIT_LAST_COMMITTER" \
+  --build-arg GIT_LAST_COMMIT_DATE="$GIT_LAST_COMMIT_DATE" \
+  --target=$REWRITER_TARGET \
+  --tag "$REWRITER_IMAGE_TAG" \
+  --file Dockerfile.rewriter .
+echo "Done!"
 
-# Since CI pushes the image directly to the registry, it needs to be pulled to be tagged
+# Since CI pushes the image directly to the registry, it needs to be pulled to be tagged for publishing job
 if [[ -n "${CI:-}" ]]; then
  docker pull "$TAG"
+ docker pull "$REWRITER_IMAGE_TAG"
 fi
 
-docker tag "$TAG" "$MINIMAL_TAG"
-docker tag "$TAG" "$ALTERNATE_TAG"
+# Generate dotenv file if necessary
+if [[ -n "${DOT_ENV_FILENAME:-}" ]]; then
+  {
+      echo "${DOTENV_VAR_NAME_PREFIX:+"${DOTENV_VAR_NAME_PREFIX}_"}IMAGE=$TAG"
+      echo "${DOTENV_VAR_NAME_PREFIX:+"${DOTENV_VAR_NAME_PREFIX}_"}MINIMAL_IMAGE=$MINIMAL_TAG"
+      echo "${DOTENV_VAR_NAME_PREFIX:+"${DOTENV_VAR_NAME_PREFIX}_"}REWRITER_IMAGE=$REWRITER_IMAGE_TAG"
+  } > "$DOT_ENV_FILENAME"
+fi
 
-[[ -n "${DOT_ENV_FILENAME:-}" ]] && echo "${DOTENV_VAR_NAME:-IMAGE}=$TAG" > "$DOT_ENV_FILENAME"
+# Tag with additional tag if specified.
+if [[ -n "$CI_COMMIT_TAG" ]]; then
+  docker tag "$TAG" "$REGISTRY:$CI_COMMIT_TAG"
+  docker tag "$REWRITER_IMAGE_TAG" "$REGISTRY/postgrest-rewriter:$CI_COMMIT_TAG"
+  
+  # On CI push new tags to registry.
+  if [[ -n "${CI:-}" ]]; then
+    docker push "$REGISTRY:$CI_COMMIT_TAG"
+    docker push "$REGISTRY/postgrest-rewriter:$CI_COMMIT_TAG"
+  fi
+fi
 
 popd
