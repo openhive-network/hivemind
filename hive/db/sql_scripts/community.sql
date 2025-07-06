bee913aa50d3b995f73b5f6c26ecccea206c4c65
Martin Lees <drov0@users.noreply.github.com>
2025-07-06 15:26:36 -0400

    slight edits to be more readable + comments

--- hive/db/sql_scripts/community.sql
+++ hive/db/sql_scripts/community.sql
@@ -1,33 +1,33 @@
 DROP FUNCTION IF EXISTS hivemind_app.set_community_role;
 CREATE OR REPLACE FUNCTION hivemind_app.set_community_role(
-    p_account_id INTEGER,
-    p_community_id INTEGER,
-    p_role_id INTEGER,
-    p_date TIMESTAMP,
-    p_max_mod_nb INTEGER,
-    p_mod_role_threshold INTEGER
+    _account_id INTEGER,
+    _community_id INTEGER,
+    _role_id INTEGER,
+    _date TIMESTAMP,
+    _max_mod_nb INTEGER, -- maximum number of roles >= to mod in a community
+    _mod_role_threshold INTEGER -- minimum role id to be counted as
 ) RETURNS TABLE(status TEXT, mod_count BIGINT) AS $$
 BEGIN
     RETURN QUERY
     WITH mod_check AS (
         SELECT 
             CASE 
-                WHEN p_role_id >= p_mod_role_threshold THEN
+                WHEN _role_id >= _mod_role_threshold THEN
                     (SELECT COUNT(*) 
                      FROM hivemind_app.hive_roles 
-                     WHERE community_id = p_community_id 
-                     AND role_id >= p_mod_role_threshold
-                         AND account_id != p_account_id)
+                     WHERE community_id = _community_id
+                     AND role_id >= _mod_role_threshold
+                         AND account_id != _account_id)
                 ELSE 0
             END as current_mod_count
     ),
     insert_attempt AS (
         INSERT INTO hivemind_app.hive_roles (account_id, community_id, role_id, created_at)
-        SELECT p_account_id, p_community_id, p_role_id, p_date
+        SELECT _account_id, _community_id, _role_id, _date
         FROM mod_check
-        WHERE current_mod_count < p_max_mod_nb OR p_role_id < p_mod_role_threshold
+        WHERE current_mod_count < _max_mod_nb OR _role_id < _mod_role_threshold
         ON CONFLICT (account_id, community_id) 
-        DO UPDATE SET role_id = p_role_id
+        DO UPDATE SET role_id = _role_id
         RETURNING *
     )
     SELECT 
