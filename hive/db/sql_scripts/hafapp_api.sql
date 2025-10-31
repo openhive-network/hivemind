4b82889d6cfbc47b6e9ddd6ef871b6c2e5dd5961
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-10-31 13:06:40 +0100

    Fix inefficient plans in enum_blocks4hivemind by using LATERAL join
    During performance tests, the planner occasionally produced suboptimal
    plans for the per-block aggregation query, resulting in repeated
    GroupAggregate executions and excessive CPU time. Switching to a
    LATERAL join makes the aggregation deterministic and consistently fast
    (~2s ? ~300ms per 1k blocks).

--- hive/db/sql_scripts/hafapp_api.sql
+++ hive/db/sql_scripts/hafapp_api.sql
@@ -68,34 +68,33 @@ AS
 $function$
 BEGIN
     RETURN QUERY
-        SELECT -- hivemind_app.hive_api_hivemind_blocks
-               hb.num
-             , hb.hash
-             , hb.prev as prev
-             , to_char( created_at,  'YYYY-MM-DDThh24:MI:SS' ) as date
-             , COALESCE( oper.operations, '{}'::jsonb[] ) as operations
-        FROM hivemind_app.blocks_view hb
-                 LEFT JOIN (
+        SELECT
+            hb.num,
+            hb.hash,
+            hb.prev,
+            to_char(hb.created_at, 'YYYY-MM-DD"T"HH24:MI:SS') AS date,
+            COALESCE(oper.operations, '{}'::jsonb[]) AS operations
+        FROM hivemind_app.blocks_view AS hb
+        LEFT JOIN LATERAL (
             SELECT
-                   op.block_num
-                 , ARRAY_AGG( to_jsonb(op) ORDER BY op.id ) as operations
-            FROM
-                (SELECT
-                     ho.id
-                      , ho.block_num
-                      , ho.op_type_id as operation_type_id
-                      , ho.op_type_id >= 50 AS is_virtual
-                      , ho.body::VARCHAR
-                 FROM hivemind_app.operations_view ho
-                 WHERE ho.block_num BETWEEN _first_block AND _last_block
-                   AND (ho.op_type_id < 50
-                     OR ho.op_type_id in (51, 53, 61, 72, 73)
-                     )
-                ) as op
-            GROUP BY op.block_num
-        ) as oper ON oper.block_num = hb.num
+                ARRAY_AGG(
+                        to_jsonb(op) ORDER BY op.id
+                ) AS operations
+            FROM (
+                     SELECT
+                         ho.id,
+                         ho.block_num,
+                         ho.op_type_id AS operation_type_id,
+                         ho.op_type_id >= 50 AS is_virtual,
+                         ho.body::VARCHAR
+                     FROM hivemind_app.operations_view ho
+                     WHERE ho.block_num = hb.num
+                       AND (ho.op_type_id < 50
+                         OR ho.op_type_id IN (51, 53, 61, 72, 73))
+                 ) AS op
+            ) AS oper ON TRUE
         WHERE hb.num BETWEEN _first_block AND _last_block
-        ORDER by hb.num
+        ORDER BY hb.num
     ;
 END
 $function$
