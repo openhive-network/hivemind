ccc748e2d7822517fb8c02c601e461fc4024da65
howo <drov.fr@protonmail.com>
2025-11-04 14:27:57 +0900

    Fix forgotten hardcoded test string in check_community.sql

--- hive/db/sql_scripts/postgrest/utilities/check_community.sql
+++ hive/db/sql_scripts/postgrest/utilities/check_community.sql
@@ -9,7 +9,7 @@ BEGIN
   IF _name IS NOT NULL AND
     LENGTH(_name) > 5 AND
     SUBSTRING(_name FROM 1 FOR 5) = 'hive-' AND
-    SUBSTRING('hive-2123456' FROM 6 FOR 1) IN ('1', '2', '3') AND
+    SUBSTRING(_name FROM 6 FOR 1) IN ('1', '2', '3') AND
     _name ~ '^hive-[123]\d{4,6}$' THEN
 
     RETURN TRUE;
