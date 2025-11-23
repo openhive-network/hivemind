4c48e386123dac206695d994601f9dc3adca86a4
Howo <drov.fr@protonmail.com>
2025-11-22 22:18:01 -0500

    add back set_role

--- hive/db/sql_scripts/community.sql
+++ hive/db/sql_scripts/community.sql
@@ -94,45 +94,43 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-DROP FUNCTION IF EXISTS hivemind_app.register_community;
-CREATE OR REPLACE FUNCTION hivemind_app.register_community(
-    _name VARCHAR,
+DROP FUNCTION IF EXISTS hivemind_app.set_community_role;
+CREATE OR REPLACE FUNCTION hivemind_app.set_community_role(
     _account_id INTEGER,
-    _block_date TIMESTAMP,
-    _block_num INTEGER,
-    _counter INTEGER
-) RETURNS VOID AS $$
-DECLARE
-    _type_id INTEGER;
-    _notification_first_block INTEGER;
+    _community_id INTEGER,
+    _role_id INTEGER,
+    _date TIMESTAMP,
+    _max_mod_nb INTEGER, -- maximum number of roles >= to mod in a community
+    _mod_role_threshold INTEGER -- minimum role id to be counted as
+) RETURNS TABLE(status TEXT, mod_count BIGINT) AS $$
 BEGIN
-    -- Extract type_id from name (6th character, after "hive-")
-    _type_id := SUBSTRING(_name, 6, 1)::INTEGER;
-
-    INSERT INTO hivemind_app.hive_communities (id, name, type_id, created_at, block_num)
-    VALUES (_account_id, _name, _type_id, _block_date, _block_num);
-
-    INSERT INTO hivemind_app.hive_roles (community_id, account_id, role_id, created_at)
-    VALUES (_account_id, _account_id, 8, _block_date); -- 8 = owner role id
-
-    SELECT hivemind_app.block_before_irreversible('90 days') INTO _notification_first_block;
-    IF _block_num > _notification_first_block THEN
-        INSERT INTO hivemind_app.hive_notification_cache
-        (id, block_num, type_id, created_at, src, dst, dst_post_id, post_id, score, payload, community, community_title)
+    RETURN QUERY
+    WITH mod_check AS (
         SELECT
-            hivemind_app.notification_id(_block_date, 1, _counter),
-            _block_num,
-            1,
-            _block_date,
-            0,
-            _account_id,
-            0,
-            0,
-            35,
-            '',
-            _name,
-            ''
-        WHERE _block_num > hivemind_app.block_before_irreversible('90 days');
-    END IF;
+            CASE
+                WHEN _role_id >= _mod_role_threshold THEN
+                    (SELECT COUNT(*)
+                     FROM hivemind_app.hive_roles
+                     WHERE community_id = _community_id
+                     AND role_id >= _mod_role_threshold
+                         AND account_id != _account_id)
+                ELSE 0
+            END as current_mod_count
+    ),
+    insert_attempt AS (
+        INSERT INTO hivemind_app.hive_roles (account_id, community_id, role_id, created_at)
+        SELECT _account_id, _community_id, _role_id, _date
+        FROM mod_check
+        WHERE current_mod_count < _max_mod_nb OR _role_id < _mod_role_threshold
+        ON CONFLICT (account_id, community_id)
+        DO UPDATE SET role_id = _role_id
+        RETURNING *
+    )
+    SELECT
+        CASE
+            WHEN EXISTS (SELECT 1 FROM insert_attempt) THEN 'success'
+            ELSE 'failed_mod_limit'
+        END as status,
+        (SELECT current_mod_count FROM mod_check) as mod_count;
 END;
 $$ LANGUAGE plpgsql;
