3e3ce2809e2d7bc3d95d1846955a68d9139df0b0
Marcin Ickiewicz <mickiewicz@syncad.com>
2026-02-16 10:48:07 +0100

    Use public HAF views instead of internal tables
    - Use hive.blocks_view instead of hafd.blocks in populate_haf_with_mocked_data.py
    - Use hafd.make_block_id() for mock block INSERT (new HAF schema uses block_id)
    
    HAF feature branch mi/refactoring_reversible_cc_squashed2 encodes block_num
    into block_id. The public views maintain backward compatibility.

--- hive/indexer/mocking/mock_block.py
+++ hive/indexer/mocking/mock_block.py
@@ -289,10 +289,10 @@ class BlockMockAfterDb:
         initminer_account_id = 3
 
         sql = f"""
-INSERT INTO hafd.blocks (num, hash, prev, created_at, producer_account_id, transaction_merkle_root, extensions,
+INSERT INTO hafd.blocks (block_id, hash, prev, created_at, producer_account_id, transaction_merkle_root, extensions,
                          witness_signature, signing_key, hbd_interest_rate, total_vesting_fund_hive, total_vesting_shares,
                          total_reward_fund_hive, virtual_supply, current_supply, current_hbd_supply, dhf_interval_ledger)
-VALUES (:num, :hash, :prev, :created_at, {initminer_account_id}, 'mocked'::bytea, NULL, 'mocked'::bytea, 'mocked', 1000, 1000,
+VALUES (hafd.make_block_id(:num, 1), :hash, :prev, :created_at, {initminer_account_id}, 'mocked'::bytea, NULL, 'mocked'::bytea, 'mocked', 1000, 1000,
                          1000000, 1000, 1000, 1000, 2000, 2000);
 """
 
